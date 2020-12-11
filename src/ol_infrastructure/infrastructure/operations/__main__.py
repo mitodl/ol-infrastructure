95aca39329813d610dbfd1efc7e51c4dd06105c1
Sar <sar.haidar@gmail.com>
2020-12-11 14:41:18 -0500

    Create elasticsearch cluster (#23)
    Add module for deploying Elasticsearch clusters.
    Added in logic to dynamically pull in service modules to deploy per environment.
    
    Co-authored-by: Tobias Macey <tmacey@mit.edu>

--- src/ol_infrastructure/infrastructure/operations/__main__.py
+++ src/ol_infrastructure/infrastructure/operations/__main__.py
@@ -1,4 +1,5 @@
-import consul
+import importlib
+
 from pulumi import Config, export
 
 from ol_infrastructure.infrastructure import operations as ops
@@ -6,12 +7,13 @@ from ol_infrastructure.infrastructure import operations as ops
 env_config = Config("environment")
 environment_name = f"{ops.env_prefix}-{ops.env_suffix}"
 business_unit = env_config.get("business_unit") or "operations"
+service_list = env_config.require_object("services")
 
-export(
-    "security_groups",
-    {
-        "consul_server": consul.consul_server_security_group.id,
-        "consul_agent": consul.consul_agent_security_group.id,
-    },
-)
-export("consul", consul.consul_export)
+security_groups = {}
+
+for service in service_list:
+    module = importlib.import_module(service)
+    security_groups.update(getattr(module, "security_groups", {}))
+    export(service, getattr(module, "export_data", {}))
+
+export("security_groups", security_groups)
