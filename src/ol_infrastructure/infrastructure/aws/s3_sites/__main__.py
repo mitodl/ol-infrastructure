cb252e7fbe2d0d246d11c543d3ce71a0ddd99a4f
sar <sar.haidar@gmail.com>
2021-02-02 14:47:13 -0500

    OCW beta resource cleanup

--- src/ol_infrastructure/infrastructure/aws/s3_sites/__main__.py
+++ src/ol_infrastructure/infrastructure/aws/s3_sites/__main__.py
@@ -1,6 +1,5 @@
 """Module for creating and managing S3 buckets that are not used by any applications."""
 from pulumi import StackReference, export
-from pulumi.config import get_config
 from pulumi_aws import route53
 
 from ol_infrastructure.components.aws.s3_cloudfront_site import (
@@ -36,26 +35,3 @@ if stack_info.env_suffix == "production":
         "xpro_certs_distribution_id", xpro_legacy_certs_site.cloudfront_distribution.id
     )
     export("xpro_certs_acm_cname", xpro_legacy_certs_site.site_tls.domain_name)
-
-
-# Static site for hosting beta releases of OCW content built with Hugo course publisher
-mitodl_zone_id = dns_stack.get_output("odl_zone_id")
-ocw_beta_site_config = S3ServerlessSiteConfig(
-    site_name=f"ocw-beta-{stack_info.env_suffix}-course-site",
-    domains=[get_config("ocw_beta_site:domain")],
-    bucket_name=f"ocw-beta-{stack_info.env_suffix}-course-site",
-    tags={"OU": "open-courseware", "Environment": f"ocw-{stack_info.env_suffix}"},
-)
-ocw_beta_site = S3ServerlessSite(ocw_beta_site_config)
-ocw_beta_domain = route53.Record(
-    "ocw-beta-course-site-domain",
-    name=ocw_beta_site_config.domains[0],
-    type="CNAME",
-    ttl=fifteen_minutes,
-    records=[ocw_beta_site.cloudfront_distribution.domain_name],
-    zone_id=mitodl_zone_id,
-)
-
-export("ocw_beta_site_bucket", ocw_beta_site.site_bucket.bucket)
-export("ocw_beta_site_distribution_id", ocw_beta_site.cloudfront_distribution.id)
-export("ocw_beta_site_acm_cname", ocw_beta_site.site_tls.domain_name)
