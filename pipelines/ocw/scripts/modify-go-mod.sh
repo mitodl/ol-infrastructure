3a434dd503b82fd169e7d0f16250f656dac7fd85
Mark Breedlove <mbrdlove@mit.edu>
2021-05-17 15:09:31 -0400

    Add OCW build for Concourse

---
+++ pipelines/ocw/scripts/modify-go-mod.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e  # fail on error
+set -x  # show commands on stdout
+
+basepath=`pwd`
+cd ocw-www
+cp go.mod go.mod.backup
+printf "\nreplace github.com/mitodl/ocw-hugo-themes/base-theme => $basepath/ocw-hugo-themes/base-theme\n" >> go.mod
+printf "\nreplace github.com/mitodl/ocw-hugo-themes/www => $basepath/ocw-hugo-themes/www\n" >> go.mod
