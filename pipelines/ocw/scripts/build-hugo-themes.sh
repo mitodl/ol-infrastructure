fd4363b811c7b0045bfceb6a3baa6eda55f6357b
Mark Breedlove <mbrdlove@mit.edu>
2021-05-19 12:52:02 -0400

    Revert "test -- remove before merge!"
    This reverts commit 2e70bee86b84ae790a006570b1e1b3455fc0e336.

--- pipelines/ocw/scripts/build-hugo-themes.sh
+++ pipelines/ocw/scripts/build-hugo-themes.sh
@@ -10,8 +10,3 @@ cd ocw-hugo-themes
 yarn install --pure-lockfile
 npm run build:githash
 npm run build
-
-# Just for testing rclone!
-mkdir ../ocw-www/dist/courses
-date > ../ocw-www/dist/courses/testfile1
-date > ../ocw-www/dist/testfile2
