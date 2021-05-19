2e70bee86b84ae790a006570b1e1b3455fc0e336
Mark Breedlove <mbrdlove@mit.edu>
2021-05-19 11:51:52 -0400

    test -- remove before merge!

--- pipelines/ocw/scripts/build-hugo-themes.sh
+++ pipelines/ocw/scripts/build-hugo-themes.sh
@@ -10,3 +10,8 @@ cd ocw-hugo-themes
 yarn install --pure-lockfile
 npm run build:githash
 npm run build
+
+# Just for testing rclone!
+mkdir ../ocw-www/dist/courses
+date > ../ocw-www/dist/courses/testfile1
+date > ../ocw-www/dist/testfile2
