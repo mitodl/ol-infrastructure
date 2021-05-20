f53536824a40eaea6f7c629870f9e6040000566d
Mark Breedlove <mbrdlove@mit.edu>
2021-05-20 14:04:53 -0400

    Change paths in shell scripts relative to container work dir

--- pipelines/ocw/scripts/build-hugo-themes.sh
+++ pipelines/ocw/scripts/build-hugo-themes.sh
@@ -4,7 +4,7 @@ set -e  # exit on error
 set -x  # show commands on stdout
 
 export PATH=$PATH:/usr/local/go/bin
-export EXTERNAL_SITE_PATH=../ocw-www
+export EXTERNAL_SITE_PATH=`pwd`/ocw-www
 
 cd ocw-hugo-themes
 yarn install --pure-lockfile
--- pipelines/ocw/scripts/build-one-course.sh
+++ pipelines/ocw/scripts/build-one-course.sh
@@ -4,8 +4,8 @@ set -e  # exit on error
 set -x  # show commands on stdout
 
 export PATH=$PATH:/usr/local/go/bin
-export EXTERNAL_SITE_PATH=../ocw-www
-export OCW_TO_HUGO_OUTPUT_DIR=../course-markdown
+export EXTERNAL_SITE_PATH=`pwd`/ocw-www
+export OCW_TO_HUGO_OUTPUT_DIR=`pwd`/course-markdown
 export COURSE_OUTPUT_DIR=$EXTERNAL_SITE_PATH/dist/courses
 export COURSE_BASE_URL=$COURSE_BASE_URL
 
