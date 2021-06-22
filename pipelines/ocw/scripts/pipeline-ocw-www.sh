670c8c24f167b7cb9dc5e6cb9604f15c5c747e4a
Matt Bertrand <mrbertrand@gmail.com>
2021-06-22 14:08:47 -0400

    feedback

--- pipelines/ocw/scripts/pipeline-ocw-www.sh
+++ pipelines/ocw/scripts/pipeline-ocw-www.sh
@@ -1,32 +1,45 @@
 #!/bin/bash
 
-SITE_BRANCH=$1  # Should be "preview" or "release"
-if [[ $SITE_BRANCH == "preview" ]]
-then
-  FASTLY_SERVICE_ID=$FASTLY_SERVICE_ID_PREVIEW
-  FASTLY_API_TOKEN=$FASTLY_API_TOKEN_PREVIEW
-  OCW_CONTENT_BUCKET=$OCW_CONTENT_BUCKET_PREVIEW
-elif [[ $SITE_BRANCH == "release" ]]
-then
-  FASTLY_SERVICE_ID=$FASTLY_SERVICE_ID_RELEASE
-  FASTLY_API_TOKEN=$FASTLY_API_TOKEN_RELEASE
-  OCW_CONTENT_BUCKET=$OCW_CONTENT_BUCKET_RELEASE
-else
-  echo "Invalid branch $1"
-  exit 1
-fi
+# This script requires prior authentication via target "odl-concourse"
 
-yes | fly -t $FLY_TARGET set-pipeline \
--p ocw-www-pipeline-$SITE_BRANCH \
---team=ocw \
---config=pipelines/ocw/pipeline-ocw-www.yml \
--v git-domain=$GITHUB_DOMAIN \
--v github-org=$GIT_CONFIG_ORG \
--v hugo-theme-branch=$HUGO_THEME_BRANCH \
--v ocw-www-repo=$OCW_WWW_REPO \
--v ocw-www-repo-branch=$SITE_BRANCH \
--v ocw-bucket=$OCW_CONTENT_BUCKET \
--v ocw-studio-url=$OCW_STUDIO_BASE_URL \
--v fastly-service-id=$FASTLY_SERVICE_ID \
--v fastly-api-token=$FASTLY_API_TOKEN \
--v git-private-key="$GIT_PRIVATE_KEY"
+# The following variables need to be set, and will be different between RC and production:
+# GIT_CONFIG_ORG=<the github organization>
+# GITHUB_DOMAIN=<the github domain>
+# OCW_WWW_REPO=<the github repo for the ocw-www site>
+# OCW_CONTENT_BUCKET_PREVIEW=<the S3 preview bucket>
+# OCW_CONTENT_BUCKET_RELEASE=<the S3 release bucket>
+# OCW_STUDIO_BASE_URL=<the ocw-studio base url>
+
+# The following need to be set via vault, per environment and pipeline group (preview, release):
+# fastly-service-id
+# fastly-api-token
+# git-private-key
+
+
+BRANCHES=("preview" "release")
+
+for branch in "${BRANCHES[@]}"
+do
+  if [[ $branch == "preview" ]]
+  then
+    OCW_CONTENT_BUCKET=$OCW_CONTENT_BUCKET_PREVIEW
+  elif [[ $branch == "release" ]]
+  then
+    OCW_CONTENT_BUCKET=$OCW_CONTENT_BUCKET_RELEASE
+  else
+    echo "Invalid branch $1"
+    exit 1
+  fi
+
+  yes | fly -t odl-concourse set-pipeline \
+  -p ocw-www-pipeline-via-vault \
+  --team=ocw \
+  --config=pipelines/ocw/pipeline-ocw-www.yml \
+  --instance-var branch=$branch \
+  -v git-domain=$GITHUB_DOMAIN \
+  -v github-org=$GIT_CONFIG_ORG \
+  -v ocw-www-repo=$OCW_WWW_REPO \
+  -v ocw-www-repo-branch=$branch \
+  -v ocw-bucket=$OCW_CONTENT_BUCKET \
+  -v ocw-studio-url=$OCW_STUDIO_BASE_URL
+done
