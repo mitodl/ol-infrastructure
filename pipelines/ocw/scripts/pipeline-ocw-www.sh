ad350808076903a15bc2b6d211bb609f11b5e2b2
Matt Bertrand <mrbertrand@gmail.com>
2021-06-24 15:24:00 -0400

    use variable for ocw-hugo-projects branch

--- pipelines/ocw/scripts/pipeline-ocw-www.sh
+++ pipelines/ocw/scripts/pipeline-ocw-www.sh
@@ -9,6 +9,7 @@
 # OCW_CONTENT_BUCKET_PREVIEW=<the S3 preview bucket>
 # OCW_CONTENT_BUCKET_RELEASE=<the S3 release bucket>
 # OCW_STUDIO_BASE_URL=<the ocw-studio base url>
+# OCW_HUGO_PROJECTS_BRANCH=<'release-candidate' on RC, 'release' on production>
 
 # The following need to be set via vault, per environment and pipeline group (preview, release):
 # fastly-service-id
@@ -41,5 +42,6 @@ do
   -v ocw-www-repo=$OCW_WWW_REPO \
   -v ocw-www-repo-branch=$branch \
   -v ocw-bucket=$OCW_CONTENT_BUCKET \
-  -v ocw-studio-url=$OCW_STUDIO_BASE_URL
+  -v ocw-studio-url=$OCW_STUDIO_BASE_URL \
+  -v ocw-hugo-projects-branch=$OCW_HUGO_PROJECTS_BRANCH
 done
