091eab365f561a42d9c53b2b52e87e89c342e14e
Eric Berg <berge@mit.edu>
2021-07-27 14:07:00 -0400

    add wan autojoin conf file

--- src/ol_infrastructure/lib/consul.py
+++ src/ol_infrastructure/lib/consul.py
@@ -1,12 +1,17 @@
 import base64
 import json
-import textwrap
+
 import yaml
 
 
 def build_consul_userdata(env_name):
-    """ Builds consul userdata for cloud init, to create auto scale group and launch configs
-        TODO setup for consul_esm """
+    """Build consul userdata for cloud init, to create auto scale group and launch configs.
+
+    :param env_name: consul environment being built
+    :returns: base64-encoded yaml cloud init data
+
+    TODO setup for consul_esm
+    """
     return base64.b64encode(
         "#cloud-config\n{}".format(
             yaml.dump(
@@ -17,6 +22,19 @@ def build_consul_userdata(env_name):
                             "content": json.dumps(
                                 {
                                     "retry_join": [
+                                        "provider=aws tag_key=consul_env "
+                                        f"tag_value={env_name}"
+                                    ],
+                                    "datacenter": env_name,
+                                }
+                            ),
+                            "owner": "consul:consul",
+                        },
+                        {
+                            "path": "/etc/consul.d/99-autojoin-wan.json.json",
+                            "content": json.dumps(
+                                {
+                                    "retry_join_wan": [
                                         "provider=aws tag_key=consul_env "
                                         f"tag_value={env_name}"
                                     ],
@@ -40,7 +58,7 @@ def build_consul_userdata(env_name):
                             ),
                             "owner": "consul:consul",
                         },
-                        """
+                        """,
                     ]
                 },
                 sort_keys=True,
