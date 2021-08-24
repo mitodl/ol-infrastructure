58123b3846f92ef6807e426250413b53d9dcbee3
Tobias Macey <tmacey@mit.edu>
2021-08-24 14:34:31 -0400

    Adding creation of edxapp user to allow setting theme permissions
    - Adding creation of edxapp user to allow for setting permissions on theme directory so that the Ansible build doesn't fail due to directory access
    - Adding python3-wheel so that dependencies can be installed as wheels

--- src/bilder/images/edxapp/prebuild.py
+++ src/bilder/images/edxapp/prebuild.py
@@ -1,25 +1,36 @@
-from pyinfra.operations import files, git
+from pyinfra.operations import files, git, server
 
 from bilder.components.baseline.steps import install_baseline_packages
 from bilder.images.edxapp.lib import WEB_NODE_TYPE, node_type
 
+EDX_USER = "edxapp"
+
 install_baseline_packages(
     packages=[
+        "build-essential",
         "curl",
         "git",
         "libmariadbclient-dev",
+        "python3-dev",
         "python3-pip",
         "python3-venv",
-        "python3-dev",
-        "build-essential",
+        "python3-wheel",
     ],
     upgrade_system=True,
 )
 
 if node_type == WEB_NODE_TYPE:
+    server.user(
+        name="Proactively create edxapp user for setting permissions on theme repo",
+        user=EDX_USER,
+        present=True,
+        shell="/bin/false",  # noqa: S604
+    )
     files.directory(
         name="Ensure themes directory is present",
         path="/edx/app/edxapp/themes/",
+        user=EDX_USER,
+        group=EDX_USER,
         present=True,
     )
     git.repo(
@@ -28,4 +39,6 @@ if node_type == WEB_NODE_TYPE:
         # Using a generic directory to simplify usage across deployments
         dest="/edx/app/edxapp/themes/edxapp-theme",
         branch="main",
+        user=EDX_USER,
+        group=EDX_USER,
     )
