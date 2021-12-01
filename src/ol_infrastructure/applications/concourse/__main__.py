c6e522df50800fed3130246427378defa7475ae9
Sar <sar.haidar@gmail.com>
2021-12-01 11:09:09 -0600

    Fix concourse permissions (#480)
    * Fix concourse permission
    
    * Fix concourse permission

--- src/ol_infrastructure/applications/concourse/__main__.py
+++ src/ol_infrastructure/applications/concourse/__main__.py
@@ -100,8 +100,8 @@ concourse_iam_permissions = {
         {
             "Effect": "Allow",
             "Action": [
-                "s3:GetObject",
-                "s3:PutObject",
+                "s3:GetObject*",
+                "s3:PutObject*",
                 "s3:DeleteObject",
                 "s3:ListBucket*",
             ],
