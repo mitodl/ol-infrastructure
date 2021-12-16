ecf627eb3ad64c1efec44588b8cb63633bc6ddbf
Tobias Macey <tmacey@mit.edu>
2021-12-16 12:32:59 -0500

    Added delete_before_replace config for Consul prepared queries

--- src/ol_infrastructure/substructure/consul/__main__.py
+++ src/ol_infrastructure/substructure/consul/__main__.py
@@ -1,4 +1,4 @@
-from pulumi import StackReference
+from pulumi import ResourceOptions, StackReference
 from pulumi_consul import (
     PreparedQuery,
     PreparedQueryFailoverArgs,
@@ -11,7 +11,9 @@ from ol_infrastructure.lib.pulumi_helper import parse_stack
 stack_info = parse_stack()
 network_stack = StackReference(f"infrastructure.aws.network.{stack_info.name}")
 operations_vpc = network_stack.require_output("operations_vpc")
-consul_provider = get_consul_provider(stack_info)
+consul_provider = get_consul_provider(stack_info).merge(
+    ResourceOptions(delete_before_replace=True)
+)
 
 vault_operations_query = PreparedQuery(
     "vault-active-server-prepared-query",
