6fb987ce4e39cd375cf1ab20c774550f3f51dd5e
Ardiea <mas48@mit.edu>
2022-03-29 10:40:23 -0400

    Updated based on review feedback.

--- src/ol_infrastructure/infrastructure/grafana_cloud/__main__.py
+++ src/ol_infrastructure/infrastructure/grafana_cloud/__main__.py
@@ -1,11 +1,9 @@
-from pulumi import Config
 from pulumi_aws import iam
 
 from ol_infrastructure.lib.aws.iam_helper import lint_iam_policy
-from ol_infrastructure.lib.vault import setup_vault_provider
+from ol_infrastructure.lib.ol_types import AWSBase
 
-if Config("vault_server").get("env_namespace"):
-    setup_vault_provider()
+aws_config = AWSBase(tags={"OU": "operations", "Environment": "operations-production"})
 
 # Reference: https://grafana.com/docs/grafana/latest/datasources/aws-cloudwatch/
 raw_policy = {
@@ -16,13 +14,27 @@ raw_policy = {
             "Effect": "Allow",
             "Action": [
                 "cloudwatch:DescribeAlarmsForMetric",
-                "cloudwatch:DescribeAlarmHistory",
-                "cloudwatch:DescribeAlarms",
                 "cloudwatch:ListMetrics",
                 "cloudwatch:GetMetricData",
-                "cloudwatch:GetInsightRuleReport",
             ],
             "Resource": "*",
+        },
+        {
+            "Sid": "AllowReadingAlarmsFromCloudWatch",
+            "Effect": "Allow",
+            "Action": [
+                "cloudwatch:DescribeAlarmHistory",
+                "cloudwatch:DescribeAlarms",
+            ],
+            "Resource": f"arn:*:cloudwatch:{aws_config.region}:*:alarm:*",
+        },
+        {
+            "Sid": "AllowReadingInsightRuleReportsFromCloudWatch",
+            "Effect": "Allow",
+            "Action": [
+                "cloudwatch:GetInsightRuleReport",
+            ],
+            "Resource": f"arn:*:cloudwatch:{aws_config.region}:*:insight-rule/*",
         },
         {
             "Sid": "AllowReadingLogsFromCloudWatch",
@@ -31,9 +43,23 @@ raw_policy = {
                 "logs:DescribeLogGroups",
                 "logs:GetLogGroupFields",
                 "logs:StartQuery",
-                "logs:StopQuery",
-                "logs:GetQueryResults",
                 "logs:GetLogEvents",
+            ],
+            "Resource": "arn:*:logs:*:*:log-group:*",
+        },
+        {
+            "Sid": "AllowStopLogQuery",
+            "Effect": "Allow",
+            "Action": [
+                "logs:StopQuery",
+            ],
+            "Resource": "*",
+        },
+        {
+            "Sid": "AllowGetLogQueryResults",
+            "Effect": "Allow",
+            "Action": [
+                "logs:GetQueryResults",
             ],
             "Resource": "*",
         },
@@ -59,15 +85,14 @@ grafana_cloudwatch_policy = iam.Policy(
     "create-grafana-readonly-cloudwatch-policy",
     name="allow-grafana-cloud-cloudwatch-access",
     path="/ol-operations/global-policies/",
-    policy=lint_iam_policy(
-        raw_policy, parliament_config={"RESOURCE_STAR": {}}, stringify=True
-    ),
+    policy=lint_iam_policy(raw_policy, stringify=True),
+    tags=aws_config.tags,
 )
 
-grafana_user = iam.User("grafanacloud-readonly")
+grafana_user = iam.User("grafanacloud-readonly", tags=aws_config.tags)
 
 iam.UserPolicyAttachment(
     "grafana-readonly-cloudwatch-policy-attachment",
     user=grafana_user.name,
-    policy_arn=grafana_cloudwatch_policy,
+    policy_arn=grafana_cloudwatch_policy.arn,
 )
