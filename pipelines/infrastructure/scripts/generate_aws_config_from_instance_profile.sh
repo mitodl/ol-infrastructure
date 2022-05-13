784adc741e0715cd6c07ed46d4318696084fe7a1
Tobias Macey <tmacey@mit.edu>
2022-05-13 11:49:00 -0400

    fix: Remove wrapping "" from ARN in aws creds script
    The script doesn't like the `"` characters so this trims them from the ARN using `sed`

--- pipelines/infrastructure/scripts/generate_aws_config_from_instance_profile.sh
+++ pipelines/infrastructure/scripts/generate_aws_config_from_instance_profile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-AWS_ROLE=$(aws sts get-caller-identity | awk '/arn/ {print $2}')
+AWS_ROLE=$(aws sts get-caller-identity | awk '/arn/ {print $2}' | sed 's/\"//g')
 export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
 $(aws sts assume-role \
 --role-arn $AWS_ROLE \
