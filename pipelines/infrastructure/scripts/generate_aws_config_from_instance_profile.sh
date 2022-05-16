0e11d506577cb3cb1a58116750318901530e353d
Tobias Macey <tmacey@mit.edu>
2022-05-16 10:49:07 -0400

    fix: Rely entirely on EC2 metadata for AWS credentials
    Removes usage of the AWS CLI and STS logic in favor of `curl`ing the appropriate
    endpoint and parsing out the desired values with `awk` and `sed`

--- pipelines/infrastructure/scripts/generate_aws_config_from_instance_profile.sh
+++ pipelines/infrastructure/scripts/generate_aws_config_from_instance_profile.sh
@@ -3,12 +3,9 @@
 set -e
 
 AWS_ROLE=$(curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/)
-export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
-$(aws sts assume-role \
---role-arn $AWS_ROLE \
---role-session-name MySessionName \
---query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
---output text))
+export AWS_ACCESS_KEY_ID=$(curl http://169.254.169.254/latest/meta-data/iam/security-credentials/$ROLE | awk '/AccessKeyId/ {print $3}' | sed 's/[",]//g')
+export AWS_SECRET_KEY=$(curl http://169.254.169.254/latest/meta-data/iam/security-credentials/$ROLE | awk '/SecretAccessKey/ {print $3}' | sed 's/[",]//g')
+export AWS_SESSION_TOKEN=$(curl http://169.254.169.254/latest/meta-data/iam/security-credentials/$ROLE | awk '/Token/ {print $3}' | sed 's/[",]//g')
 cd aws_creds
 echo "[default]" > credentials
 echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> credentials
