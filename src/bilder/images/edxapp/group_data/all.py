e5a4215043899b69bdd3e80b783615656f5de903
Tobias Macey <tmacey@mit.edu>
2022-06-01 09:29:43 -0400

    version: Remove edX plugin version pins for Nutmeg
    For the Nutmeg release we want to pull in the newer versions of our plugins so that they
    include the updated functionality for that release.

--- src/bilder/images/edxapp/group_data/all.py
+++ src/bilder/images/edxapp/group_data/all.py
@@ -52,29 +52,29 @@ edx_plugins_added = {
     "mitx": [
         "celery-redbeat",  # Support for using Redis as the lock for Celery schedules
         "django-redis",  # Support for Redis caching in Django
-        "edx-sga==0.17.3",  # remove pin when upgrading to nutmeg
+        "edx-sga",
         "edx-sysadmin",
         "git+https://github.com/raccoongang/xblock-pdf.git@8d63047c53bc8fdd84fa7b0ec577bb0a729c215f#egg=xblock-pdf",  # noqa: E501
         "ol-openedx-logging",
         "ol-openedx-sentry",
         "ol-openedx-course-export",
         "rapid-response-xblock==0.6.0",
-        "ol-openedx-canvas-integration==0.1.1",  # TODO: Remove pin when upgrading to nutmeg
-        "ol-openedx-rapid-response-reports==0.1.0",  # TODO: Remove pin when upgrading to nutmeg
+        "ol-openedx-canvas-integration",
+        "ol-openedx-rapid-response-reports",
         "git+https://github.com/arslanashraf7/super-csv@740b7c76cff72f0bbe6b1f3dde3a1086cf8e1cc3#egg=super-csv",  # noqa: E501
     ],
     "mitx-staging": [
         "celery-redbeat",  # Support for using Redis as the lock for Celery schedules
         "django-redis",  # Support for Redis caching in Django
-        "edx-sga==0.17.3",  # remove pin when upgrading to nutmeg
+        "edx-sga",
         "edx-sysadmin",
         "git+https://github.com/raccoongang/xblock-pdf.git@8d63047c53bc8fdd84fa7b0ec577bb0a729c215f#egg=xblock-pdf",
         "ol-openedx-logging",
         "ol-openedx-sentry",
         "ol-openedx-course-export",
-        "rapid-response-xblock==0.6.0",
-        "ol-openedx-canvas-integration==0.1.1",  # TODO: Remove pin when upgrading to nutmeg
-        "ol-openedx-rapid-response-reports==0.1.0",  # TODO:  Remove pin when upgrading to nutmeg
+        "rapid-response-xblock",
+        "ol-openedx-canvas-integration",
+        "ol-openedx-rapid-response-reports",
         "git+https://github.com/arslanashraf7/super-csv@740b7c76cff72f0bbe6b1f3dde3a1086cf8e1cc3#egg=super-csv",  # noqa: E501
     ],
 }
