1b7c1902b11ccb88bfe657b7cc2ae4a4c06e6f34
Arslan <arslan.ashraf@arbisoft.com>
2022-06-03 16:41:05 +0500

    chore: add super-csv package from the encode fix git

--- src/bilder/images/edxapp/group_data/all.py
+++ src/bilder/images/edxapp/group_data/all.py
@@ -32,6 +32,7 @@ edx_plugins_added = {
         "ol-openedx-course-export",
         "ol-openedx-checkout-external",
         "social-auth-mitxpro==0.6",
+        "git+https://github.com/openedx/super-csv.git@740b7c76cff72f0bbe6b1f3dde3a1086cf8e1cc3#egg=super-csv",
     ],
     "xpro": [
         "celery-redbeat",  # Support for using Redis as the lock for Celery schedules
@@ -44,6 +45,7 @@ edx_plugins_added = {
         "ol-openedx-course-export",
         "social-auth-mitxpro==0.6",
         "git+https://github.com/ubc/ubcpi.git@1.0.0#egg=ubcpi-xblock",
+        "git+https://github.com/openedx/super-csv.git@740b7c76cff72f0bbe6b1f3dde3a1086cf8e1cc3#egg=super-csv",
     ],
     "mitx": [
         "celery-redbeat",  # Support for using Redis as the lock for Celery schedules
@@ -57,6 +59,7 @@ edx_plugins_added = {
         "rapid-response-xblock==0.6.0",
         "ol-openedx-canvas-integration",
         "ol-openedx-rapid-response-reports",
+        "git+https://github.com/openedx/super-csv.git@740b7c76cff72f0bbe6b1f3dde3a1086cf8e1cc3#egg=super-csv",
     ],
     "mitx-staging": [
         "celery-redbeat",  # Support for using Redis as the lock for Celery schedules
@@ -70,6 +73,7 @@ edx_plugins_added = {
         "rapid-response-xblock",
         "ol-openedx-canvas-integration",
         "ol-openedx-rapid-response-reports",
+        "git+https://github.com/openedx/super-csv.git@740b7c76cff72f0bbe6b1f3dde3a1086cf8e1cc3#egg=super-csv",
     ],
 }
 
