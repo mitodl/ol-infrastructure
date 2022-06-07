c0945f7af64ed4f5c4f4ee073f088fefbbd377f7
Tobias Macey <tmacey@mit.edu>
2022-06-07 16:46:12 -0400

    refactor: Clean up Fargate model and add attribute details
    The AWS API has a lot of detail that is needed to use it effectively. This adds
    well-documented `Field` objects that use the contents of the relevant AWS documentation
    for those attributes. It also cleans up the log and container definitions to be easier
    to use.

--- src/ol_infrastructure/lib/aws/ecs/container_definition_config.py
+++ src/ol_infrastructure/lib/aws/ecs/container_definition_config.py
@@ -1,9 +1,40 @@
 from typing import Optional
 
-from pulumi_aws.secretsmanager import Secret
-from pydantic import BaseModel, PositiveInt
+from pydantic import BaseModel, PositiveInt, Field
 
 from bridge.lib.magic_numbers import DEFAULT_HTTP_PORT, HALF_GIGABYTE_MB
+from ol_infrastructure.lib.pulumi_helper import StackInfo
+
+
+def build_container_log_options(
+    service_name: str,
+    task_name: str,
+    stack_info: StackInfo,
+    container_name: Optional[str] = None,
+) -> dict[str, str]:
+    return {
+        "awslogs-group": f"ecs/{service_name}/{task_name}/{stack_info.env_suffix}/",  # noqa: E501
+        "awslogs-region": "us-east-1",
+        "awslogs-stream-prefix": f"{container_name}",
+        "awslogs-create-group": "true",
+    }
+
+
+class Secret(BaseModel):
+    name: str = Field(..., description="The name of the secret.")
+    value_from: str = Field(
+        ...,
+        alias="valueFrom",
+        description=(
+            "The secret to expose to the container. The supported values are either the"
+            " full ARN of the AWS Secrets Manager secret or the full ARN of the"
+            " parameter in the AWS Systems Manager Parameter Store.  If the AWS Systems"
+            " Manager Parameter Store parameter exists in the same Region as the task"
+            " you are launching, then you can use either the full ARN or name of the"
+            " parameter. If the parameter exists in a different Region, then the full"
+            " ARN must be specified. "
+        ),
+    )
 
 
 class OLContainerLogConfig(BaseModel):
@@ -20,34 +51,83 @@ class OLContainerLogConfig(BaseModel):
 
 
 # Many more options available (in AWS) that are not defined in this configuration
+# https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_ContainerDefinition.html
 class OLFargateContainerDefinitionConfig(BaseModel):
-    container_name: str
-    # Memory reserved for this container. If container exceeds this amount, it will be
-    # killed.
-    memory: Optional[PositiveInt] = PositiveInt(HALF_GIGABYTE_MB)
-    # Fully qualified (registry/repository:tag) where ECS agent can retrieve image
-    image: str
-    # Soft limit of memory to reserve for the container
-    memory_reservation: Optional[PositiveInt]
-    # What port will be assigned to container.
-    container_port: PositiveInt = PositiveInt(DEFAULT_HTTP_PORT)
-    # The command that is passed to the container
-    command: Optional[list[str]] = None
-    # Number of cpu units reserved for container
-    cpu: Optional[PositiveInt]
-    # Enabling this flag means if this container stops or fails, all other containers
-    # that are part of the task are stopped
-    is_essential: bool = False
-    # Environment variables to pass to container
-    environment: Optional[dict[str, str]] = None
-    # Secrets that will be exposed to your container
-    secrets: Optional[list[Secret]] = None
-    log_configuration: Optional[OLContainerLogConfig] = None
-    # If enabled, container is given elevated privileges, similar to 'root' user
-    privileged: bool = False
-    # If set to True, container will be attached to target group and load balancer using
-    # the port_mappings name and container port
-    attach_to_load_balancer: bool = False
+    container_name: str = Field(
+        ...,
+        description="Name of the container in the task config",
+        parameter_name="name",
+    )
+    memory: Optional[PositiveInt] = Field(
+        PositiveInt(HALF_GIGABYTE_MB),
+        description="Memory reserved for this container. "
+        "If container exceeds this amount, it will be killed",
+        parameter_name="memory",
+    )
+    image: str = Field(
+        ...,
+        description="Fully qualified (registry/repository:tag) where ECS agent "
+        "can retrieve image",
+        parameter_name="image",
+    )
+    memory_reservation: Optional[PositiveInt] = Field(
+        None,
+        description="Soft limit of memory to reserve for the container",
+        parameter_name="memoryReservation",
+    )
+    container_port: PositiveInt = Field(
+        PositiveInt(DEFAULT_HTTP_PORT),
+        description="What port will be assigned to container.",
+        parameter_name="containerPort",
+    )
+    command: Optional[list[str]] = Field(
+        None,
+        description="The command that is passed to the container",
+        parameter_name="command",
+    )
+    cpu: Optional[PositiveInt] = Field(
+        None,
+        description="Number of cpu units reserved for container",
+        parameter_name="cpu",
+    )
+    is_essential: bool = Field(
+        False,
+        description="Enabling this flag means if this container stops or fails, "
+        "all other containers that are part of the task are stopped",
+        parameter_name="essential",
+    )
+    environment: Optional[dict[str, str]] = Field(
+        None,
+        description="Environment variables to pass to container",
+        parameter_name="environment",
+    )
+    secrets: Optional[list[Secret]] = Field(
+        None,
+        description="Secrets that will be exposed to your container",
+        parameter_name="secrets",
+    )
+    log_configuration: Optional[OLContainerLogConfig] = Field(
+        None,
+        description="Configuration for setting up log outputs for this container",
+        parameter_name="logConfiguration",
+    )
+    privileged: bool = Field(
+        False,
+        description="If enabled, container is given elevated privileges, "
+        "similar to 'root' user",
+        parameter_name="privileged",
+    )
+    attach_to_load_balancer: bool = Field(
+        False,
+        description="If set to True, container will be attached to target group and "
+        "load balancer using the port_mappings name and container port",
+    )
+    volumes_from: Optional[list[dict[str, str]]] = Field(
+        None,
+        description="Allow for mounting paths betwen containers. Useful for rendering "
+        "configuration templates via Vault agent or consul-template sidecars.",
+        parameter_name="volumesFrom",
+    )
 
     class Config:
         arbitrary_types_allowed = True
