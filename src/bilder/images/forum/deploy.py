569f655b2fa574377ffef94a9f6e54aeeb229599
Angelo <angelomitinho@yahoo.com>
2022-06-09 13:50:02 -0500

    Forum

---
+++ src/bilder/images/forum/deploy.py
@@ -0,0 +1,116 @@
+import os
+from pathlib import Path
+
+from pyinfra import host
+from pyinfra.operations import files, server
+
+from bilder.components.hashicorp.consul.models import (
+    Consul,
+    ConsulAddresses,
+    ConsulConfig,
+)
+from bilder.components.hashicorp.consul_template.models import ConsulTemplate
+from bilder.components.hashicorp.steps import configure_hashicorp_product
+from bilder.components.hashicorp.vault.models import (
+    Vault,
+    VaultAgentCache,
+    VaultAgentConfig,
+    VaultAutoAuthAWS,
+    VaultAutoAuthConfig,
+    VaultAutoAuthFileSink,
+    VaultAutoAuthMethod,
+    VaultAutoAuthSink,
+    VaultConnectionConfig,
+    VaultListener,
+    VaultTCPListener,
+    VaultTemplate,
+)
+from bilder.components.hashicorp.vault.steps import vault_template_permissions
+from bilder.facts.has_systemd import HasSystemd
+from bilder.lib.linux_helpers import DOCKER_COMPOSE_DIRECTORY
+from bridge.lib.magic_numbers import VAULT_HTTP_PORT
+from bridge.lib.versions import CONSUL_VERSION, VAULT_VERSION
+from bridge.secrets.sops import set_env_secrets
+
+set_env_secrets(Path("consul/consul.env"))
+
+TEMPLATES_DIRECTORY = Path(__file__).resolve().parent.joinpath("templates")
+FILES_DIRECTORY = Path(__file__).resolve().parent.joinpath("files")
+VERSIONS = {
+    "consul": os.environ.get("CONSUL_VERSION", CONSUL_VERSION),
+    "vault": os.environ.get("VAULT_VERSION", VAULT_VERSION),
+}
+
+
+files.put(
+    name="Place the forum docker-compose.yaml file",
+    src=str(Path(__file__).resolve().parent.joinpath("files", "docker-compose.yaml")),
+    dest=DOCKER_COMPOSE_DIRECTORY,
+    mode="0660",
+)
+
+files.put(
+    name="Place the forum .env file",
+    src=str(Path(__file__).resolve().parent.joinpath("files", "forum.env.j2")),
+    dest=DOCKER_COMPOSE_DIRECTORY,
+    mode="0660",
+)
+
+vault_templates = [
+    VaultTemplate(
+        source=f"{DOCKER_COMPOSE_DIRECTORY}/forum.env.j2",
+        destination=Path("/etc/forum.env"),
+    ),
+]
+consul_configuration = {
+    Path("00-default.json"): ConsulConfig(
+        addresses=ConsulAddresses(dns="127.0.0.1", http="127.0.0.1"),
+        advertise_addr='{{ GetInterfaceIP "ens5" }}',
+    )
+}
+
+vault_config = VaultAgentConfig(
+    cache=VaultAgentCache(use_auto_auth_token="force"),
+    listener=[
+        VaultListener(
+            tcp=VaultTCPListener(
+                address=f"127.0.0.1:{VAULT_HTTP_PORT}", tls_disable=True
+            )
+        )
+    ],
+    vault=VaultConnectionConfig(
+        address=f"https://vault.query.consul:{VAULT_HTTP_PORT}",
+        tls_skip_verify=True,
+    ),
+    auto_auth=VaultAutoAuthConfig(
+        method=VaultAutoAuthMethod(
+            type="aws",
+            mount_path="auth/aws",
+            config=VaultAutoAuthAWS(role="airbyte-server"),
+        ),
+        sink=[VaultAutoAuthSink(type="file", config=[VaultAutoAuthFileSink()])],
+    ),
+    template=vault_templates,
+)
+
+vault = Vault(
+    version=VERSIONS["vault"],
+    configuration={Path("vault.json"): vault_config},
+)
+consul = Consul(version=VERSIONS["consul"], configuration=consul_configuration)
+consul_template_config = ConsulTemplate()
+
+hashicorp_products = [vault, consul]
+
+vault_template_permissions(vault_config)
+
+for product in hashicorp_products:
+    configure_hashicorp_product(product)
+if host.get_fact(HasSystemd):
+
+    server.service(
+        name="Ensure docker compose service is enabled",
+        service="docker-compose",
+        running=False,
+        enabled=True,
+    )
