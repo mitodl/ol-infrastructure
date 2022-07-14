9418fffa60e33191d086102437f864acfa6b73ac
Tobias Macey <tmacey@mit.edu>
2022-07-14 12:06:59 -0400

    fix: Replace use of docker-image with registry-image

--- src/concourse/lib/resource_types.py
+++ src/concourse/lib/resource_types.py
@@ -4,6 +4,6 @@ from concourse.lib.models import Identifier, ResourceType
 def rclone() -> ResourceType:
     return ResourceType(
         name=Identifier("rclone"),
-        type="docker-image",
+        type="registry-image",
         source={"repository": "mitodl/concourse-rclone-resource", "tag": "latest"},
     )
