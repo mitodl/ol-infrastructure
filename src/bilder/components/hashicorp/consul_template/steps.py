ac1953b7f122fac99771d14dc206c39535a9ac6c
Tobias Macey <tmacey@mit.edu>
2022-07-18 11:32:59 -0400

    fix: Resolve mypy errors in pre-commit run

--- src/bilder/components/hashicorp/consul_template/steps.py
+++ src/bilder/components/hashicorp/consul_template/steps.py
@@ -3,7 +3,10 @@ from pathlib import Path
 from pyinfra.api import deploy
 from pyinfra.operations import apt, server
 
-from bilder.components.hashicorp.consul_template.models import ConsulTemplateConfig
+from bilder.components.hashicorp.consul_template.models import (
+    ConsulTemplateConfig,
+    ConsulTemplateTemplate,
+)
 
 
 @deploy("Manage consul-template template destination permissions")
@@ -14,7 +17,7 @@ def consul_template_permissions(
         name="Install ACL package for more granular file permissions",
         packages=["acl"],
     )
-    templates = []
+    templates: list[ConsulTemplateTemplate] = []
     for config in consul_template_configs.values():
         templates.extend(config.template or [])
     for template in templates:
