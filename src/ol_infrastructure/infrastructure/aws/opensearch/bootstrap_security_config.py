978bb298357dee01b6cd94d0c6d60a27df06c205
Ardiea <mas48@mit.edu>
2022-07-21 07:45:50 -0400

    Try 3 at fixing the opensearch permissions.

--- src/ol_infrastructure/infrastructure/aws/opensearch/bootstrap_security_config.py
+++ src/ol_infrastructure/infrastructure/aws/opensearch/bootstrap_security_config.py
@@ -33,7 +33,7 @@ read_only_role = {
     ],
 }
 read_write_role = {
-    "cluster_permissions": ["cluster_composite_ops"],
+    "cluster_permissions": ["cluster_composite_ops", "indices:data/read/scroll", "indices:data/read/scroll/clear"],
     "index_permissions": [
         {
             "index_patterns": ["*"],  # TODO: Define actual indices
@@ -43,6 +43,8 @@ read_write_role = {
                 "indices_all",
                 "indices:data/read/scroll",
                 "indices:data/read/scroll/clear",
+                "indices:data/read/scroll*",
+                "indices:data/read/scroll/clear*",
             ],  # TODO: Confirm this is all that is needed
         }
     ],
