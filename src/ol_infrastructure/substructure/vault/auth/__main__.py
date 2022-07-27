28001b7a6dec992b02589dbc7c7648ed81596200
sar <sar.haidar@gmail.com>
2022-07-27 07:27:27 -0500

    Fixed vault policy setup

--- src/ol_infrastructure/substructure/vault/auth/__main__.py
+++ src/ol_infrastructure/substructure/vault/auth/__main__.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
-import pulumi_vault as vault
 from pulumi import Config
+from pulumi_vault import AuthBackend, AuthBackendTuneArgs, Policy, aws, github
 
 from bridge.lib.magic_numbers import ONE_MONTH_SECONDS, SIX_MONTHS
 from ol_infrastructure.lib.ol_types import Environment
@@ -15,33 +15,33 @@ if Config("vault_server").get("env_namespace"):
     setup_vault_provider()
 
 # Generic AWS backend
-vault_aws_auth = vault.AuthBackend(
+vault_aws_auth = AuthBackend(
     "vault-aws-auth-backend",
     type="aws",
 )
 
-vault_aws_auth_client = vault.aws.AuthBackendClient(
+vault_aws_auth_client = aws.AuthBackendClient(
     "vault-aws-auth-backend-client-configuration",
     backend=vault_aws_auth.path,
 )
 
 # Per environment backends
 for env in Environment:
-    vault_aws_auth = vault.AuthBackend(
+    vault_aws_auth = AuthBackend(
         f"vault-aws-auth-backend-{env.value}",
         type="aws",
         path=f"aws-{env.value}",
         description="AWS authentication via EC2 IAM",
-        tune=vault.AuthBackendTuneArgs(token_type="default-service"),
+        tune=AuthBackendTuneArgs(token_type="default-service"),
     )
 
-    vault_aws_auth_client = vault.aws.AuthBackendClient(
+    vault_aws_auth_client = aws.AuthBackendClient(
         f"vault-aws-auth-backend-client-configuration-{env.value}",
         backend=vault_aws_auth.path,
     )
 
 # GitHub auth backend
-vault_github_auth = vault.github.AuthBackend(
+vault_github_auth = github.AuthBackend(
     "vault-github-auth-backend",
     organization="mitodl",
     description="GitHub Auth mount in a Vault server",
@@ -50,12 +50,26 @@ vault_github_auth = vault.github.AuthBackend(
     token_max_ttl=SIX_MONTHS,
 )
 
-policy_folder = sorted(
-    (Path(__file__).resolve().parent).parent.joinpath("policies/github/").rglob("*.hcl")
-)
-
-if "ci" or "qa" in stack_info.env_suffix:
-    for team in ["odl-engineering", "arbisoft-contractors"]:
-        vault_github_auth_team = vault.github.Team(
-            f"vault-github-auth-{team}", team=team, policies=["software-engineer"]
-        )
+if stack_info.env_suffix != "production":
+    policy_folder = (Path(__file__).resolve()).parent.parent.joinpath(
+        "policies/github/"
+    )
+    for hcl_file in policy_folder.iterdir():
+        if "software_engineer.hcl" in hcl_file.name:
+            software_engineer_policy_file = open(hcl_file).read()
+            software_engineer_policy = Policy(
+                "github-auth-software-engineer", policy=software_engineer_policy_file
+            )
+            for team in ["odl-engineering", "arbisoft-contractors"]:
+                vault_github_auth_team = github.Team(
+                    f"vault-github-auth-{team}",
+                    team=team,
+                    policies=["software-engineer"],
+                )
+        if "admin.hcl" in hcl_file.name:
+            devops_policy_file = open(hcl_file).read()
+            devops_policy = Policy("github-auth-devops", policy=devops_policy_file)
+            for team in ["devops"]:
+                vault_github_auth_team = github.Team(
+                    f"vault-github-auth-{team}", team=team, policies=["admin"]
+                )
