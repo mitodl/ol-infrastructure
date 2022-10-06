8632fb2952014c4264d84c3495e0f4b9a126a6fc
Tobias Macey <tmacey@mit.edu>
2022-10-06 12:16:19 -0400

    feat: Add a PyInfra component for the Traefik proxy
    We have a small mix of proxy services that we depend on, with Caddy being the newest
    addition. Unfortunately Caddy has proven to be fragile in some cases, and debugging is
    challenging due to the multiple supported configuration syntaxes that are not all fully
    documented. This adds a component to allow us to use Traefik in its place (particularly
    the automatic TLS management).

--- src/bilder/components/traefik/models.py
+++
@@ -1,6 +0,0 @@
-from ol_configuration_management.lib.model_helpers import OLBaseSettings
-
-
-class TraefikConfig(OLBaseSettings):
-    class Config:
-        env_prefix = "traefik_"
--- src/bilder/components/traefik/steps.py
+++ src/bilder/components/traefik/steps.py
@@ -1,41 +1,121 @@
+from io import StringIO
 from pathlib import Path
 
+import httpx
+from pyinfra import host
 from pyinfra.api import deploy
+from pyinfra.facts.server import LinuxName
 from pyinfra.operations import files, server, systemd
 
-from bilder.components.traefik.models import TraefikConfig
+from bilder.components.traefik.models.component import TraefikConfig
+from bilder.facts.system import DebianCpuArch, RedhatCpuArch
+from bilder.lib.linux_helpers import linux_family
 
 
 @deploy("Install traefik")
-def install_traefik(state=None, host=None):
-    server.shell(name="Sample operation", commands=["echo 'foobar'"])
+def install_traefik_binary(traefik_config: TraefikConfig):
+    server.user(
+        name="Create system user for Traefik",
+        user=traefik_config.user,
+        system=True,
+        shell="/bin/false",  # noqa: S604
+    )
+    if linux_family(host.get_fact(LinuxName)).lower == "debian":
+        cpu_arch = host.get_fact(DebianCpuArch)
+    elif linux_family(host.get_fact(LinuxName)).lower == "redhat":
+        cpu_arch = host.get_fact(RedhatCpuArch)
+    else:
+        cpu_arch = "amd64"
+    file_download = f"traefik_v{traefik_config.version}_linux_{cpu_arch}.tar.gz"
+    file_hashes = (
+        httpx.get(
+            "https://github.com/traefik/traefik/releases/download/v{0}/traefik_v{0}_checksums.txt".format(  # noqa: E501
+                traefik_config.version
+            ),
+            follow_redirects=True,
+        )
+        .read()
+        .decode("utf8")
+        .strip("\n")
+        .split("\n")
+    )
+    file_hash_map = {
+        file_hash.split()[1]: file_hash.split()[0] for file_hash in file_hashes
+    }
+    download_destination = "/tmp/traefik.tar.gz"  # noqa: S108
+    target_directory = "/usr/local/bin/"
+    download_binary = files.download(
+        name="Download Traefik archive",
+        src=f"https://github.com/traefik/traefik/releases/download/v{traefik_config.version}/{file_download}",  # noqa: E501
+        dest=download_destination,
+        sha256sum=file_hash_map[file_download],
+    )
+    server.shell(
+        name="Unpack Traefik",
+        commands=[f"tar -xvzf {download_destination} -C {target_directory}"],
+    )
+    files.file(
+        name="Ensure Traefik binary is executable",
+        path=str(Path(target_directory).joinpath("traefik")),
+        assume_present=download_binary.changed,
+        user=traefik_config.user,
+        group=traefik_config.group,
+        mode="755",
+    )
+    files.directory(
+        name="Ensure configuration directory for Traefik",
+        path=str(traefik_config.configuration_directory),
+        present=True,
+        user=traefik_config.user,
+        group=traefik_config.group,
+        recursive=True,
+    )
 
 
 @deploy("Configure traefik")
-def configure_traefik(traefik_config: TraefikConfig, state=None, host=None):
-    files.template(
-        name="Write a configuration file",
-        src=Path(__file__).parent.joinpath("templates/conf.ini.j2"),
-        dest="/etc/traefik/traefik.conf",
-        settings=traefik_config,
+def configure_traefik(traefik_config: TraefikConfig):
+    files.put(
+        name="Write Traefik static configuration file",
+        src=StringIO(
+            traefik_config.static_configuration.json(exclude_unset=True, indent=2)
+        ),
+        dest=str(
+            traefik_config.configuration_directory.joinpath(
+                traefik_config.static_configuration_file
+            )
+        ),
+        user=traefik_config.user,
     )
+    for fpath, file_config in (traefik_config.file_configurations or {}).items():
+        files.put(
+            name=f"Write Traefik dynamic configuration file {fpath}",
+            dest=str(traefik_config.configuration_directory.joinpath(fpath)),
+            src=StringIO(file_config.json(exclude_unset=True, indent=2)),
+            user=traefik_config.user,
+        )
 
 
 @deploy("Manage traefik service")
 def traefik_service(
     traefik_config: TraefikConfig,
-    state=None,
-    host=None,
     do_restart=False,
     do_reload=False,
+    start_immediately=False,
 ):
+    systemd_unit = files.template(
+        name="Create service definition for Traefik",
+        dest="/usr/lib/systemd/system/traefik.service",
+        src=str(
+            Path(__file__).resolve().parent.joinpath("templates", "traefik.service.j2")
+        ),
+        context=traefik_config,
+    )
     systemd.service(
         name="Enable traefik service",
         service="traefik",
-        running=True,
+        running=start_immediately,
         enabled=True,
         restarted=do_restart,
         reloaded=do_reload,
-        state=state,
-        host=host,
+        daemon_reload=systemd_unit.changed,
     )
