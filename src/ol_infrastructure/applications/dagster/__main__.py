de0a76a7391122761c11d10373b62febc5a4c008
Sar <sar.haidar@gmail.com>
2022-10-07 08:19:49 -0500

    Related to ol-data PR#356 (#1120)
    * Related to ol-data PR#356
    
    * Added Dagster environment def
    
    * Added Dagster environment def
    
    * Fix duplicate
    
    * Remove unnecessary volume
    
    * Added path

--- src/ol_infrastructure/applications/dagster/__main__.py
+++ src/ol_infrastructure/applications/dagster/__main__.py
@@ -597,6 +597,10 @@ dagster_instance = ec2.Instance(
                                 ),  # noqa: WPS355
                                 "owner": "root:root",
                             },
+                            {
+                                "path": "/etc/default/consul-template",
+                                "content": f"DAGSTER_ENVIRONMENT={stack_info.env_suffix}",
+                            },
                         ]
                     },
                     sort_keys=True,
