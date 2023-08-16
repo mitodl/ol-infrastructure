f82113d71ed5ffbd31f7f78d3039145e1a6b4139
Mike Davidson <41908054+Ardiea@users.noreply.github.com>
2023-08-16 14:05:25 -0400

    Md/mit open config r2 (#1689)
    * Move directory for mitopen
    
    * Created a postgres database for mitopen and associated vault auth. Fixed a few other issues.
    
    * Removing CI environment configuration items for mitopen.

--- src/ol_infrastructure/applications/mitopen/__main__.py
+++ src/ol_infrastructure/applications/mitopen/__main__.py
@@ -1,28 +1,32 @@
-from pulumi import Config, export, ResourceOptions
+from pulumi import Config, export, ResourceOptions, StackReference
 from pulumi.output import Output
-from pulumi_aws import iam, s3
+from pulumi_aws import iam, s3, ec2
 from pathlib import Path
 
 import json
 
 import pulumi_vault as vault
 
-from bridge.lib.magic_numbers import FIVE_MINUTES
+from bridge.lib.magic_numbers import FIVE_MINUTES, DEFAULT_POSTGRES_PORT
 from bridge.secrets.sops import read_yaml_secrets
 
+from ol_infrastructure.components.aws.database import OLPostgresDBConfig, OLAmazonDB
+from ol_infrastructure.components.services.vault import (
+    OLVaultDatabaseBackend,
+    OLVaultPostgresDatabaseConfig,
+)
 from ol_infrastructure.lib.aws.iam_helper import IAM_POLICY_VERSION, lint_iam_policy
 from ol_infrastructure.lib.ol_types import AWSBase
 from ol_infrastructure.lib.pulumi_helper import parse_stack
+from ol_infrastructure.lib.stack_defaults import defaults
 
 from ol_infrastructure.lib.vault import setup_vault_provider
 
-# Configuration items and initialziations
-if Config("vault_server").get("env_namespace"):
-    setup_vault_provider()
-
 setup_vault_provider()
-mit_open_config = Config("mit_open")
+mitopen_config = Config("mitopen")
 stack_info = parse_stack()
+network_stack = StackReference(f"infrastructure.aws.network.{stack_info.name}")
+apps_vpc = network_stack.require_output("applications_vpc")
 aws_config = AWSBase(
     tags={
         "OU": "mit-open",
@@ -109,7 +113,7 @@ open_policy_document = {
     "Statement": s3_bucket_permissions,
 }
 
-mit_open_iam_policy = iam.Policy(
+mitopen_iam_policy = iam.Policy(
     f"ol_mitopen_iam_permissions_{stack_info.env_suffix}",
     name=f"ol-mitopen-application-permissions-{stack_info.env_suffix}",
     path=f"/ol-applications/mitopen/{stack_info.env_suffix}/",
@@ -118,15 +122,15 @@ mit_open_iam_policy = iam.Policy(
     ),
 )
 
-mit_open_vault_iam_role = vault.aws.SecretBackendRole(
+mitopen_vault_iam_role = vault.aws.SecretBackendRole(
     f"ol-mitopen-iam-permissions-vault-policy-{stack_info.env_suffix}",
-    name=f"ol-mitopen-application-{stack_info.env_suffix}",
+    name="ol-mitopen-application",
     backend="aws-mitx",
     credential_type="iam_user",
-    policy_arns=[mit_open_iam_policy.arn],
+    policy_arns=[mitopen_iam_policy.arn],
 )
 
-mit_open_vault_mount = vault.Mount(
+mitopen_vault_mount = vault.Mount(
     f"ol-mitopen-configuration-secrets-mount-{stack_info.env_suffix}",
     path="secret-mitopen",
     type="kv-v2",
@@ -135,22 +139,75 @@ mit_open_vault_mount = vault.Mount(
     opts=ResourceOptions(delete_before_replace=True),
 )
 
-mit_open_vault_secrets = read_yaml_secrets(
+mitopen_vault_secrets = read_yaml_secrets(
     Path(f"mitopen/secrets.{stack_info.env_suffix}.yaml"),
 )
 
 vault.generic.Secret(
     f"ol-mitopen-configuration-secrets-{stack_info.env_suffix}",
-    path=mit_open_vault_mount.path.apply("{}/secrets".format),
-    data_json=json.dumps(mit_open_vault_secrets),
+    path=mitopen_vault_mount.path.apply("{}/secrets".format),
+    data_json=json.dumps(mitopen_vault_secrets),
 )
+
+mitopen_db_security_group = ec2.SecurityGroup(
+    f"ol-mitopen-db-access-{stack_info.env_suffix}",
+    description=f"Access control for the MIT Open application DB in {stack_info.name}",
+    ingress=[
+        ec2.SecurityGroupIngressArgs(
+            protocol="tcp",
+            from_port=DEFAULT_POSTGRES_PORT,
+            to_port=DEFAULT_POSTGRES_PORT,
+            cidr_blocks=["0.0.0.0/0"],
+            ipv6_cidr_blocks=["::/0"],
+            description="Allow access over the public internet from Heroku.",
+        )
+    ],
+    egress=[
+        ec2.SecurityGroupEgressArgs(
+            from_port=0,
+            to_port=0,
+            protocol="-1",
+            cidr_blocks=["0.0.0.0/32"],
+            ipv6_cidr_blocks=["::/0"],
+        )
+    ],
+    tags=aws_config.tags,
+    vpc_id=apps_vpc["id"],
+)
+
+rds_password = mitopen_config.require("db_password")
+rds_defaults = defaults(stack_info)["rds"]
+rds_defaults["instance_size"] = (
+    mitopen_config.get("db_instance_size") or rds_defaults["instance_size"]
+)
+mitopen_db_config = OLPostgresDBConfig(
+    instance_name=f"ol-mitopen-db-{stack_info.env_suffix}",
+    password=rds_password,
+    subnet_group_name=apps_vpc["rds_subnet"],
+    security_groups=[mitopen_db_security_group],
+    tags=aws_config.tags,
+    db_name="mitopen",
+    public_access=True,
+    **rds_defaults,
+)
+
+mitopen_db = OLAmazonDB(mitopen_db_config)
+
+mitopen_vault_backend_config = OLVaultPostgresDatabaseConfig(
+    db_name=mitopen_db_config.db_name,
+    mount_point=f"{mitopen_db_config.engine}-mitopen",
+    db_admin_username=mitopen_db_config.username,
+    db_admin_password=rds_password,
+    db_host=mitopen_db.db_instance.address,
+)
+mitopen_vault_backend = OLVaultDatabaseBackend(mitopen_vault_backend_config)
 
 export(
     "mitopen",
     {
-        "iam_policy": mit_open_iam_policy.arn,
+        "iam_policy": mitopen_iam_policy.arn,
         "vault_iam_role": Output.all(
-            mit_open_vault_iam_role.backend, mit_open_vault_iam_role.name
+            mitopen_vault_iam_role.backend, mitopen_vault_iam_role.name
         ).apply(lambda role: f"{role[0]}/roles/{role[1]}"),
     },
 )
