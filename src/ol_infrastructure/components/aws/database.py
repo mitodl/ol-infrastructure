50e84068bfa8c8e8f27f92f0f95b1fe123757986
Tobias Macey <tmacey@mit.edu>
2023-08-21 14:56:55 -0400

    fix: Address type lint failure

--- src/ol_infrastructure/components/aws/database.py
+++ src/ol_infrastructure/components/aws/database.py
@@ -339,4 +339,7 @@ class OLAmazonDB(pulumi.ComponentResource):
                 },
             },
         }
-        return dict(**global_profiles, **(monitoring_profiles[profile_name]))
+        return dict(
+            **global_profiles,
+            **(monitoring_profiles[profile_name]),  # type:ignore[dict-item]
+        )
