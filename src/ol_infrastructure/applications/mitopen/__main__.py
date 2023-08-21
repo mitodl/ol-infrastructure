1b8b30fbc2529ad8144019607dbbd595fa70d112
Mike Davidson <41908054+Ardiea@users.noreply.github.com>
2023-08-21 14:57:32 -0400

    Setting default password encryption to md5 for mitopen rds instance. (#1703)

--- src/ol_infrastructure/applications/mitopen/__main__.py
+++ src/ol_infrastructure/applications/mitopen/__main__.py
@@ -187,6 +187,7 @@ mitopen_db_config = OLPostgresDBConfig(
     security_groups=[mitopen_db_security_group],
     tags=aws_config.tags,
     db_name="mitopen",
+    parameter_overrides=[{"name": "password_encryption", "value": "md5"}],
     public_access=True,
     **rds_defaults,
 )
