1267fa0125c3ab9be923cc056f86d2b4737116b0
Chris Patti <feoh@feoh.org>
2023-08-23 10:24:39 -0400

    Revert "fix: Put health check path for Airbyte in the right place."
    This reverts commit 3443cd580ee04aa30f5011688e7d647dea639498.

--- src/ol_infrastructure/applications/airbyte/__main__.py
+++ src/ol_infrastructure/applications/airbyte/__main__.py
@@ -470,6 +470,7 @@ consul.Keys(
 #     Web Node EC2 Deployment     #
 ###################################
 lb_config = OLLoadBalancerConfig(
+    health_check_path="/ping",
     subnets=target_vpc["subnet_ids"],
     security_groups=[airbyte_server_security_group],
     tags=aws_config.merged_tags({"Name": airbyte_server_tag}),
@@ -479,7 +480,7 @@ tg_config = OLTargetGroupConfig(
     vpc_id=vpc_id,
     health_check_interval=60,
     health_check_matcher="200-399",
-    health_check_path="/ping",
+    health_check_path="/",
     tags=aws_config.merged_tags({"Name": airbyte_server_tag}),
 )
 
