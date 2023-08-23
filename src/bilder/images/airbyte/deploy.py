4f75e3322f11bf5e53444bd3a54ef962517a1bd4
Chris Patti <feoh@feoh.org>
2023-08-23 10:26:27 -0400

    Revert "config: Add ping endpoint for Airbyte to be used by ALB healthcheck"
    This reverts commit 2d8614a72646cc529ee3a60cd8365774614c7584.

--- src/bilder/images/airbyte/deploy.py
+++ src/bilder/images/airbyte/deploy.py
@@ -89,7 +89,6 @@ traefik_static_config = traefik_static.TraefikStaticConfig(
     entry_points={
         "https": traefik_static.EntryPoints(address=":443"),
     },
-    ping=traefik_static.Ping(entryPoint="https"),
 )
 traefik_config = TraefikConfig(
     static_configuration=traefik_static_config, version=VERSIONS["traefik"]
--- src/ol_infrastructure/applications/airbyte/__main__.py
+++ src/ol_infrastructure/applications/airbyte/__main__.py
@@ -470,7 +470,6 @@ consul.Keys(
 #     Web Node EC2 Deployment     #
 ###################################
 lb_config = OLLoadBalancerConfig(
-    health_check_path="/ping",
     subnets=target_vpc["subnet_ids"],
     security_groups=[airbyte_server_security_group],
     tags=aws_config.merged_tags({"Name": airbyte_server_tag}),
