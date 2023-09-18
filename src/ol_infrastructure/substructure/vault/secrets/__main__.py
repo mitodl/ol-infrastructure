b73da32484b4cadf0578a6c4b106936e3d96ba10
sar <sar.haidar@gmail.com>
2023-09-18 13:41:03 -0500

    Use kv2 call to secret-global

--- src/ol_infrastructure/substructure/vault/secrets/__main__.py
+++ src/ol_infrastructure/substructure/vault/secrets/__main__.py
@@ -5,18 +5,21 @@ from pathlib import Path
 import pulumi_vault as vault
 from bridge.secrets.sops import read_yaml_secrets
 from ol_infrastructure.lib.pulumi_helper import parse_stack
-from pulumi import ResourceOptions
+from ol_infrastructure.lib.vault import setup_vault_provider
+from pulumi import Config, ResourceOptions
 
 stack_info = parse_stack()
 global_vault_secrets = read_yaml_secrets(
     Path(f"vault/secrets.{stack_info.env_suffix}.yaml")
 )
+if Config("vault_server").get("env_namespace"):
+    setup_vault_provider()
 
 # Create the secret mount used for storing global secrets
 global_vault_mount = vault.Mount(
     "global-secrets-mount",
     path="secret-global",
-    type="kv-v2",
+    type="kv",
     options={"version": 2},
     description="Storage of global secrets used across our applications.",
     opts=ResourceOptions(delete_before_replace=True),
@@ -24,8 +27,9 @@ global_vault_mount = vault.Mount(
 
 for key, data in global_vault_secrets.items():
     secret_path = partial("{1}/{0}".format, key)
-    vault.generic.Secret(
+    vault.kv.SecretV2(
         f"global-vault-secrets-{key}",
-        path=global_vault_mount.path.apply(secret_path),
+        name=key,
+        mount=global_vault_mount,
         data_json=json.dumps(data),
     )
