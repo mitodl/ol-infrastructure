6814a7aba72c64ec46688afdcfe9092ec6988093
Ardiea <mas48@mit.edu>
2023-09-18 10:17:47 -0400

    second round commit for ecs cluster.

--- src/ol_infrastructure/infrastructure/aws/ecs/__main__.py
+++ src/ol_infrastructure/infrastructure/aws/ecs/__main__.py
@@ -4,7 +4,7 @@ import json
 import pulumi
 import yaml
 from pulumi import ResourceOptions
-from pulumi_aws import autoscaling, ec2, ecs, ssm
+from pulumi_aws import autoscaling, ec2, ecs, iam, ssm
 
 from ol_infrastructure.lib.ol_types import AWSBase
 from ol_infrastructure.lib.pulumi_helper import parse_stack
@@ -128,7 +128,115 @@ cluster_capacity_provider = ecs.ClusterCapacityProviders(
     opts=ResourceOptions(depends_on=[capacity_provider, ecs_cluster]),
 )
 
+if cluster_config.get_bool("traefik_enabled") or True:
+    traefik_task_policy_document = ecs_cluster.arn.apply(
+        lambda cluster_arn: json.dumps(  # noqa: ARG005
+            {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Sid": "TraefikECSGetClusterInfo",
+                        "Effect": "Allow",
+                        "Action": [
+                            "ecs:ListClusters",
+                            "ecs:DescribeClusters",
+                        ],
+                        "Resource": ["{cluster_arn}"],
+                    },
+                    {
+                        "Sid": "TraefikECSReadClusterWorkload",
+                        "Effect": "Allow",
+                        "Action": [
+                            "ecs:ListTasks",
+                            "ecs:DescribeTasks",
+                            "ecs:DescribeContainerInstances",
+                            "ecs:DescribeTaskDefinition",
+                        ],
+                        "Resource": ["*"],
+                        "Condition": {"ArnEquals": {"ecs:cluster": "{cluster_arn}"}},
+                    },
+                ],
+            }
+        )
+    )
 
+    traefik_task_policy = iam.Policy(
+        f"{environment_name}-ecs-traefik-task-policy",
+        name=f"{environment_name}-ecs-traefik-task-policy",
+        path=f"/ol-operations/ecs/{environment_name}/",
+        policy=traefik_task_policy_document,
+        description=f"Allows the traefik tasks in {cluster_name}-ecs-cluster"
+        " to discover tasks on ECS clusters.",
+    )
+
+    refresh_interval = cluster_config.get_int("traefik_refresh_interval_seconds") or 15
+    log_level = cluster_config.get("traefik_log_level") or "WARN"
+    traefik_task_definition = ecs.TaskDefinition(
+        f"{environment_name}-traefik-ingress-task-definition",
+        family=f"{environment_name}",
+        # execution_role_arn=,
+        # task_role_arn=,
+        container_definitions=json.dumps(
+            [
+                {
+                    "name": f"{environment_name}-traefik",
+                    "image": "traefik:v2.10.4",
+                    "essential": True,
+                    "command": [
+                        "--api.insecure=true",
+                        "--entryPoints.http.address=:80",
+                        "--entryPoints.http.http.redirections.entryPoint.to=https",
+                        "--entryPoints.http.http.redirections.entryPoint.scheme=https",
+                        "--entryPoints.https.address=:443",
+                        "--providers.ecs=true",
+                        "--providers.ecs.exposedByDefault=false",
+                        "--providers.ecs.autoDiscoverClusters=true",
+                        "--providers.ecs.clusters={cluster_name}",
+                        f"--providers.ecs.refreshSeconds={refresh_interval}",
+                        f"--log.level={log_level}",
+                        "--accesslog=true",
+                    ],
+                    "port_mappings": [
+                        {
+                            "name": f"{cluster_name}-traefik-80-tcp",
+                            "containerPort": 80,
+                            "hostPort": 80,
+                            "protocol": "tcp",
+                            "appProtocol": "http",
+                        },
+                        {
+                            "name": f"{cluster_name}-traefik-443-tcp",
+                            "containerPort": 443,
+                            "hostPort": 443,
+                            "protocol": "tcp",
+                            "appProtocol": "http",
+                        },
+                        {
+                            "name": f"{cluster_name}-traefik-8080-tcp",
+                            "containerPort": 8080,
+                            "hostPort": 8080,
+                            "protocol": "tcp",
+                            "appProtocol": "http",
+                        },
+                    ],
+                    "logConfiguration": {
+                        "logDriver": "awslogs",
+                        "options": {
+                            "awslogs-create-group": "true",
+                            "awslogs-group": f"/ecs/{cluster_name}/traefik",
+                            "awslogs-region": "us-east-1",
+                            "awslogs-stream-prefix": "ecs",
+                        },
+                    },
+                },
+            ]
+        ),
+        cpu="1024",
+        memory="1024",
+        network_mode="bridge",
+        placement_constraints=[],
+        requires_compatibilities=["EC2"],
+    )
 #
 #
 #
