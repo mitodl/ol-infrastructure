041d72eee5f810952c00a991014dce2b5f5a8fa6
Tobias Macey <tmacey@mit.edu>
2023-09-28 11:44:56 -0400

    config: Fix path settings for mitopen Pulumi pipeline

--- src/ol_concourse/pipelines/infrastructure/mit_open/pipeline.py
+++ src/ol_concourse/pipelines/infrastructure/mit_open/pipeline.py
@@ -4,30 +4,30 @@ from ol_concourse.lib.models.pipeline import Identifier, Pipeline
 from ol_concourse.lib.resources import git_repo
 from ol_concourse.pipelines.constants import PULUMI_CODE_PATH
 
-mit_open_pulumi_code = git_repo(
-    name=Identifier("ol-infrastructure-pulumi-mit_open"),
+mitopen_pulumi_code = git_repo(
+    name=Identifier("ol-infrastructure-pulumi-mitopen"),
     uri="https://github.com/mitodl/ol-infrastructure",
     branch="main",
     paths=[
-        "src/ol_infrastructure/applications/mit_open/",
+        "src/ol_infrastructure/applications/mitopen/",
         "src/ol_infrastructure/lib/",
         "src/bridge/lib/",
     ],
 )
 
 pulumi_jobs = pulumi_jobs_chain(
-    pulumi_code=mit_open_pulumi_code,
+    pulumi_code=mitopen_pulumi_code,
     stack_names=[
-        "applications.mit_open.QA",
-        "applications.mit_open.Production",
+        "applications.mitopen.QA",
+        "applications.mitopen.Production",
     ],
-    project_name="ol-infrastructure-mit_open-application",
-    project_source_path=PULUMI_CODE_PATH.joinpath("applications/mit_open/"),
+    project_name="ol-infrastructure-mitopen-application",
+    project_source_path=PULUMI_CODE_PATH.joinpath("applications/mitopen/"),
 )
 
 mm_fragment = PipelineFragment.combine_fragments(pulumi_jobs)
-mit_open_pipeline = Pipeline(
-    resources=[*mm_fragment.resources, mit_open_pulumi_code],
+mitopen_pipeline = Pipeline(
+    resources=[*mm_fragment.resources, mitopen_pulumi_code],
     resource_types=mm_fragment.resource_types,
     jobs=mm_fragment.jobs,
 )
@@ -36,7 +36,7 @@ if __name__ == "__main__":
     import sys
 
     with open("definition.json", "w") as definition:  # noqa: PTH123
-        definition.write(mit_open_pipeline.json(indent=2))
-    sys.stdout.write(mit_open_pipeline.json(indent=2))
+        definition.write(mitopen_pipeline.json(indent=2))
+    sys.stdout.write(mitopen_pipeline.json(indent=2))
     sys.stdout.write("\n")
-    sys.stdout.write("fly -t pr-inf set-pipeline -p pulumi-mit_open -c definition.json")
+    sys.stdout.write("fly -t pr-inf set-pipeline -p pulumi-mitopen -c definition.json")
