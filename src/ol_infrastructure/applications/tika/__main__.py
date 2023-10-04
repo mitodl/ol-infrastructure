aa834a42a1f6205edc1be412d0b4a15792ab6a9b
Chris Patti <feoh@feoh.org>
2023-10-04 16:41:58 -0400

    Fix - Tika is a compute intensive application not suited to burst instances. Change instance type to general purpose large

--- src/ol_infrastructure/applications/tika/__main__.py
+++ src/ol_infrastructure/applications/tika/__main__.py
@@ -185,7 +185,8 @@ tag_specs = [
 lt_config = OLLaunchTemplateConfig(
     block_device_mappings=block_device_mappings,
     image_id=tika_server_ami.id,
-    instance_type=tika_config.get("instance_type") or InstanceTypes.burstable_medium,
+    instance_type=tika_config.get("instance_type")
+    or InstanceTypes.general_purpose_large,
     instance_profile_arn=tika_server_instance_profile.arn,
     security_groups=[
         tika_server_security_group,
