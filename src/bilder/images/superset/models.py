387a02b86ee764d4ecb27fb06f4897fefe309611
Tobias Macey <tmacey@mit.edu>
2023-12-12 11:35:19 -0500

    chore: Remove unused file

--- src/bilder/images/superset/models.py
+++
@@ -1,6 +0,0 @@
-from ol_configuration_management.lib.model_helpers import OLBaseSettings
-from pydantic_settings import SettingsConfigDict
-
-
-class SupersetConfig(OLBaseSettings):
-    model_config = SettingsConfigDict(env_prefix="superset_")
