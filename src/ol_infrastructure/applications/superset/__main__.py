a0fe4b1c26f31e74f70381f32621146e3f81266b
Tobias Macey <tmacey@mit.edu>
2023-12-12 11:10:04 -0500

    config: Set a warmup period for Superset web nodes

--- src/ol_infrastructure/applications/superset/__main__.py
+++ src/ol_infrastructure/applications/superset/__main__.py
@@ -587,6 +587,7 @@ superset_web_auto_scale_config = superset_config.get_object("web_auto_scale") or
 superset_web_asg_config = OLAutoScaleGroupConfig(
     asg_name=f"superset-web-{superset_env}",
     aws_config=aws_config,
+    health_check_grace_period=60,
     desired_size=superset_web_auto_scale_config["desired"],
     min_size=superset_web_auto_scale_config["min"],
     max_size=superset_web_auto_scale_config["max"],
