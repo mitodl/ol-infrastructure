4378302b5dbccd4be90434ccbe73552c371a05ef
Chris Patti <feoh@feoh.org>
2023-12-20 09:35:26 -0500

    Update src/ol_infrastructure/applications/ocw_site/__main__.py
    Co-authored-by: Tobias Macey <tmacey@mit.edu>

--- src/ol_infrastructure/applications/ocw_site/__main__.py
+++ src/ol_infrastructure/applications/ocw_site/__main__.py
@@ -122,11 +122,10 @@ s3.BucketPolicy(
             "Statement": [
                 {
                     "Effect": "Allow",
-                    "Principal": {"AWS": "*"},
+                    "Principal": "*",
                     "Action": "s3:GetObject",
                     "Resource": [
-                        f"arn:aws:s3:::{draft_bucket_name}/images/*",
-                        f"arn:aws:s3:::{draft_bucket_name}/resumes/*",
+                        f"{draft_bucket_arn}/*",
                     ],
                 }
             ],
