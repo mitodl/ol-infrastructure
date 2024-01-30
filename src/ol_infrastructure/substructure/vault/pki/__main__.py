625a6a078aa889637180181698bdc235ecf90187
Tobias Macey <tmacey@mit.edu>
2024-01-30 12:30:06 -0500

    config: Add back explicit dependency link on intermediate root CA for PKI

--- src/ol_infrastructure/substructure/vault/pki/__main__.py
+++ src/ol_infrastructure/substructure/vault/pki/__main__.py
@@ -7,7 +7,7 @@ from ol_infrastructure.components.services.vault import (
 from ol_infrastructure.lib.ol_types import BusinessUnit
 from ol_infrastructure.lib.pulumi_helper import parse_stack
 from ol_infrastructure.lib.vault import setup_vault_provider
-from pulumi import Config, StackReference, export
+from pulumi import Config, ResourceOptions, StackReference, export
 
 SIX_MONTHS = 60 * 60 * 24 * 30 * 6
 TWELVE_MONTHS = 60 * 60 * 24 * 30 * 12
@@ -58,6 +58,7 @@ for business_unit in BusinessUnit:
 
     pki_intermediate_env = OLVaultPKIIntermediateEnvBackend(
         backend_config=pki_intermediate_env_config,
+        opts=ResourceOptions(depends_on=[pki_intermediate_ca]),
     )
 
     pki_intermediate_env_export_struct = {
