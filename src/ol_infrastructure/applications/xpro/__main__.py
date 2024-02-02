e4e5d180ff6a411b9530da7d1608aa28d69e3b5c
Ardiea <mas48@mit.edu>
2024-02-02 10:35:34 -0500

    Migrating xpro heroku app configuration from salt to pulumi.

--- src/ol_infrastructure/applications/xpro/__main__.py
+++ src/ol_infrastructure/applications/xpro/__main__.py
@@ -9,9 +9,10 @@ import json
 from pathlib import Path
 
 import pulumi_vault as vault
+import pulumiverse_heroku as heroku
 from bridge.lib.magic_numbers import DEFAULT_POSTGRES_PORT
 from bridge.secrets.sops import read_yaml_secrets
-from pulumi import Config, StackReference, export
+from pulumi import Config, InvokeOptions, StackReference, export
 from pulumi_aws import ec2, iam, s3
 
 from ol_infrastructure.components.aws.database import OLAmazonDB, OLPostgresDBConfig
@@ -20,14 +21,20 @@ from ol_infrastructure.components.services.vault import (
     OLVaultPostgresDatabaseConfig,
 )
 from ol_infrastructure.lib.aws.iam_helper import lint_iam_policy
+from ol_infrastructure.lib.heroku import setup_heroku_provider
 from ol_infrastructure.lib.ol_types import AWSBase
 from ol_infrastructure.lib.pulumi_helper import parse_stack
 from ol_infrastructure.lib.stack_defaults import defaults
 from ol_infrastructure.lib.vault import setup_vault_provider
 
 if Config("vault_server").get("env_namespace"):
-    setup_vault_provider()
+    setup_vault_provider(skip_child_token=True)
+setup_heroku_provider()
+
 xpro_config = Config("xpro")
+heroku_config = Config("heroku")
+heroku_app_config = Config("heroku_app")
+
 stack_info = parse_stack()
 network_stack = StackReference(f"infrastructure.aws.network.{stack_info.name}")
 apps_vpc = network_stack.require_output("applications_vpc")
@@ -215,6 +222,174 @@ for key, data in xpro_vault_secrets.items():
         path=f"secret-xpro/{key}",
         data_json=json.dumps(data),
     )
+
+# Heroku App configuration
+
+# Values that are generally unchanging across environments
+heroku_vars = {
+    "CRON_COURSERUN_SYNC_HOURS": "*",
+    "CYBERSOURCE_MERCHANT_ID": "mit_odl_xpro",
+    "CERTIFICATE_CREATION_DELAY_IN_HOURS": 48,
+    "FEATURE_COUPON_SHEETS": "True",
+    "FEATURE_COUPON_SHEETS_TRACK_REQUESTER": "True",
+    "FEATURE_COURSE_DROPDOWN": "True",
+    "FEATURE_WEBINARS": "True",
+    "FEATURE_ENABLE_BLOG": "True",
+    "FEATURE_ENABLE_TAXES_DISPLAY": "True",
+    "HUBSPOT_PIPELINE_ID": "75e28846-ad0d-4be2-a027-5e1da6590b98",
+    "MITOL_DIGITAL_CREDENTIALS_AUTH_TYPE": "code",
+    "MITOL_DIGITAL_CREDENTIALS_DEEP_LINK_URL": "dccrequest://request",
+    "MITXPRO_ADMIN_EMAIL": "cuddle-bunnies@mit.edu",
+    "MITXPRO_DB_CONN_MAX_AGE": 0,
+    "MITXPRO_DB_DISABLE_SSL": "True",
+    "MITXPRO_EMAIL_TLS": "True",
+    "MITXPRO_FROM_EMAIL": "MIT xPRO <support@xpro.mit.edu>",
+    "MITXPRO_OAUTH_PROVIDER": "mitxpro-oauth2",
+    "MITXPRO_REPLY_TO_ADDRESS": "MIT xPRO <support@xpro.mit.edu>",
+    "MITXPRO_SECURE_SSL_REDIRECT": "True",
+    "MITXPRO_USE_S3": "True",
+    "NODE_MODULES_CACHE": "False",
+    "OAUTH2_PROVIDER_ALLOWED_REDIRECT_URI_SCHEMES": "http,https,dccrequest",
+    # This can be removed once PR#1314 is in production,
+    "OPENEDX_OAUTH_APP_NAME": "edx-oauth-app",
+    # This replaces OPENEDX_GRADES_API_TOKEN and is
+    # tied to xpro-grades-api user in openedx,
+    "OPENEDX_SERVICE_WORKER_USERNAME": "xpro-service-worker-api",
+    "PGBOUNCER_DEFAULT_POOL_SIZE": 50,
+    "PGBOUNCER_MIN_POOL_SIZE": 5,
+    "SHEETS_DATE_TIMEZONE": "America/New_York",
+    "SHEETS_TASK_OFFSET": "120",
+    "SHOW_UNREDEEMED_COUPON_ON_DASHBOARD": "True",
+    "SITE_NAME": "MIT xPRO",
+    "USE_X_FORWARDED_HOST": "True",
+}
+
+# Combine var source above with values explictly defined in pulumi configuration file
+heroku_vars.update(**heroku_app_config.get_object("vars"))
+
+# Finally, populate a map of vars that contain secrets
+auth_aws_mitx_creds_xpro_app = vault.generic.get_secret_output(
+    path="aws-mitx/creds/xpro-app",
+    with_lease_start_time=False,
+    opts=InvokeOptions(parent=xpro_vault_backend_role),
+)
+auth_postgres_xpro_creds_app = vault.generic.get_secret_output(
+    path="postgres-xpro/creds/app",
+    with_lease_start_time=False,
+    opts=InvokeOptions(parent=xpro_vault_backend_role),
+)
+secret_operations_global_mailgun_api_key = vault.generic.get_secret_output(
+    path="secret-operations/global/mailgun-api-key",
+    opts=InvokeOptions(parent=xpro_vault_backend_role),
+)
+
+sensitive_heroku_vars = {
+    # Secrets that can be source locally from SOPS
+    "COUPON_REQUEST_SHEET_ID": xpro_vault_secrets["google-sheets"]["sheet_id"],
+    "CYBERSOURCE_ACCESS_KEY": xpro_vault_secrets["cybersource"]["access_key"],
+    "CYBERSOURCE_INQUIRY_LOG_NACL_ENCRYPTION_KEY": xpro_vault_secrets["cybersource"][
+        "inquiry_log_nacl_encryption_key"
+    ],
+    "CYBERSOURCE_PROFILE_ID": xpro_vault_secrets["cybersource"]["profile_id"],
+    "CYBERSOURCE_SECURITY_KEY": xpro_vault_secrets["cybersource"]["security_key"],
+    "CYBERSOURCE_TRANSACTION_KEY": xpro_vault_secrets["cybersource"]["transaction_key"],
+    "DEFERRAL_REQUEST_WORKSHEET_ID": xpro_vault_secrets["google-sheets"][
+        "deferral_worksheet_id"
+    ],
+    "DIGITAL_CREDENTIALS_ISSUER_ID": xpro_vault_secrets["digital-credentials"][
+        "issuer_id"
+    ],
+    "DIGITAL_CREDENTIALS_OAUTH2_CLIENT_ID": xpro_vault_secrets["digital-credentials"][
+        "oauth2_client_id"
+    ],
+    "DIGITAL_CREDENTIALS_VERIFICATION_METHOD": xpro_vault_secrets[
+        "digital-credentials"
+    ]["verification_method"],
+    "DRIVE_OUTPUT_FOLDER_ID": xpro_vault_secrets["google-sheets"]["folder_id"],
+    "DRIVE_SERVICE_ACCOUNT_CREDS": xpro_vault_secrets["google-sheets"][
+        "service_account_creds"
+    ],
+    "DRIVE_SHARED_ID": xpro_vault_secrets["google-sheets"]["drive_shared_id"],
+    "ENROLLMENT_CHANGE_SHEET_ID": xpro_vault_secrets["google-sheets"][
+        "enroll_change_sheet_id"
+    ],
+    "HIREFIRE_TOKEN": xpro_vault_secrets["hirefire"]["token"],
+    "MITOL_DIGITAL_CREDENTIALS_HMAC_SECRET": xpro_vault_secrets["digital-credentials"][
+        "hmac_secret"
+    ],
+    "MITOL_DIGITAL_CREDENTIALS_VERIFY_SERVICE_BASE_URL": xpro_vault_secrets[
+        "digital-credentials"
+    ]["sign_and_verify_url"],
+    "MITOL_HUBSPOT_API_PRIVATE_TOKEN": xpro_vault_secrets["hubspot"][
+        "api_private_token"
+    ],
+    "MITXPRO_EMAIL_HOST": xpro_vault_secrets["smtp"]["relay_host"],
+    "MITXPRO_EMAIL_PASSWORD": xpro_vault_secrets["smtp"]["relay_password"],
+    "MITXPRO_EMAIL_PORT": xpro_vault_secrets["smtp"]["relay_port"],
+    "MITXPRO_EMAIL_USER": xpro_vault_secrets["smtp"]["relay_username"],
+    "MITXPRO_REGISTRATION_ACCESS_TOKEN": xpro_vault_secrets["openedx"][
+        "registration_access_token"
+    ],
+    "MITXPRO_SUPPORT_EMAIL": xpro_vault_secrets["smtp"]["support_email"],
+    "OPENEDX_API_CLIENT_ID": xpro_vault_secrets["openedx-api-client"]["client_id"],
+    "OPENEDX_API_CLIENT_SECRET": xpro_vault_secrets["openedx-api-client"][
+        "client_secret"
+    ],
+    "OPENEDX_API_KEY": xpro_vault_secrets["openedx"]["edxapp_api_key"],
+    "OPENEDX_GRADES_API_TOKEN": xpro_vault_secrets["openedx"]["grades_api_token"],
+    "OPENEDX_SERVICE_WORKER_API_TOKEN": xpro_vault_secrets["openedx"][
+        "service_worker_api_token"
+    ],
+    "RECAPTCHA_SECRET_KEY": xpro_vault_secrets["recaptcha"]["secret_key"],
+    "RECAPTCHA_SITE_KEY": xpro_vault_secrets["recaptcha"]["site_key"],
+    "REFUND_REQUEST_WORKSHEET_ID": xpro_vault_secrets["google-sheets"][
+        "refund_worksheet_id"
+    ],
+    "SECRET_KEY": xpro_vault_secrets["django"]["secret-key"],
+    "SENTRY_DSN": xpro_vault_secrets["sentry"]["dsn"],
+    "SHEETS_ADMIN_EMAILS": xpro_vault_secrets["google-sheets"]["admin_emails"],
+    "STATUS_TOKEN": xpro_vault_secrets["django"]["status-token"],
+    "VOUCHER_DOMESTIC_AMOUNT_KEY": xpro_vault_secrets["voucher-domestic"]["amount_key"],
+    "VOUCHER_DOMESTIC_COURSE_KEY": xpro_vault_secrets["voucher-domestic"]["course_key"],
+    "VOUCHER_DOMESTIC_CREDITS_KEY": xpro_vault_secrets["voucher-domestic"][
+        "credits_key"
+    ],
+    "VOUCHER_DOMESTIC_DATES_KEY": xpro_vault_secrets["voucher-domestic"]["dates_key"],
+    "VOUCHER_DOMESTIC_DATE_KEY": xpro_vault_secrets["voucher-domestic"]["date_key"],
+    "VOUCHER_DOMESTIC_EMPLOYEE_ID_KEY": xpro_vault_secrets["voucher-domestic"][
+        "employee_id_key"
+    ],
+    "VOUCHER_DOMESTIC_EMPLOYEE_KEY": xpro_vault_secrets["voucher-domestic"][
+        "employee_key"
+    ],
+    "VOUCHER_DOMESTIC_KEY": xpro_vault_secrets["voucher-domestic"]["key"],
+    # Auth secrets that require something more involved
+    "AWS_ACCESS_KEY_ID": auth_aws_mitx_creds_xpro_app.data.apply(
+        lambda data: "{}".format(data["access_key"])
+    ),
+    "AWS_SECRET_ACCESS_KEY": auth_aws_mitx_creds_xpro_app.data.apply(
+        lambda data: "{}".format(data["secret_key"])
+    ),
+    "DATABASE_URL": auth_postgres_xpro_creds_app.data.apply(
+        lambda data: "postgres://{}:{}@xpro-db-applications-{}.cbnm7ajau6mi.us-east-1.rds.amazonaws.com:5432/xpro".format(
+            data["username"], data["password"], stack_info.name.lower()
+        )
+    ),
+    # Static secrets that require something more involved
+    "MAILGUN_KEY": secret_operations_global_mailgun_api_key.data.apply(
+        lambda data: "{}".format(data["value"])
+    ),
+}
+
+
+# Put it all together into a ConfigAssociation resource
+heroku_app_id = heroku_config.require("app_id")
+xpro_heroku_configassociation = heroku.app.ConfigAssociation(
+    f"xpro-{stack_info.env_suffix}-heroku-configassociation",
+    app_id=heroku_app_id,
+    sensitive_vars=sensitive_heroku_vars,
+    vars=heroku_vars,
+)
 
 export(
     "xpro_app",
