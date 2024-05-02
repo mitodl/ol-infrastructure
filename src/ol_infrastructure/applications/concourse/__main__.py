b485dfcb5df60026f406ae691ba38de66b7549a5
Tobias Macey <tmacey@mit.edu>
2024-05-02 11:01:38 -0400

    config,version: Remove DB version override for Concourse
    Postgres 12.14 is no longer in the supported list. Removed explicit specifier to allow
    default value to be applied.

--- src/ol_infrastructure/applications/concourse/__main__.py
+++ src/ol_infrastructure/applications/concourse/__main__.py
@@ -391,7 +391,6 @@ concourse_db_config = OLPostgresDBConfig(
     security_groups=[concourse_db_security_group],
     tags=aws_config.tags,
     db_name="concourse",
-    engine_version="12.14",
     **rds_defaults,
 )
 concourse_db = OLAmazonDB(concourse_db_config)
