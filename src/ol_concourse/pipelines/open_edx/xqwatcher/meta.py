286815dc680474d7d01fbf2bc0571a9a1599b746
Ardiea <mas48@mit.edu>
2024-05-29 08:25:56 -0400

    fixing pipeline typo.

--- src/ol_concourse/pipelines/open_edx/xqwatcher/meta.py
+++ src/ol_concourse/pipelines/open_edx/xqwatcher/meta.py
@@ -37,7 +37,9 @@ def build_meta_job(release_name):
         pipeline_team = "main"
         pipeline_id = "self"
     else:
-        pipeline_definition_path = "src/ol_concourse/pipelines/open_edx/xqwatcher/docker_packer_pulumi_pipeline.py"  # noqa: E501
+        pipeline_definition_path = (
+            "src/ol_concourse/pipelines/open_edx/xqwatcher/packer_pulumi_pipeline.py"
+        )
         pipeline_team = "infrastructure"
         pipeline_id = f"docker-packer-pulumi-xqwatcher-{release_name}"
     return Job(
