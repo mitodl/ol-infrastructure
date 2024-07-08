4953bd2216ca081e5e810c81ba3033a2106f8d71
Tobias Macey <tmacey@mit.edu>
2024-07-08 11:59:11 -0400

    fix: Don't try to import DNS zones for newly created zones

--- src/ol_infrastructure/lib/aws/route53_helper.py
+++ src/ol_infrastructure/lib/aws/route53_helper.py
@@ -21,24 +21,27 @@ def zone_opts(domain: str) -> pulumi.ResourceOptions:
     :rtype: pulumi.ResourceOptions
     """
     zone = route53_client.list_hosted_zones_by_name(DNSName=domain)["HostedZones"][0]
-    zone_id = zone["Id"].split("/")[
-        -1
-    ]  # 'Id' attribute is of the form /hostedzone/<ZONE_ID>
-    tags = route53_client.list_tags_for_resource(
-        ResourceType="hostedzone", ResourceId=zone_id
-    )
-    if "pulumi_managed" in {tag["Key"] for tag in tags["ResourceTagSet"]["Tags"]}:
-        opts = pulumi.ResourceOptions()
-    else:
-        opts = pulumi.ResourceOptions(
-            import_=zone_id, ignore_changes=["tags", "comment"]
+    if zone["Name"] == domain:
+        zone_id = zone["Id"].split("/")[
+            -1
+        ]  # 'Id' attribute is of the form /hostedzone/<ZONE_ID>
+        tags = route53_client.list_tags_for_resource(
+            ResourceType="hostedzone", ResourceId=zone_id
         )
-        if not pulumi.runtime.is_dry_run():
-            route53_client.change_tags_for_resource(
-                ResourceType="hostedzone",
-                ResourceId=zone_id,
-                AddTags=[{"Key": "pulumi_managed", "Value": "true"}],
+        if "pulumi_managed" in {tag["Key"] for tag in tags["ResourceTagSet"]["Tags"]}:
+            opts = pulumi.ResourceOptions()
+        else:
+            opts = pulumi.ResourceOptions(
+                import_=zone_id, ignore_changes=["tags", "comment"]
             )
+            if not pulumi.runtime.is_dry_run():
+                route53_client.change_tags_for_resource(
+                    ResourceType="hostedzone",
+                    ResourceId=zone_id,
+                    AddTags=[{"Key": "pulumi_managed", "Value": "true"}],
+                )
+    else:
+        opts = pulumi.ResourceOptions()
     return opts
 
 
