b805ef9ef807711f3fbbac3bd221133855171d17
Ardiea <mas48@mit.edu>
2024-08-23 15:43:12 -0400

    Initial commit of additional k8s work.

--- src/ol_infrastructure/infrastructure/aws/eks/__main__.py
+++ src/ol_infrastructure/infrastructure/aws/eks/__main__.py
@@ -2,7 +2,7 @@
 
 import json
 
-import pulumi_aws
+import pulumi_aws as aws
 import pulumi_eks as eks
 import yaml
 from pulumi import Config, StackReference, export
@@ -27,7 +27,18 @@ pod_ip_blocks = target_vpc["k8s_pod_subnet_cidrs"]
 pod_subnet_ids = target_vpc["k8s_pod_subnet_ids"]
 service_ip_block = target_vpc["k8s_service_subnet_cidr"]
 
-cluster_name = f"{stack_info.env_prefix}-{stack_info.env_suffix}-eks-cluster"
+# Commented out means we let the cluster manages it automatically
+default_addons = {
+    #    "aws-node" - daemonset
+    #    "coredns" - deployment
+    #    "kube-proxy" - daemonset
+    "aws-ebs-csi-driver": {
+        "addon_name": "aws-ebs-csi-driver",
+        "addon_version": "v1.33.0-eksbuild.1",
+    }
+}
+
+cluster_name = f"{stack_info.env_prefix}-{stack_info.env_suffix}"
 
 aws_config = AWSBase(
     tags={
@@ -46,44 +57,24 @@ default_assume_role_policy = {
     },
 }
 
-administrator_iam_role = pulumi_aws.iam.Role(
-    f"{cluster_name}-admin-role",
+administrator_iam_role = aws.iam.Role(
+    f"{cluster_name}-eks-admin-role",
     assume_role_policy=json.dumps(default_assume_role_policy),
-    name_prefix=f"{cluster_name}-admin-role-"[:IAM_ROLE_NAME_PREFIX_MAX_LENGTH],
+    name_prefix=f"{cluster_name}-eks-admin-role-"[:IAM_ROLE_NAME_PREFIX_MAX_LENGTH],
     path=f"/ol-infrastructure/eks/{cluster_name}/",
     tags=aws_config.tags,
 )
 
-node_role = pulumi_aws.iam.Role(
-    f"{cluster_name}-node-role",
-    assume_role_policy=json.dumps(default_assume_role_policy),
-    name_prefix=f"{cluster_name}-node-role-"[:IAM_ROLE_NAME_PREFIX_MAX_LENGTH],
-    path=f"/ol-infrastructure/eks/{cluster_name}/",
-    tags=aws_config.tags,
-)
-managed_policy_arns = [
-    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
-    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-]
-for i, policy in enumerate(managed_policy_arns):
-    # Create RolePolicyAttachment without returning it.
-    pulumi_aws.iam.RolePolicyAttachment(
-        f"{cluster_name}-node-role-policy-{i}", policy_arn=policy, role=node_role.id
-    )
-node_instance_profile = pulumi_aws.iam.InstanceProfile(
-    f"{cluster_name}-node-instanceProfile", role=node_role.name
-)
 
 cluster = eks.Cluster(
-    cluster_name,
+    f"{cluster_name}-eks-cluster",
     name=cluster_name,
     access_entries={
         "admin": eks.AccessEntryArgs(
             principal_arn=administrator_iam_role.arn,
             access_policies={
                 "admin": eks.AccessPolicyAssociationArgs(
-                    access_scope=pulumi_aws.eks.AccessPolicyAssociationAccessScopeArgs(
+                    access_scope=aws.eks.AccessPolicyAssociationAccessScopeArgs(
                         type="cluster",
                     ),
                     policy_arn="arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy",
@@ -111,29 +102,63 @@ cluster = eks.Cluster(
     ],
 )
 
+# Configure addons
+extra_addons = eks_config.get_object("extra_addons") or {}
+addons = default_addons | extra_addons
+for addon_key, addon_definition in addons.items():
+    eks.Addon(
+        f"{cluster_name}-eks-addon-{addon_key}",
+        cluster=cluster,
+        **addon_definition,
+    )
 
-eks.ManagedNodeGroup(
-    f"{cluster_name}-managednodegroup-simple",
-    cluster=cluster,
-    # cluster_name=cluster._name,
-    capacity_type="ON_DEMAND",
-    instance_types=["t3.medium"],
-    node_group_name=f"{cluster_name}-managednodegroup-simple-testing",
-    # node_subnet_ids=pod_subnet_ids,
-    node_role_arn=node_role.arn,
+
+# Configure node groups
+node_role = aws.iam.Role(
+    f"{cluster_name}-eks-node-role",
+    assume_role_policy=json.dumps(default_assume_role_policy),
+    name_prefix=f"{cluster_name}-eks-node-role-"[:IAM_ROLE_NAME_PREFIX_MAX_LENGTH],
+    path=f"/ol-infrastructure/eks/{cluster_name}/",
+    tags=aws_config.tags,
 )
-
-# change this to a managed node group
-# eks.NodeGroupV2(
-#    f"{cluster_name}-nodegroup-simple",
-#    cluster=cluster,
-#    instance_type="t3.medium",
-#    desired_capacity=2,
-#    min_size=1,
-#    max_size=2,
-#    node_subnet_ids=pod_subnet_ids,
-#    instance_profile=node_instance_profile,
-# )
+managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+]
+for i, policy in enumerate(managed_policy_arns):
+    aws.iam.RolePolicyAttachment(
+        f"{cluster_name}-eks-node-role-policy-{i}", policy_arn=policy, role=node_role.id
+    )
+node_instance_profile = aws.iam.InstanceProfile(
+    f"{cluster_name}-eks-node-instanceProfile", role=node_role.name
+)
+for ng_name, ng_config in eks_config.require_object("nodegroups").items():
+    taint_list = []
+    for taint_name, taint_config in ng_config["taints"].items():
+        taint_list.append(
+            aws.eks.NodeGroupTaintArgs(
+                key=taint_name,
+                value=taint_config["value"] or None,
+                effect=taint_config["effect"],
+            ),
+        )
+    eks.ManagedNodeGroup(
+        f"{cluster_name}-eks-managednodegroup-{ng_name}",
+        capacity_type="ON_DEMAND",
+        cluster=cluster,
+        instance_types=ng_config["instance_types"],
+        labels=ng_config["labels"] or {},
+        node_group_name=f"{cluster_name}-managednodegroup-{ng_name}",
+        node_role_arn=node_role.arn,
+        scaling_config=aws.eks.NodeGroupScalingConfigArgs(
+            desired_size=ng_config["scaling"]["desired"] or 2,
+            max_size=ng_config["scaling"]["max"] or 3,
+            min_size=ng_config["scaling"]["min"] or 1,
+        ),
+        tags=aws_config.merged_tags(ng_config["tags"] or {}),
+        taints=taint_list,
+    )
 
 export(
     "cluster_export",
