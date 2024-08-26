c6019f29e0919f2f64871edde4b1c512d04eff7b
Muhammad Anas <88967643+Anas12091101@users.noreply.github.com>
2024-08-26 22:37:12 +0500

    feat: remove certificate link from resedential course authoring mfe (#2628)
    * feat: remove certificate link from resedential course authoring mfe
    
    * fix: typo
    
    * fix: issues
    
    * fix: type issues
    
    * fix: convert bool to string lowercase
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/ol_concourse/pipelines/open_edx/mfe/pipeline.py
+++ src/ol_concourse/pipelines/open_edx/mfe/pipeline.py
@@ -2,7 +2,7 @@ import sys
 import textwrap
 from collections import defaultdict
 from itertools import chain, product
-from typing import Optional
+from typing import Literal, Optional
 
 from pydantic import BaseModel
 
@@ -40,6 +40,7 @@ class OpenEdxVars(BaseModel):
     accessibility_url: Optional[str] = None
     account_settings_url: Optional[str] = None
     contact_url: Optional[str] = None
+    enable_certificate_page: Optional[Literal["true", "false"]] = None
     deployment_name: OpenEdxDeploymentName
     display_feedback_widget: Optional[str] = None
     environment: str
@@ -82,6 +83,7 @@ def mfe_params(
         "CONTACT_URL": open_edx.contact_url,
         "CSRF_TOKEN_API_PATH": "/csrf/api/v1/token",
         "DISPLAY_FEEDBACK_WIDGET": open_edx.display_feedback_widget,
+        "ENABLE_CERTIFICATE_PAGE": open_edx.enable_certificate_page,
         "DISCUSSIONS_MFE_BASE_URL": f"https://{open_edx.lms_domain}/{discussion_mfe_path}",
         "FAVICON_URL": open_edx.favicon_url,
         "HONOR_CODE_URL": open_edx.honor_code_url,
