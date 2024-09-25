013d59bc76e9eb37918333bda931847e268a30aa
sar <sar.haidar@gmail.com>
2024-09-25 09:15:16 -0500

    Add iam_tag to vault accounts

--- src/ol_infrastructure/components/services/vault.py
+++ src/ol_infrastructure/components/services/vault.py
@@ -235,7 +235,7 @@ class OLVaultAWSSecretsEngineConfig(BaseModel):
     vault_backend_path: str
     policy_documents: dict[str, str]
     credential_type: str = "iam_user"
-    iam_tags: list[str] = ["operations"]
+    iam_tags: list[str] = ["OU=operations"]
 
     @field_validator("vault_backend_path")
     @classmethod
