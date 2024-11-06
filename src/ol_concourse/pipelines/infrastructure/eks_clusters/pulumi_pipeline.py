b95b2b6f5cb4e179059cb6e2a8ae1960bc682a24
Chris Patti <feoh@feoh.org>
2024-11-06 16:02:03 -0500

    feat: Initial shot from the hip at an EKS Cluster update meta-pipeline.

---
+++ src/ol_concourse/pipelines/infrastructure/eks_clusters/pulumi_pipeline.py
@@ -0,0 +1,41 @@
+from ol_concourse.lib.jobs.infrastructure import pulumi_job
+from ol_concourse.lib.models.pipeline import Identifier, Pipeline
+from ol_concourse.lib.resources import git_repo
+from ol_concourse.pipelines.constants import PULUMI_CODE_PATH
+
+eks_cluster_code = git_repo(
+    Identifier("ol-infrastructure"),
+    uri="https://github.com/mitodl/ol-infrastructure",
+    paths=[
+        "src/ol_infrastructure/infrastructure/aws/eks",
+    ],
+)
+
+pulumi_job_fragment = pulumi_job(
+    eks_cluster_code,
+    stack_name="infrastructure.aws.eks",
+    project_name="ol-infrastructure-infrastructure-aws-eks",
+    project_source_path=PULUMI_CODE_PATH.joinpath("substructure/xpro_partner_dns/"),
+)
+
+xpro_partner_dns_pipeline = Pipeline(
+    resource_types=pulumi_job_fragment.resource_types,
+    resources=[eks_cluster_code, *pulumi_job_fragment.resources],
+    jobs=pulumi_job_fragment.jobs,
+)
+
+if __name__ == "__main__":
+    import sys
+
+    with open("definition.json", "w") as definition:  # noqa: PTH123
+        definition.write(xpro_partner_dns_pipeline.model_dump_json(indent=2))
+    sys.stdout.write(xpro_partner_dns_pipeline.model_dump_json(indent=2))
+    sys.stdout.writelines(
+        [
+            "\n",
+            (
+                "fly -t <target> set-pipeline -p pulumi-xpro-partner-dns -c"
+                " definition.json"
+            ),
+        ]
+    )
