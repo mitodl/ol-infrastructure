3a3f6e6bfac231548c3912ca471c53509e0de9d4
Chris Patti <feoh@feoh.org>
2024-11-15 12:17:36 -0500

    feat: Add eks substructure stacks to pipeline

--- src/ol_concourse/pipelines/infrastructure/eks_clusters/pulumi_pipeline.py
+++ src/ol_concourse/pipelines/infrastructure/eks_clusters/pulumi_pipeline.py
@@ -4,7 +4,7 @@ from ol_concourse.lib.models.pipeline import Identifier
 from ol_concourse.lib.resources import git_repo
 from ol_concourse.pipelines.constants import PULUMI_CODE_PATH
 
-eks_cluster_code = git_repo(
+eks_infrastructure_code = git_repo(
     Identifier("ol-infrastructure"),
     uri="https://github.com/mitodl/ol-infrastructure",
     paths=[
@@ -12,22 +12,41 @@ eks_cluster_code = git_repo(
     ],
 )
 
+eks_substructure_code = git_repo(
+    Identifier("ol-infrastructure-substructure"),
+    uri="https://github.com/mitodl/ol-infrastructure",
+    paths=[
+        "src/ol_infrastructure/substructure/aws/eks",
+    ],
+)
+
 pipeline_fragments = []
 
 for cluster in ["data", "operations"]:
     stages = ["CI", "QA", "Production"]
-    temp_chain = pulumi_jobs_chain(
-        eks_cluster_code,
+
+    infra_chain = pulumi_jobs_chain(
+        eks_infrastructure_code,
         project_name="ol-infrastructure-eks",
         project_source_path=PULUMI_CODE_PATH.joinpath("infrastructure/aws/eks"),
         stack_names=[f"infrastructure.aws.eks.{cluster}.{stage}" for stage in stages],
     )
-    pipeline_fragments.append(temp_chain)
+    pipeline_fragments.append(infra_chain)
+
+    substructure_chain = pulumi_jobs_chain(
+        eks_substructure_code,
+        project_name="ol-substructure-eks",
+        project_source_path=PULUMI_CODE_PATH.joinpath("substructure/aws/eks"),
+        stack_names=[f"substructure.aws.eks.{cluster}.{stage}" for stage in stages],
+    )
+    pipeline_fragments.append(substructure_chain)
 
 eks_cluster_update_pipeline = PipelineFragment.combine_fragments(
     *pipeline_fragments
 ).to_pipeline()
-eks_cluster_update_pipeline.resources.append(eks_cluster_code)
+
+eks_cluster_update_pipeline.resources.append(eks_infrastructure_code)
+eks_cluster_update_pipeline.resources.append(eks_substructure_code)
 
 if __name__ == "__main__":
     import sys
