79ee83cc48c393ef5366c17050281fb5c137847c
Chris Patti <feoh@feoh.org>
2024-12-05 09:06:26 -0500

    Upgrade XPro QA to OpenEdX Sumac

--- src/bridge/settings/openedx/version_matrix.py
+++ src/bridge/settings/openedx/version_matrix.py
@@ -37,7 +37,7 @@ class OpenLearningOpenEdxDeployment(Enum):
         deployment_name="xpro",
         env_release_map=[
             EnvRelease("CI", OpenEdxSupportedRelease["sumac"]),
-            EnvRelease("QA", OpenEdxSupportedRelease["quince"]),
+            EnvRelease("QA", OpenEdxSupportedRelease["sumac"]),
             EnvRelease("Production", OpenEdxSupportedRelease["quince"]),
         ],
     )
