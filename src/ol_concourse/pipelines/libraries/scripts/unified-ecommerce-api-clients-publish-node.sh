542912dbbb2873efb938dfb30a7f4ee405f2dc5d
James Kachel <jkachel@mit.edu>
2024-12-13 14:23:26 -0600

    Install yarn (otherwise it tries to use the default 1.22 version)
    This is a touch more involved. We actually need to enable Corepack, and then it will go get the appropriate yarn for us. Then, the publish command is different in yarn berry - so hopefully this is enough. (It will default to the "latest" tag and I think the version will come out of the package.json?)

--- src/ol_concourse/pipelines/libraries/scripts/unified-ecommerce-api-clients-publish-node.sh
+++ src/ol_concourse/pipelines/libraries/scripts/unified-ecommerce-api-clients-publish-node.sh
@@ -2,9 +2,11 @@
 # publish the npm package to npmjs.org
 
 echo "//registry.npmjs.org/:_authToken=((npm_publish.npmjs_token))" > .npmrc
+COREPACK_ENABLE_DOWNLOAD_PROMPT=0
+corepack enable
 yarn install --immutable
 yarn build
 # OK so this is vaguely gross but it will do :)
 new_version=$(cat ../../../VERSION)
 echo "Publishing version $new_version"
-yarn publish --access public --new-version "$new_version"
+yarn npm publish --access public
