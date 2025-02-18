57a1e7b1e05928391b15a1d475a571396aa414ca
Tobias Macey <tmacey@mit.edu>
2025-02-18 15:16:24 -0500

    chore: Configure Renovate to maintain versions in versions.py
    The versions.py file is a useful source of truth for what versions are
    running. Unfortunately it frequently falls out of date as we pull versions dynamically
    into the build pipelines. This adds information to the versions.py file that Renovate
    can use to keep that file up to date as upstream versions change. https://docs.renovatebot.com/modules/manager/regex/

--- src/bridge/lib/versions.py
+++ src/bridge/lib/versions.py
@@ -1,26 +1,37 @@
-AIRBYTE_VERSION = "1.3.0"
-APISIX_CLOUD_CLI_VERSION = "0.30.2"
-APISIX_VERSION = "2.15.3"
+# renovate: datasource=github-releases depName=concourse packageName=concourse/concourse
 CONCOURSE_VERSION = "7.8.3"
+# renovate: datasource=github-releases depName=consul-template packageName=hashicorp/consul-template
 CONSUL_TEMPLATE_VERSION = "0.29.5"
+# renovate: datasource=github-releases depName=consul packageName=hashicorp/consul
 CONSUL_VERSION = "1.14.0"
-DAGSTER_VERSION = "1.0.17"
+# renovate: datasource=github-releases depName=keycloak packageName=keycloak/keycloak
 KEYCLOAK_VERSION = "26.1"
+# renovate: datasource=helm depName=open-metadata packageName=open-metadata/openmetadata
 OPEN_METADATA_VERSION = "1.6.3"
 OVS_VERSION = "v0.65.1-3-g2630021"
 REDASH_VERSION = "9d273e4"
+# renovate: datasource=github-releases depName=traefik packageName=traefik/traefik
 TRAEFIK_VERSION = "2.10.3"
 TUTOR_PERMISSIONS_VERSION = "15.3.4"
+# renovate: datasource=github-releases depName=vault packageName=hashicorp/vault
 VAULT_VERSION = "1.12.1"
 
 # EKS Specific Versions
+# renovate: datasource=helm depName=airbyte packageName=airbyte/airbyte
 AIRBYTE_CHART_VERSION = "1.3.0"
+# renovate: datasource=helm depName=apisix packageName=apisix/apisix
 APISIX_CHART_VERSION = "2.10.0"
+# renovate: datasource=helm depName=cert-manager packageName=cert-manager/cert-manager
 CERT_MANAGER_CHART_VERSION = "v1.16.1"
 EBS_CSI_DRIVER_VERSION = "v1.36.0-eksbuild.1"
 EFS_CSI_DRIVER_VERSION = "v2.0.8-eksbuild.1"
+# renovate: datasource=helm depName=external-dns packageName=external-dns/external-dns
 EXTERNAL_DNS_CHART_VERSION = "1.15.0"
+# renovate: datasource=github-releases depName=gateway-api packageName=kubernetes-sigs/gateway-api
 GATEWAY_API_VERSION = "v1.2.1"
+# renovate: datasource=helm depName=traefik packageName=traefik/traefik
 TRAEFIK_CHART_VERSION = "v34.2.0"
+# renovate: datasource=helm depName=vantage-kubernetes-agent packageName=vantage-kubernetes-agent/vantage-kubernetes-agent
 VANTAGE_K8S_AGENT_CHART_VERSION = "1.0.36"
+# renovate: datasource=helm depName=vault-secrets-operator packageName=vault-secrets-operator/vault-secrets-operator
 VAULT_SECRETS_OPERATOR_CHART_VERSION = "0.9.0"
