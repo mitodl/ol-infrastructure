dcfe0efbc50895ba416c66934d4d4082866e65b9
Tobias Macey <tmacey@mit.edu>
2025-02-18 17:50:24 -0500

    fix: Finally fix the helm charts

--- src/bridge/lib/versions.py
+++ src/bridge/lib/versions.py
@@ -31,7 +31,7 @@ EXTERNAL_DNS_CHART_VERSION = "1.15.0"
 GATEWAY_API_VERSION = "v1.2.1"
 # renovate: datasource=helm depName=traefik packageName=traefik registryUrl=https://traefik.github.io/charts
 TRAEFIK_CHART_VERSION = "v34.2.0"
-# renovate: datasource=helm depName=vantage-kubernetes-agent packageName=vantage-kubernetes-agent
+# renovate: datasource=helm depName=vantage-kubernetes-agent packageName=vantage-kubernetes-agent registryUrl=https://vantage-sh.github.io/helm-charts
 VANTAGE_K8S_AGENT_CHART_VERSION = "1.0.36"
-# renovate: datasource=helm depName=vault-secrets-operator packageName=vault-secrets-operator registryUrl=https://artifacthub.io/packages/helm/vault-secrets-operator/vault-secrets-operator
+# renovate: datasource=github-releases depName=vault-secrets-operator packageName=hashicorp/vault-secrets-operator
 VAULT_SECRETS_OPERATOR_CHART_VERSION = "0.9.0"
