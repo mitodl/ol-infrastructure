d1e7728dd8cad3bdee8860752ddd26a0271f5ad7
Tobias Macey <tmacey@mit.edu>
2025-02-18 17:51:47 -0500

    fix: use Helm source for Vault secrets operator

--- src/bridge/lib/versions.py
+++ src/bridge/lib/versions.py
@@ -33,5 +33,5 @@ GATEWAY_API_VERSION = "v1.2.1"
 TRAEFIK_CHART_VERSION = "v34.2.0"
 # renovate: datasource=helm depName=vantage-kubernetes-agent packageName=vantage-kubernetes-agent registryUrl=https://vantage-sh.github.io/helm-charts
 VANTAGE_K8S_AGENT_CHART_VERSION = "1.0.36"
-# renovate: datasource=github-releases depName=vault-secrets-operator packageName=hashicorp/vault-secrets-operator
+# renovate: datasource=helm depName=vault-secrets-operator packageName=vault-secrets-operator registryUrl=https://helm.releases.hashicorp.com
 VAULT_SECRETS_OPERATOR_CHART_VERSION = "0.9.0"
