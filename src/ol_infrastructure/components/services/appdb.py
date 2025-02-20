b184270dc6301da9acf6436b00f923743c6fd839
Chris Patti <feoh@feoh.org>
2025-02-20 17:41:48 -0500

    Fix the appdb class moving everything into the constructor.

--- src/ol_infrastructure/components/services/appdb.py
+++ src/ol_infrastructure/components/services/appdb.py
@@ -3,14 +3,18 @@ This module collapses as much boilerplate as possible to facilitate the setup
 of an application's database.
 """
 
-from pulumi import ComponentResource, Config, StackReference
+from pulumi import ComponentResource, StackReference
 from pulumi_aws import ec2
 
 from bridge.lib.magic_numbers import (
     AWS_RDS_DEFAULT_DATABASE_CAPACITY,
     DEFAULT_POSTGRES_PORT,
 )
-from ol_infrastructure.components.aws.database import OLAmazonDB, OLPostgresDBConfig
+from ol_infrastructure.components.aws.database import (
+    OLAmazonDB,
+    OLPostgresDBConfig,
+    SecretStr,
+)
 from ol_infrastructure.lib.ol_types import AWSBase
 from ol_infrastructure.lib.pulumi_helper import parse_stack
 from ol_infrastructure.lib.stack_defaults import defaults
@@ -27,21 +31,35 @@ aws_config = AWSBase(
 )
 
 
+class OLDatabaseConfig:
+    """Confugration for the MIT OL Database component"""
+
+    app_name: str
+    app_db_name: str
+    app_db_password: str
+    app_db_instance_size: str | None
+    app_db_capacity: str | None
+    target_vpc_name: str
+    app_security_group: ec2.SecurityGroup
+
+
 class OLDatabase(ComponentResource):
-    def __init__(
-        self,
-        app_name: str,
-        app_db_name: str,
-        app_security_group,
-        app_db_security_group,
-        pulumi_app_config: Config,
-        app_db_config: OLPostgresDBConfig,
-        target_vpc_name: str,
-    ):
-        target_vpc = network_stack.require_output(target_vpc_name)
+    """MIT OL Database component"""
+
+    def __init__( # noqa: D107
+            self,
+            ol_db_config: OLDatabaseConfig
+        ):
+
+        self.ol_db_config = ol_db_config
+        super().__init__(
+            "ol:infrastructure:aws:OLDatabase", ol_db_config.app_name, None
+        )
+
+        target_vpc = network_stack.require_output(ol_db_config.target_vpc_name)
         self.app_db_security_group = ec2.SecurityGroup(
-            f"{app_name}-db-security-group-{stack_info.env_suffix}",
-            name=f"{app_name}-db-security-group-{stack_info.env_suffix}",
+            f"{ol_db_config.app_name}-db-security-group-{stack_info.env_suffix}",
+            name=f"{ol_db_config.app_name}-db-security-group-{stack_info.env_suffix}",
             description="Access control for the learn-ai database.",
             ingress=[
                 ec2.SecurityGroupIngressArgs(
@@ -54,7 +72,7 @@ class OLDatabase(ComponentResource):
                     description="Access to postgres from consul and vault.",
                 ),
                 ec2.SecurityGroupIngressArgs(
-                    security_groups=[app_security_group.id],
+                    security_groups=[self.ol_db_config.app_security_group.id],
                     protocol="tcp",
                     from_port=DEFAULT_POSTGRES_PORT,
                     to_port=DEFAULT_POSTGRES_PORT,
@@ -67,20 +85,29 @@ class OLDatabase(ComponentResource):
 
         rds_defaults = defaults(stack_info)["rds"]
         rds_defaults["instance_size"] = (
-            self.pulumi_app_config.get("db_instance_size")
-            or DBInstanceTypes.small.value
+            self.ol_db_config.app_db_instance_size or rds_defaults["instance_size"]
         )
 
         self.app_db_config = OLPostgresDBConfig(
-            instance_name=f"{app_name}-db-{stack_info.env_suffix}",
-            password=pulumi_app_config.get("db_password"),
+            instance_name=f"{ol_db_config.app_name}-db-{stack_info.env_suffix}",
+            password=SecretStr(self.ol_db_config.app_db_password),
             subnet_group_name=target_vpc["rds_subnet"],
-            security_groups=[app_db_security_group],
-            storage=pulumi_app_config.get("db_capacity")
-            or str(AWS_RDS_DEFAULT_DATABASE_CAPACITY),
+            security_groups=[self.app_db_security_group],
+            storage=self.ol_db_config.app_db_capacity or str(AWS_RDS_DEFAULT_DATABASE_CAPACITY),
             engine_major_version="16",
             tags=aws_config.tags,
-            db_name=app_db_name,
+            db_name=self.ol_db_config.app_db_name,
             **defaults(stack_info)["rds"],
         )
-        self.app_db = OLAmazonDB(self.app_db_config)
+        self.app_db_vault_backend_config = OLVaultPostgresDatabaseConfig(
+            db_name=self.app_db_config.db_name,
+            mount_point=f"{self.app_db_config.engine}-learn-ai",
+            db_admin_username=self.app_db_config.username,
+            db_admin_password=self.app_db_config.db_password,
+            db_host=app_db.db_instance.address,
+        )
+        app_db_vault_backend = OLVaultDatabaseBackend(
+            self.app_db_vault_backend_config,
+        )
+
+        self.app_db = OLAmazonDB(app_db_config)
