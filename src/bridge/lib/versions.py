2bc563f0aa7d9146eb87dfc237aa68487acb12bb
Tobias Macey <tmacey@mit.edu>
2025-03-10 10:57:41 -0400

    chore(renovate): fix version matching pattern for eks builds

--- src/bridge/lib/versions.py
+++ src/bridge/lib/versions.py
@@ -23,7 +23,9 @@ AIRBYTE_CHART_VERSION = "1.5.1"
 APISIX_CHART_VERSION = "2.10.0"
 # renovate: datasource=helm depName=cert-manager packageName=cert-manager
 CERT_MANAGER_CHART_VERSION = "v1.16.1"
+# renovate: datasource=github-releases depName=aws-ebs-csi-driver packageName=kubernetes-sigs/aws-ebs-csi-driver
 EBS_CSI_DRIVER_VERSION = "v1.36.0-eksbuild.1"
+# renovate: datasource=github-releases depName=aws-ebs-csi-driver packageName=kubernetes-sigs/aws-efs-csi-driver
 EFS_CSI_DRIVER_VERSION = "v2.0.8-eksbuild.1"
 # renovate: datasource=helm depName=external-dns packageName=external-dns
 EXTERNAL_DNS_CHART_VERSION = "1.15.0"
