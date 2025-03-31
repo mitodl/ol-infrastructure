889f707d6d5c60bec8d154e5aff7ddaf52bfc6ff
Tobias Macey <tmacey@mit.edu>
2025-03-31 09:23:42 -0400

    fix: Constrain event target name length

--- src/ol_infrastructure/components/aws/mediaconvert.py
+++ src/ol_infrastructure/components/aws/mediaconvert.py
@@ -187,6 +187,7 @@ class OLMediaConvert(ComponentResource):
 
         self.mediaconvert_cloudwatch_target = cloudwatch.EventTarget(
             event_target_name,
+            name=event_target_name[:63],
             rule=self.mediaconvert_cloudwatch_rule.name,
             arn=self.sns_topic.arn,
             opts=component_ops,
