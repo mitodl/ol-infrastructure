3b3f447f6d80e897e90e25c57dffa3da1150d0e7
Tobias Macey <tmacey@mit.edu>
2025-04-02 17:45:59 -0400

    refactor: Let the pipeline work for arbitrary apps

--- src/ol_concourse/pipelines/infrastructure/learn_ai/docker_pulumi.py
+++ src/ol_concourse/pipelines/infrastructure/learn_ai/docker_pulumi.py
@@ -29,23 +29,25 @@ from ol_concourse.lib.resources import git_repo, pulumi_provisioner, registry_im
 from ol_concourse.pipelines.constants import PULUMI_WATCHED_PATHS
 
 
-def _define_git_resources() -> tuple[Resource, Resource, Resource, Resource]:
+def _define_git_resources(
+    app_name: str,
+) -> tuple[Resource, Resource, Resource, Resource]:
     """Define the git resources needed for the pipeline."""
-    learn_ai_main_repo = git_repo(
-        Identifier("learn-ai-main"),
-        uri="https://github.com/mitodl/learn-ai",
+    main_repo = git_repo(
+        name=Identifier(f"{app_name}-main"),
+        uri=f"https://github.com/mitodl/{app_name}",
         branch="main",
     )
 
-    learn_ai_release_candidate_repo = git_repo(
-        Identifier("learn-ai-release-candidate"),
-        uri="http://github.com/mitodl/learn-ai",
+    release_candidate_repo = git_repo(
+        name=Identifier(f"{app_name}-release-candidate"),
+        uri=f"http://github.com/mitodl/{app_name}",
         branch="release-candidate",
         fetch_tags=True,
     )
 
-    learn_ai_release_repo = git_repo(
-        Identifier("learn-ai-release"),
+    release_repo = git_repo(
+        name=Identifier(f"{app_name}-release"),
         uri="http://github.com/mitodl/learn-ai",
         branch="release",
         fetch_tags=True,
@@ -53,25 +55,28 @@ def _define_git_resources() -> tuple[Resource, Resource, Resource, Resource]:
     )
 
     ol_infra_repo = git_repo(
-        Identifier("ol-infra"),
+        Identifier(f"ol-infra-{app_name}"),
         uri="https://github.com/mitodl/ol-infrastructure",
         branch="main",
-        paths=["src/ol_infrastructure/applications/learn_ai", *PULUMI_WATCHED_PATHS],
+        paths=[
+            f"src/ol_infrastructure/applications/{app_name.replace('-', '_')}",
+            *PULUMI_WATCHED_PATHS,
+        ],
     )
     return (
-        learn_ai_main_repo,
-        learn_ai_release_candidate_repo,
-        learn_ai_release_repo,
+        main_repo,
+        release_candidate_repo,
+        release_repo,
         ol_infra_repo,
     )
 
 
-def _define_registry_image_resources() -> tuple[Resource, Resource]:
+def _define_registry_image_resources(app_name: str) -> tuple[Resource, Resource]:
     """Define the registry image resources needed for the pipeline."""
     # Used for publishing the CI containers to dockerhub
-    learn_ai_registry_ci_image = registry_image(
-        name=Identifier("learn-ai-ci-image"),
-        image_repository="mitodl/learn-ai-app-main",
+    registry_ci_image = registry_image(
+        name=Identifier(f"{app_name}-ci-image"),
+        image_repository=f"mitodl/{app_name}-app-main",
         username="((dockerhub.username))",
         password="((dockerhub.password))",  # noqa: S106
         tag_regex="[0-9A-Fa-f]+",  # Should only capture the CI images
@@ -79,9 +84,9 @@ def _define_registry_image_resources() -> tuple[Resource, Resource]:
     )
 
     # Used for publishing the RC / production containers to dockerhub
-    learn_ai_registry_rc_image = registry_image(
-        name=Identifier("learn-ai-rc-image"),
-        image_repository="mitodl/learn-ai-app-rc",
+    registry_rc_image = registry_image(
+        name=Identifier(f"{app_name}-rc-image"),
+        image_repository=f"mitodl/{app_name}-app-rc",
         image_tag=None,  # Only filter on tagged images
         username="((dockerhub.username))",
         password="((dockerhub.password))",  # noqa: S106
@@ -91,31 +96,35 @@ def _define_registry_image_resources() -> tuple[Resource, Resource]:
     )
 
     return (
-        learn_ai_registry_ci_image,
-        learn_ai_registry_rc_image,
+        registry_ci_image,
+        registry_rc_image,
     )
 
 
 def _define_pulumi_resources(
-    ol_infra_repo: Resource,
-) -> tuple[ResourceType, Resource]:
+    app_name: str, ol_infra_repo_name: str
+) -> tuple[ResourceType, str]:
     """Define the Pulumi resource type and resource."""
     pulumi_resource_type = pulumi_provisioner_resource()
     pulumi_resource = pulumi_provisioner(
-        name=Identifier("pulumi-ol-infrastructure-learn-ai-application"),
-        project_name="ol-infrastructure-learn-ai-application",
-        project_path=f"{ol_infra_repo.name}/src/ol_infrastructure/applications/learn_ai",
+        name=Identifier(f"pulumi-ol-infrastructure-{app_name}-application"),
+        project_name=f"ol-infrastructure-{app_name}-application",
+        project_path=(
+            f"{ol_infra_repo_name}/src/ol_infrastructure/applications/"
+            f"{app_name.replace('-', '_')}"
+        ),
     )
     return pulumi_resource_type, pulumi_resource
 
 
 def _build_image_job(
-    branch_type: str,
+    app_name: Resource,
+    branch_type: Resource,
     git_repo_resource: Resource,
     registry_image_resource: Resource,
 ) -> Job:
     """Generate an image build job for a specific branch type (main or rc)."""
-    job_name = f"build-learn-ai-image-from-{branch_type.replace('_', '-')}"
+    job_name = f"build-{app_name}-image-from-{branch_type}"
     version_var = f"{branch_type}_version"
     version_output_dir = f"{branch_type}_version"
     version_file = f"{version_output_dir}/version"
@@ -186,13 +195,14 @@ def _build_image_job(
 
 
 def _common_deployment_steps(
+    app_name: str,
     stack_env: str,
     registry_image_resource: Resource,
     pulumi_resource: Resource,
     ol_infra_repo: Resource,
 ) -> list[Step]:
     """Generate the common task and put steps for a deployment job."""
-    pulumi_stack_name = f"applications.learn_ai.{stack_env}"
+    pulumi_stack_name = f"applications.{app_name.replace('-', '_')}.{stack_env}"
     return [
         LoadVarStep(
             load_var="image_tag",
@@ -206,7 +216,7 @@ def _common_deployment_steps(
                     type=REGISTRY_IMAGE,
                     source=RegistryImage(repository="amazon/aws-cli"),
                 ),
-                inputs=[Input(name=ol_infra_repo.name)],
+                inputs=[Input(name=ol_infra_repo)],
                 outputs=[Output(name=Identifier("aws_creds"))],
                 run=Command(
                     path=f"{ol_infra_repo.name}/pipelines/infrastructure/scripts/generate_aws_config_from_instance_profile.sh",
@@ -214,14 +224,14 @@ def _common_deployment_steps(
             ),
         ),
         PutStep(
-            put=pulumi_resource.name,
+            put=pulumi_resource,
             get_params={"skip_implicit_get": True},
             params={
                 "env_os": {
                     "AWS_DEFAULT_REGION": "us-east-1",
-                    "PYTHONPATH": f"/usr/lib/:/tmp/build/put/{ol_infra_repo.name}/src/",
+                    "PYTHONPATH": f"/usr/lib/:/tmp/build/put/{ol_infra_repo}/src/",
                     "GITHUB_TOKEN": "((github.public_repo_access_token))",
-                    "LEARN_AI_DOCKER_TAG": "((.:image_tag))",
+                    f"{app_name.replace('-', '_').upper()}_DOCKER_TAG": "((.:image_tag))",  # noqa: E501
                 },
                 "stack_name": pulumi_stack_name,
             },
@@ -229,31 +239,35 @@ def _common_deployment_steps(
     ]
 
 
-def build_learn_ai_pipeline() -> Pipeline:
+def build_app_pipeline(app_name: str) -> Pipeline:
     # Define Resources
     (
-        learn_ai_main_repo,
-        learn_ai_release_candidate_repo,
-        learn_ai_release_repo,
+        main_repo,
+        release_candidate_repo,
+        release_repo,
         ol_infra_repo,
-    ) = _define_git_resources()
+    ) = _define_git_resources(app_name)
     (
-        learn_ai_registry_ci_image,
-        learn_ai_registry_rc_image,
-    ) = _define_registry_image_resources()
-    pulumi_resource_type, pulumi_resource = _define_pulumi_resources(ol_infra_repo)
+        registry_ci_image,
+        registry_rc_image,
+    ) = _define_registry_image_resources(app_name)
+    pulumi_resource_type, pulumi_resource = _define_pulumi_resources(
+        app_name, ol_infra_repo.name
+    )
 
     # Define Build Jobs
 
-    learn_ai_main_image_build_job = _build_image_job(
+    main_image_build_job = _build_image_job(
+        app_name=app_name,
         branch_type="main",
-        git_repo_resource=learn_ai_main_repo,
-        registry_image_resource=learn_ai_registry_ci_image,
+        git_repo_resource=main_repo,
+        registry_image_resource=registry_ci_image,
     )
-    learn_ai_rc_image_build_job = _build_image_job(
+    rc_image_build_job = _build_image_job(
+        app_name=app_name,
         branch_type="release_candidate",
-        git_repo_resource=learn_ai_release_candidate_repo,
-        registry_image_resource=learn_ai_registry_rc_image,
+        git_repo_resource=release_candidate_repo,
+        registry_image_resource=registry_rc_image,
     )
 
     # Define Deployment Jobs
@@ -261,17 +275,18 @@ def build_learn_ai_pipeline() -> Pipeline:
     # CI Deployment
     ci_fragment = pulumi_job(
         pulumi_code=ol_infra_repo,
-        stack_name="applications.learn_ai.CI",
-        project_name="ol-infrastructure-learn-ai-application",
-        project_source_path="src/ol_infrastructure/applications/learn_ai",
+        stack_name=f"applications.{app_name.replace('-', '_')}.CI",
+        project_name=f"ol-infrastructure-{app_name}-application",
+        project_source_path=(
+            f"src/ol_infrastructure/applications/{app_name.replace('-', '_')}"
+        ),
         dependencies=[
             GetStep(
-                get=learn_ai_registry_ci_image.name,
+                get=registry_ci_image.name,
                 trigger=True,
-                passed=[learn_ai_main_image_build_job.name],
+                passed=[main_image_build_job.name],
                 params={"skip_download": True},
             ),
-            GetStep(get=ol_infra_repo.name, trigger=True),
         ],
     )
 
@@ -279,35 +294,36 @@ def build_learn_ai_pipeline() -> Pipeline:
     qa_and_production_fragment = pulumi_jobs_chain(
         pulumi_code=ol_infra_repo,
         stack_names=[
-            "applications.learn_ai.QA",
-            "applications.learn_ai.Production",
+            f"applications.{app_name.replace('-', '_')}.QA",
+            f"applications.{app_name.replace('-', '_')}.Production",
         ],
-        project_name="ol-infrastructure-learn-ai-application",
-        project_source_path="src/ol_infrastructure/applications/learn_ai",
+        project_name=f"ol-infrastructure-{app_name}-application",
+        project_source_path=(
+            f"src/ol_infrastructure/applications/{app_name.replace('-', '_')}"
+        ),
         dependencies=[
             GetStep(
-                get=learn_ai_registry_rc_image.name,
+                get=registry_rc_image.name,
                 trigger=True,
-                passed=[learn_ai_rc_image_build_job.name],
+                passed=[rc_image_build_job.name],
                 params={"skip_download": True},
             ),
-            GetStep(get=ol_infra_repo.name, trigger=True),
         ],
     )
 
     # Group into Fragments
 
-    learn_ai_main_branch_container_fragement = PipelineFragment(
-        resources=[learn_ai_main_repo, learn_ai_registry_ci_image],
-        jobs=[learn_ai_main_image_build_job],
+    main_branch_container_fragement = PipelineFragment(
+        resources=[main_repo, registry_ci_image],
+        jobs=[main_image_build_job],
     )
 
-    learn_ai_release_candidate_container_fragment = PipelineFragment(
+    release_candidate_container_fragment = PipelineFragment(
         resources=[
-            learn_ai_release_candidate_repo,
-            learn_ai_registry_rc_image,
+            release_candidate_repo,
+            registry_rc_image,
         ],
-        jobs=[learn_ai_rc_image_build_job],
+        jobs=[rc_image_build_job],
     )
 
     ci_deployment_fragment = PipelineFragment(
@@ -315,7 +331,7 @@ def build_learn_ai_pipeline() -> Pipeline:
         resources=[
             ol_infra_repo,
             pulumi_resource,
-            learn_ai_registry_ci_image,
+            registry_ci_image,
             *ci_fragment.resources,
         ],
         jobs=ci_fragment.jobs,
@@ -326,16 +342,18 @@ def build_learn_ai_pipeline() -> Pipeline:
     combined_fragment = PipelineFragment.combine_fragments(
         ci_deployment_fragment,
         qa_and_production_fragment,
-        learn_ai_main_branch_container_fragement,
-        learn_ai_release_candidate_container_fragment,
+        main_branch_container_fragement,
+        release_candidate_container_fragment,
     )
     return combined_fragment.to_pipeline()
 
 
 if __name__ == "__main__":
     with open("definition.json", "w") as definition:  # noqa: PTH123
-        definition.write(build_learn_ai_pipeline().model_dump_json(indent=2))
-    sys.stdout.write(build_learn_ai_pipeline().model_dump_json(indent=2))
+        definition.write(
+            build_app_pipeline(app_name="learn-ai").model_dump_json(indent=2)
+        )
+    sys.stdout.write(build_app_pipeline(app_name="learn-ai").model_dump_json(indent=2))
     sys.stdout.writelines(
         (
             "\n",
