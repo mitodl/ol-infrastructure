ecbb4653da47bf22584a51b7aa6eb897f952d5d9
Arslan Ashraf <arslan.ashraf@arbisoft.com>
2025-04-04 17:31:29 +0500

    address feedback

--- src/bridge/settings/openedx/mfe/slot_config/xpro/learning-mfe-config.env.jsx
+++ src/bridge/settings/openedx/mfe/slot_config/xpro/learning-mfe-config.env.jsx
@@ -1,3 +1,4 @@
+import { getConfig } from '@edx/frontend-platform';
 import * as remoteTutorDrawer from "./mitodl-smoot-design/dist/bundles/remoteTutorDrawer.umd.js"
 
 
@@ -5,3 +6,9 @@ remoteTutorDrawer.init({
   messageOrigin: getConfig().LMS_BASE_URL,
   transformBody: messages => ({ message: messages[messages.length - 1].content }),
 })
+
+const config = {
+  ...process.env
+};
+  
+export default config;
