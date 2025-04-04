e6b88cc3144a2a0e85767d3bd5db86c2371d881c
Arslan Ashraf <arslan.ashraf@arbisoft.com>
2025-04-04 15:57:53 +0500

    config: Enable AI Tutor on xPRO

---
+++ src/bridge/settings/openedx/mfe/slot_config/xpro/learning-mfe-config.env.jsx
@@ -0,0 +1,7 @@
+import * as remoteTutorDrawer from "./mitodl-smoot-design/dist/bundles/remoteTutorDrawer.umd.js"
+
+
+remoteTutorDrawer.init({
+  messageOrigin: getConfig().LMS_BASE_URL,
+  transformBody: messages => ({ message: messages[messages.length - 1].content }),
+})
