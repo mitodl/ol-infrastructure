735bd9c94af77224d4e778cb8d062966706eee8e
Tobias Macey <tmacey@mit.edu>
2025-04-18 10:58:40 -0400

    config: Fix branch targets for UE and MITx Online API client pipelines

--- src/ol_concourse/pipelines/libraries/configuration.py
+++ src/ol_concourse/pipelines/libraries/configuration.py
@@ -11,19 +11,19 @@ PIPELINE_CONFIGS = {
     "mitxonline": {
         "source_repo_name": "mitxonline",
         "source_repo_uri": "https://github.com/mitodl/mitxonline",
-        "source_repo_branch": "main",
+        "source_repo_branch": "release",
         "client_repo_name": "mitxonline-api-clients",
         "client_repo_uri": "git@github.com:mitodl/mitxonline-api-clients.git",
-        "client_repo_branch": "release",
+        "client_repo_branch": "main",
         "client_repo_subpath": "mitxonline-api-axios",
     },
     "unified-ecommerce": {
         "source_repo_name": "unified-ecommerce",
         "source_repo_uri": "https://github.com/mitodl/unified-ecommerce",
-        "source_repo_branch": "main",
+        "source_repo_branch": "release",
         "client_repo_name": "unified-ecommerce-api-clients",
         "client_repo_uri": "git@github.com:mitodl/unified-ecommerce-api-clients.git",
-        "client_repo_branch": "release",
+        "client_repo_branch": "main",
         "client_repo_subpath": "unified-ecommerce-api-axios",
     },
 }
