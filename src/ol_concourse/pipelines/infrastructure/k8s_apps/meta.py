9eb1e6efe3e6bb6a5a0ac5c5e1a1df8a948f7173
Ardiea <mas48@mit.edu>
2025-04-29 12:15:29 -0400

    Adding mitxonline to the metapipeline for k8s apps.

--- src/ol_concourse/pipelines/infrastructure/k8s_apps/meta.py
+++ src/ol_concourse/pipelines/infrastructure/k8s_apps/meta.py
@@ -115,7 +115,7 @@ def meta_pipeline(app_names: list[str]) -> Pipeline:
 
 
 if __name__ == "__main__":
-    app_names = ["learn-ai", "unified-ecommerce", "mit-learn"]
+    app_names = ["learn-ai", "unified-ecommerce", "mit-learn", "mitxonline"]
 
     with open("definition.json", "w") as definition:  # noqa: PTH123
         definition.write(meta_pipeline(app_names).model_dump_json(indent=2))
