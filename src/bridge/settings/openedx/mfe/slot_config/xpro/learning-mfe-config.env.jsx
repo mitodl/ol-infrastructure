c00de681c11485445e11a523d4d7883b1bfe6d8e
Chris Chudzicki <christopher.chudzicki@gmail.com>
2025-05-06 13:26:22 -0400

    do not double bundle smoot

--- src/bridge/settings/openedx/mfe/slot_config/xpro/learning-mfe-config.env.jsx
+++ src/bridge/settings/openedx/mfe/slot_config/xpro/learning-mfe-config.env.jsx
@@ -1,11 +1,18 @@
-import { getConfig } from '@edx/frontend-platform';
 import config from './common-mfe-config.env.jsx';
-import * as remoteTutorDrawer from "./mitodl-smoot-design/dist/bundles/remoteTutorDrawer.umd.js"
 
-
-remoteTutorDrawer.init({
-  messageOrigin: getConfig().LMS_BASE_URL,
-  transformBody: messages => ({ message: messages[messages.length - 1].content }),
+import(
+  /**
+   * remoteTutorDrawer is already bundled to include its own version
+   * of React and ReactDOM.
+   *
+   * Add webpackIgnore to avoid bundling it again.
+   */
+  /* webpackIgnore: true */
+ "/static/remoteTutorDrawer.es.js").then(module => {
+   module.init({
+     messageOrigin: "http://local.openedx.io:8000",
+     transformBody: messages => ({ message: messages[messages.length - 1].content }),
+   })
 })
 
 export default config;
