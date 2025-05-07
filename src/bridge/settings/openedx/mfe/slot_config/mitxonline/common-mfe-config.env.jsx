98846b1b9626cae8bc4d041ce08711061130adf9
Asad Ali <asad.ali@arbisoft.com>
2025-05-07 12:23:24 +0500

    refactor mitxonline cofig

--- src/bridge/settings/openedx/mfe/slot_config/mitxonline/common-mfe-config.env.jsx
+++ src/bridge/settings/openedx/mfe/slot_config/mitxonline/common-mfe-config.env.jsx
@@ -4,88 +4,101 @@ import Footer, { Logo, MenuLinks, CopyrightNotice } from './Footer.jsx';
 
 const configData = getConfig();
 const currentYear = new Date().getFullYear();
-
-let userMenu = [
-  {
-    url: `${process.env.MIT_LEARN_BASE_URL}/dashboard`,
-    title: 'Dashboard',
-  },
-  {
-    url: `${process.env.MIT_LEARN_API_BASE_URL}/learn/logout/`,
-    title: 'Sign Out',
-  },
-];
-
-let footerLegalLinks = [
-  {
-    url: `${process.env.MIT_LEARN_BASE_URL}/about`,
-    title: 'About Us',
-  },
-  {
-    url: `${process.env.MIT_LEARN_BASE_URL}/privacy`,
-    title: 'Privacy Policy',
-  },
-  {
-    url: `${process.env.MIT_LEARN_BASE_URL}/honor`,
-    title: 'Honor Code',
-  },
-  {
-    url: `${process.env.MIT_LEARN_BASE_URL}/terms-of-service`,
-    title: 'Terms of Service',
-  },
-  {
-    url: 'https://accessibility.mit.edu/',
-    title: 'Accessibility',
-  },
-];
+const edxMfeAppName = configData.APP_ID;
+const authoringAppID = "authoring";
+const MITxTCourseKeyFormat = "course-v1:mitxt";
+const isMITxTCourse = window.location.href.toLowerCase().includes(MITxTCourseKeyFormat);
+const accessibilityURL = process.env.ACCESSIBILITY_URL || 'https://accessibility.mit.edu/';
+const linkTitles = {
+  dashboard: "Dashboard",
+  profile: "Profile",
+  account: "Settings",
+  logout: "Sign Out",
+  aboutUs: "About Us",
+  privacyPolicy: "Privacy Policy",
+  honorCode: "Honor Code",
+  termsOfService: "Terms of Service",
+  accessibility: "Accessibility",
+};
 
 let copyRightText = 'Massachusetts Institute of Technology';
 let logo = <Logo imageUrl={process.env.MIT_LEARN_LOGO} destinationUrl={process.env.MIT_LEARN_BASE_URL} />;
-const MITxTCourseKeyFormat = "course-v1:mitxt"
+let userMenu = [
+  {
+    url: `${process.env.MIT_LEARN_BASE_URL}/dashboard`,
+    title: linkTitles.dashboard,
+  },
+  {
+    url: `${process.env.MIT_LEARN_API_BASE_URL}/learn/logout/`,
+    title: linkTitles.logout,
+  },
+];
+let footerLegalLinks = [
+  {
+    url: `${process.env.MIT_LEARN_BASE_URL}/about`,
+    title: linkTitles.aboutUs,
+  },
+  {
+    url: `${process.env.MIT_LEARN_BASE_URL}/privacy`,
+    title: linkTitles.privacyPolicy,
+  },
+  {
+    url: `${process.env.MIT_LEARN_BASE_URL}/honor`,
+    title: linkTitles.honorCode,
+  },
+  {
+    url: `${process.env.MIT_LEARN_BASE_URL}/terms-of-service`,
+    title: linkTitles.termsOfService,
+  },
+  {
+    url: accessibilityURL,
+    title: linkTitles.accessibility,
+  },
+];
 
-if (window.location.href.toLowerCase().includes(MITxTCourseKeyFormat)) {
+if (isMITxTCourse && edxMfeAppName !== authoringAppID) {
   copyRightText = `${configData.SITE_NAME.replace(/\b(CI|QA|Staging)\b/g, "").trim()}. All rights reserved.`;
   logo = <Logo imageUrl={configData.LOGO_URL} destinationUrl={configData.MARKETING_SITE_BASE_URL} />;
 
   userMenu = [
     {
       url: `${configData.MARKETING_SITE_BASE_URL}/dashboard`,
-      title: 'Dashboard',
+      title: linkTitles.dashboard,
     },
     {
       url: `${configData.MARKETING_SITE_BASE_URL}/profile/`,
-      title: 'Profile',
+      title: linkTitles.profile,
     },
     {
       url: `${configData.MARKETING_SITE_BASE_URL}/account-settings/`,
-      title: 'Settings',
+      title: linkTitles.account,
     },
     {
       url: `${configData.LMS_BASE_URL}/logout`,
-      title: 'Sign Out',
+      title: linkTitles.logout,
     },
   ];
 
   footerLegalLinks = [
     {
       url: `${configData.MARKETING_SITE_BASE_URL}/about-us/`,
-      title: 'About Us',
+      title: linkTitles.aboutUs,
     },
     {
       url: `${configData.MARKETING_SITE_BASE_URL}/privacy-policy/`,
-      title: 'Privacy Policy',
+      title: linkTitles.privacyPolicy,
     },
     {
       url: `${configData.MARKETING_SITE_BASE_URL}/honor-code/`,
-      title: 'Honor Code',
+      title: linkTitles.honorCode,
     },
     {
       url: `${configData.MARKETING_SITE_BASE_URL}/terms-of-service/`,
-      title: 'Terms of Service',
+      title: linkTitles.termsOfService,
     },
     {
-      url: 'https://accessibility.mit.edu/',
-      title: 'Accessibility',
+      url: accessibilityURL,
+      title: linkTitles.accessibility,
     },
   ];
 }
@@ -183,12 +196,14 @@ let config = {
 };
 
 // Additional plugin config based on MFE
-const edxMfeAppName = configData.APP_ID;
-
-if (edxMfeAppName === "authoring") {
-  if (!window.location.href.toLowerCase().includes(MITxTCourseKeyFormat)) {
-    mergeConfig({"LOGO_URL": process.env.MIT_LEARN_LOGO});
-  }
+if (edxMfeAppName === authoringAppID) {
+  mergeConfig(
+    {
+      "LOGO_URL": process.env.MIT_LEARN_LOGO,
+      "LOGOUT_URL": `${process.env.MIT_LEARN_API_BASE_URL}/learn/logout/`,
+      "SITE_NAME": "MIT Learn",
+    }
+  );
   config = {
     ...config,
     pluginSlots: {
@@ -234,13 +249,13 @@ if (learningApps.includes(edxMfeAppName)) {
   };
 }
 
-const modifyLogoHref = ( widget ) => {
+const MITLearnLogo = ( widget ) => {
   widget.content.href = process.env.MIT_LEARN_BASE_URL;
   widget.content.src = process.env.MIT_LEARN_LOGO;
   return widget;
 };
 
-if (!window.location.href.toLowerCase().includes(MITxTCourseKeyFormat)) {
+if (!isMITxTCourse) {
   config.pluginSlots = {
     ...config.pluginSlots,
     logo_slot: {
@@ -249,7 +264,7 @@ if (!window.location.href.toLowerCase().includes(MITxTCourseKeyFormat)) {
         {
           op: PLUGIN_OPERATIONS.Modify,
           widgetId: 'default_contents',
-          fn: modifyLogoHref,
+          fn: MITLearnLogo,
         },
       ]
     },
