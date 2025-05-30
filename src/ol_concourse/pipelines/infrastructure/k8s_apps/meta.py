91a55efc99aad02ec2585b6aa49a38836cb91c8e
Ardiea <mas48@mit.edu>
2025-05-30 10:24:51 -0400

    Revert "Updated the k8s_apps pipeline to work for nextjs. Required some extensive refactoring. (#3225)"
    This reverts commit a7d514f8417d2e6508556d7be5a73bc88014d057.

--- src/ol_concourse/pipelines/infrastructure/k8s_apps/meta.py
+++ src/ol_concourse/pipelines/infrastructure/k8s_apps/meta.py
@@ -17,7 +17,7 @@ from ol_concourse.lib.models.pipeline import (
 from ol_concourse.lib.resources import git_repo
 
 
-def meta_job(app_name: str, deployment_config: dict[str, str]) -> Job:
+def meta_job(app_name: str) -> Job:
     return Job(
         name=Identifier(f"create-{app_name}-pipeline"),
         plan=[
@@ -45,8 +45,6 @@ def meta_job(app_name: str, deployment_config: dict[str, str]) -> Job:
                         args=[
                             "../k8s-app-pipeline-definitions/src/ol_concourse/pipelines/infrastructure/k8s_apps/docker_pulumi.py",
                             app_name,
-                            deployment_config["repo_name"],
-                            deployment_config["dockerfile_path"],
                         ],
                     ),
                 ),
@@ -59,7 +57,7 @@ def meta_job(app_name: str, deployment_config: dict[str, str]) -> Job:
     )
 
 
-def meta_pipeline(app_configs: dict[str, dict[str, str]]) -> Pipeline:
+def meta_pipeline(app_names: list[str]) -> Pipeline:
     pipeline_definitions = git_repo(
         name=Identifier("k8s-app-pipeline-definitions"),
         uri="https://github.com/mitodl/ol-infrastructure",
@@ -70,12 +68,7 @@ def meta_pipeline(app_configs: dict[str, dict[str, str]]) -> Pipeline:
             "src/ol_concourse/pipelines/constants.py",
         ],
     )
-    pipeline_jobs = [
-        meta_job(app_name, deployment_config)
-        for app_name, deployment_config in app_configs.items()
-    ]
-    app_names = list(app_configs.keys())
-
+    pipeline_jobs = [meta_job(app_name) for app_name in app_names]
     pipeline_jobs.append(
         Job(
             name=Identifier("set-k8s-app-meta-pipeline"),
@@ -122,29 +115,8 @@ def meta_pipeline(app_configs: dict[str, dict[str, str]]) -> Pipeline:
 
 
 if __name__ == "__main__":
-    app_configs = {
-        "learn-ai": {
-            "repo_name": "learn-ai",
-            "dockerfile_path": "./Dockerfile",
-        },
-        "unified-ecommerce": {
-            "repo_name": "unified-ecommerce",
-            "dockerfile_path": "./Dockerfile",
-        },
-        "mit-learn": {
-            "repo_name": "mit-learn",
-            "dockerfile_path": "./Dockerfile",
-        },
-        "mit-learn-nextjs": {
-            "repo_name": "mit-learn",
-            "dockerfile_path": "frontends/main/Dockerfile.web",
-        },
-        "mitxonline": {
-            "repo_name": "mitxonline",
-            "dockerfile_path": "./Dockerfile",
-        },
-    }
+    app_names = ["learn-ai", "unified-ecommerce", "mit-learn", "mitxonline"]
 
     with open("definition.json", "w") as definition:  # noqa: PTH123
-        definition.write(meta_pipeline(app_configs).model_dump_json(indent=2))
-    sys.stdout.write(meta_pipeline(app_configs).model_dump_json(indent=2))
+        definition.write(meta_pipeline(app_names).model_dump_json(indent=2))
+    sys.stdout.write(meta_pipeline(app_names).model_dump_json(indent=2))
