64aa1d31a7a1e0752494d059ffa1abe4673cd3a3
Chris Patti <feoh@feoh.org>
2025-06-03 18:35:44 -0400

    mitodl/hq#7185
    feat: Underpinnings for Vault static secrets.

--- src/ol_infrastructure/applications/botkube/__main__.py
+++ src/ol_infrastructure/applications/botkube/__main__.py
@@ -1,8 +1,19 @@
 # ruff: noqa: CPY001, D100
+import json
+from pathlib import Path
+
 import pulumi_kubernetes as kubernetes
-from pulumi import Config, StackReference
+import pulumi_vault as vault
+from pulumi import Config, ResourceOptions, StackReference
 
 from bridge.lib.versions import BOTKUBE_CHART_VERSION
+from bridge.secrets.sops import read_yaml_secrets
+from ol_infrastructure.components.services.vault import (
+    OLVaultK8SResources,
+    OLVaultK8SResourcesConfig,
+    OLVaultK8SSecret,
+    OLVaultK8SStaticSecretConfig,
+)
 from ol_infrastructure.lib.aws.eks_helper import (
     check_cluster_namespace,
     setup_k8s_provider,
@@ -19,9 +30,11 @@ from ol_infrastructure.lib.pulumi_helper import parse_stack
 stack_info = parse_stack()
 
 botkube_config = Config("config_botkube")
+vault_config = Config("vault")
 opensearch_stack = StackReference(
     f"infrastructure.aws.opensearch.apps.{stack_info.name}"
 )
+
 opensearch_cluster = opensearch_stack.require_output("cluster")
 
 cluster_stack = StackReference(f"infrastructure.aws.eks.applications.{stack_info.name}")
@@ -30,15 +43,112 @@ aws_config = AWSBase(
     tags={"OU": BusinessUnit.operations, "Environment": Environment.applications},
 )
 
+botkube_namespace = "botkube-namespace"
+
 k8s_global_labels = K8sGlobalLabels(
     service=Services.botkube,
     ou=BusinessUnit.data,
     stack=stack_info,
 ).model_dump()
 
+# Begin vault hoo-ha.
+botkube_vault_secrets = read_yaml_secrets(
+    Path(f"botkube/secrets.{stack_info.env_suffix}.yaml"),
+)
+
+botkube_vault_mount = vault.Mount(
+    f"botkube-secrets-mount-{stack_info.env_suffix}",
+    path="secret-botkube",
+    type="kv-v2",
+    options={"version": "2"},
+    description="Secrets for the learn ai application.",
+    opts=ResourceOptions(delete_before_replace=True),
+)
+botkube_static_vault_secrets = vault.generic.Secret(
+    f"botkube-secrets-{stack_info.env_suffix}",
+    path=botkube_vault_mount.path.apply("{}/secrets".format),
+    data_json=json.dumps(botkube_vault_secrets),
+)
+botkube_static_vault_secrets = vault.generic.Secret(
+    f"botkube-secrets-{stack_info.env_suffix}",
+    path=botkube_vault_mount.path.apply("{}/secrets".format),
+    data_json=json.dumps(botkube_vault_secrets),
+)
+
+botkube_vault_policy = vault.Policy(
+    f"botkube-vault-policy-{stack_info.env_suffix}",
+    name="botkube",
+    policy=Path(__file__).parent.joinpath("botkube_policy.hcl").read_text(),
+)
+
+botkube_vault_auth_backend_role = vault.kubernetes.AuthBackendRole(
+    f"botkube-vault-auth-backend-role-{stack_info.env_suffix}",
+    role_name="botkube",
+    backend=cluster_stack.require_output("vault_auth_endpoint"),
+    bound_service_account_names=["*"],
+    bound_service_account_namespaces=[botkube_namespace],
+    token_policies=[botkube_vault_policy.name],
+)
+
+# Stopped at: Make a kubernetes auth backend role that uses the policy we just installed
+vault_k8s_resources_config = OLVaultK8SResourcesConfig(
+    application_name="botkube",
+    namespace=botkube_namespace,
+    labels=k8s_global_labels,
+    vault_address=vault_config.require("address"),
+    vault_auth_endpoint=cluster_stack.require_output("vault_auth_endpoint"),
+    vault_auth_role_name=botkube_vault_auth_backend_role.role_name,
+)
+
+vault_k8s_resources = OLVaultK8SResources(
+    resource_config=vault_k8s_resources_config,
+    opts=ResourceOptions(
+        delete_before_replace=True,
+        depends_on=[botkube_vault_auth_backend_role],
+    ),
+)
+
+# Load the static secrets into a k8s secret via VSO
+static_secrets_name = "botkube-static-secrets"  # pragma: allowlist secret
+static_secrets = OLVaultK8SSecret(
+    name=f"botkube-{stack_info.env_suffix}-static-secrets",
+    resource_config=OLVaultK8SStaticSecretConfig(
+        name="botkube-static-secrets",
+        namespace=botkube_namespace,
+        labels=k8s_global_labels,
+        dest_secret_name=static_secrets_name,
+        dest_secret_labels=k8s_global_labels,
+        mount="secret-botkube",
+        mount_type="kv-v2",
+        path="secrets",
+        includes=["*"],
+        excludes=[],
+        exclude_raw=True,
+        vaultauth=vault_k8s_resources.auth_name,
+    ),
+    opts=ResourceOptions(
+        delete_before_replace=True,
+        parent=vault_k8s_resources,
+        depends_on=[botkube_static_vault_secrets],
+    ),
+)
+# end Vault hoo-ha
+
+botkube_config = Config("config_botkube")
+opensearch_stack = StackReference(
+    f"infrastructure.aws.opensearch.apps.{stack_info.name}"
+)
+
+opensearch_cluster = opensearch_stack.require_output("cluster")
+
+cluster_stack = StackReference(f"infrastructure.aws.eks.applications.{stack_info.name}")
+
+aws_config = AWSBase(
+    tags={"OU": BusinessUnit.operations, "Environment": Environment.applications},
+)
+
 setup_k8s_provider(kubeconfig=cluster_stack.require_output("kube_config"))
 
-botkube_namespace = "botkube-namespace"
 cluster_stack.require_output("namespaces").apply(
     lambda ns: check_cluster_namespace(botkube_namespace, ns)
 )
