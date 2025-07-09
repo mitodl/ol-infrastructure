79bf5a877ddba60e4febefb7ea924e5dfc0bb9fe
Tobias Macey <tmacey@mit.edu>
2025-07-09 16:37:24 -0400

    config: Match SPI flag format changes in 26.3 that are used in Docker build

--- src/ol_infrastructure/applications/keycloak/__main__.py
+++ src/ol_infrastructure/applications/keycloak/__main__.py
@@ -500,7 +500,7 @@ keycloak_resource = kubernetes.apiextensions.CustomResource(
             },
             {"name": "metrics-enabled", "value": "true"},
             {
-                "name": "spi-login-provider",
+                "name": "spi-login--provider",
                 "value": "ol-freemarker",
             },
             {
