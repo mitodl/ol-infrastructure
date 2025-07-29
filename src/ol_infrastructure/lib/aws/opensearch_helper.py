7e6b2d3f660b137101890658ad18aadfdaa20acf
Tobias Macey <tmacey@mit.edu>
2025-07-29 15:51:44 -0400

    config: Update instance types and defaults for OpenSearch clusters

---
+++ src/ol_infrastructure/lib/aws/opensearch_helper.py
@@ -0,0 +1,11 @@
+from enum import Enum, unique
+
+
+@unique
+class SearchInstanceTypes(str, Enum):
+    small = "t3.small.search"
+    medium = "t3.medium.search"
+    general_purpose_large = "m7g.large.search"
+    general_purpose_xlarge = "m7g.xlarge.search"
+    high_mem_regular = "r7g.large.search"
+    high_mem_xlarge = "r7g.2xlarge.search"
