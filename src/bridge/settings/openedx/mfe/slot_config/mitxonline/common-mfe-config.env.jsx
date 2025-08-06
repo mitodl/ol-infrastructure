416170f88ca7187574ea5912386cc41831caf413
Muhammad Anas <muhammad.anas@arbisoft.com>
2025-08-06 14:57:55 +0500

    refactor: move the config up

--- src/bridge/settings/openedx/mfe/slot_config/mitxonline/common-mfe-config.env.jsx
+++ src/bridge/settings/openedx/mfe/slot_config/mitxonline/common-mfe-config.env.jsx
@@ -172,6 +172,10 @@ const footerSlotConfig = {
 let config = {
   ...process.env,
   pluginSlots: footerSubSlotsConfig,
+  // Override the proctoring info panel 'Review instructions and system requirements' link
+  externalLinkUrlOverrides : {
+    "https://support.edx.org/hc/en-us/sections/115004169247-Taking-Timed-and-Proctored-Exams": "https://mitxonline.zendesk.com/hc/en-us/articles/4418223178651-What-is-the-Proctortrack-Onboarding-Exam",
+  },
 };
 
 // Additional plugin config based on MFE
@@ -219,14 +223,6 @@ if (learningApps.includes(edxMfeAppName)) {
       },
     ],
   };
-}
-
-// Override the proctoring info panel 'Review instructions and system requirements' link
-config = {
-  ...config,
-  externalLinkUrlOverrides : {
-    "https://support.edx.org/hc/en-us/sections/115004169247-Taking-Timed-and-Proctored-Exams": "https://mitxonline.zendesk.com/hc/en-us/articles/4418223178651-What-is-the-Proctortrack-Onboarding-Exam",
-  },
 }
 
 export default config;
