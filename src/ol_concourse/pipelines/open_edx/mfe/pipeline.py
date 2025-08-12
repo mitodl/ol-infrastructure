fc5eedbf59d7d612b51585f5fa71f9c9773d6b2f
Muhammad Anas <muhammad.anas@arbisoft.com>
2025-08-12 20:07:52 +0500

    conf: turned off enable jumpnav to not show dropdown on breadcrump link click

--- src/ol_concourse/pipelines/open_edx/mfe/pipeline.py
+++ src/ol_concourse/pipelines/open_edx/mfe/pipeline.py
@@ -65,6 +65,7 @@ class OpenEdxVars(BaseModel):
     enable_video_upload_page_link_in_content_dropdown: (
         Literal["true", "false"] | None
     ) = None
+    enable_jumpnav: Literal["true", "false"] | None = None
 
     @property
     def release_name(self) -> OpenEdxSupportedRelease:
@@ -122,6 +123,7 @@ def mfe_params(
         "USER_INFO_COOKIE_NAME": f"{open_edx.environment}-edx-user-info",
         "ENABLE_VIDEO_UPLOAD_PAGE_LINK_IN_CONTENT_DROPDOWN": open_edx.enable_video_upload_page_link_in_content_dropdown,  # noqa: E501
         "PARAGON_THEME_URLS": "{}",
+        "ENABLE_JUMPNAV": open_edx.enable_jumpnav,
     }
 
 
