d36a3312d2bb32ae256590d2adcbcb65a805307e
Tobias Macey <tmacey@mit.edu>
2025-08-14 17:15:11 -0400

    config: Update to use Airbyte helm chart v2

--- src/bridge/lib/versions.py
+++ src/bridge/lib/versions.py
@@ -19,8 +19,8 @@ TUTOR_PERMISSIONS_VERSION = "15.3.4"
 VAULT_VERSION = "1.20.3"
 
 # EKS Specific Versions
-# renovate: datasource=helm depName=airbyte packageName=airbyte registryUrl=https://airbytehq.github.io/helm-charts
-AIRBYTE_CHART_VERSION = "1.8.2"
+# renovate: datasource=helm depName=airbyte packageName=airbyte registryUrl=https://airbytehq.github.io/charts
+AIRBYTE_CHART_VERSION = "2.0.7"
 # renovate: datasource=docker depName=apisix packageName=registry-1.docker.io/bitnamicharts/apisix
 APISIX_CHART_VERSION = "5.1.6"
 # renovate: datasource=helm depName=aws-load-balancer-controller packageName=eks/aws-load-balancer-controller registryUrl=https://aws.github.io/eks-charts
