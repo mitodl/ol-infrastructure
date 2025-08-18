bced451f76b512d6047d87a7d670adc25078f153
Chris Patti <feoh@feoh.org>
2025-08-18 16:24:14 -0400

    Add an API token requirement for Canvas Tutor (#3482)
    * Revert "fix: Extend the API token requirements"
    
    This reverts commit 84d6f6225b4e0c12828aba642819be138093b630.
    
    * fix: Add an API key for canvas tutor agent.

--- src/ol_infrastructure/applications/learn_ai/__main__.py
+++ src/ol_infrastructure/applications/learn_ai/__main__.py
@@ -907,7 +907,24 @@ mit_learn_learn_ai_https_apisix_route = OLApisixRoute(
                 ),
             ],
             hosts=[learn_api_domain],
-            paths=["/ai/http/canvas_*"],
+            paths=["/ai/http/canvas_syllabus_agent/*"],
+            backend_service_name=learn_ai_app_k8s.application_lb_service_name,
+            backend_service_port=learn_ai_app_k8s.application_lb_service_port_name,
+            backend_resolve_granularity="service",
+        ),
+        OLApisixRouteConfig(
+            route_name="canvas_tutor_agent",
+            priority=20,
+            plugins=[
+                OLApisixPluginConfig(
+                    name="key-auth",
+                    config={
+                        "header": "canvas_token",
+                    },
+                ),
+            ],
+            hosts=[learn_api_domain],
+            paths=["/ai/http/canvas_tutor_agent/*"],
             backend_service_name=learn_ai_app_k8s.application_lb_service_name,
             backend_service_port=learn_ai_app_k8s.application_lb_service_port_name,
             backend_resolve_granularity="service",
