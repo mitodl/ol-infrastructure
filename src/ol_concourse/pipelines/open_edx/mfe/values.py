b391f45f84d5a5d914e74021031ad5de0a81e0a0
Muhammad Anas <muhammad.anas@arbisoft.com>
2025-08-18 13:21:51 +0500

    config: set enable_jumpnav to true

--- src/ol_concourse/pipelines/open_edx/mfe/values.py
+++ src/ol_concourse/pipelines/open_edx/mfe/values.py
@@ -124,7 +124,7 @@ mitxonline = [
         trademark_text="© MITx Online. All rights reserved except where noted.",
         logo_trademark_url="https://courses.ci.mitxonline.mit.edu/static/mitxonline/images/mit-logo.svg",
         enable_video_upload_page_link_in_content_dropdown="true",
-        enable_jumpnav="false",
+        enable_jumpnav="true",
     ),
     OpenEdxVars(
         about_us_url="https://rc.mitxonline.mit.edu/about-us/",
@@ -151,7 +151,7 @@ mitxonline = [
         trademark_text="© MITx Online. All rights reserved except where noted.",
         logo_trademark_url="https://courses.rc.mitxonline.mit.edu/static/mitxonline/images/mit-logo.svg",
         enable_video_upload_page_link_in_content_dropdown="true",
-        enable_jumpnav="false",
+        enable_jumpnav="true",
     ),
     OpenEdxVars(
         about_us_url="https://mitxonline.mit.edu/about-us/",
@@ -178,7 +178,7 @@ mitxonline = [
         trademark_text="© MITx Online. All rights reserved except where noted.",
         logo_trademark_url="https://courses.mitxonline.mit.edu/static/mitxonline/images/mit-logo.svg",
         enable_video_upload_page_link_in_content_dropdown="true",
-        enable_jumpnav="false",
+        enable_jumpnav="true",
     ),
 ]
 
