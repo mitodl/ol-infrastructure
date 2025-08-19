0dcd7d6f4cf29301383de6a60cb095f5f1468e44
Tobias Macey <tmacey@mit.edu>
2025-08-19 15:38:50 -0400

    chore: Remove extraneous guard clauses

--- src/ol_infrastructure/components/aws/database.py
+++ src/ol_infrastructure/components/aws/database.py
@@ -252,12 +252,7 @@ class OLAmazonDB(pulumi.ComponentResource):
             )
         ):
             db_instance_identifier = current_db_state.get("DBInstanceIdentifier")
-            if db_instance_identifier is not None:
-                turn_off_deletion_protection(db_instance_identifier)
-            else:
-                pulumi.log.warn(
-                    "Could not turn off deletion protection: DBInstanceIdentifier is None."  # noqa: E501
-                )
+            turn_off_deletion_protection(db_instance_identifier)
             deletion_protection_for_primary = False
             custom_timeouts = pulumi.CustomTimeouts(
                 create=f"{db_config.blue_green_timeout_minutes}m",
