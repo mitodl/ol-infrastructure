b67eec11ceb4fac4bda8cc65ed1e80e7fdcdd304
dansubak <dansubak@mit.edu>
2025-08-20 11:25:27 -0400

    Use new fancy private key

--- src/ol_concourse/pipelines/container_images/jupyter_courses.py
+++ src/ol_concourse/pipelines/container_images/jupyter_courses.py
@@ -51,7 +51,7 @@ course_repository = ssh_git_repo(
     name=Identifier("course_name"),
     uri="((course_repo))",
     branch="main",
-    private_key="((github_enterprise_ssh_key))",
+    private_key="((ol_notebooks_private_ssh_key))",
 )
 
 # Shouldn't need the AWS account ID as it'll infer it from the host's creds. Docs are a bit squiggy,
