df58ec959ff98afd870a0b1081794d0217bb3f64
Tobias Macey <tmacey@mit.edu>
2025-08-21 13:58:51 -0400

    refactor: Move redirect dict to JSON blob and only apply in production

--- src/ol_infrastructure/applications/fastly_redirector/__main__.py
+++ src/ol_infrastructure/applications/fastly_redirector/__main__.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 import pulumi
@@ -6,6 +7,7 @@ from pulumi import Config
 from pulumi_aws import route53
 
 from bridge.lib.constants import FASTLY_A_TLS_1_3, FASTLY_CNAME_TLS_1_3
+from bridge.lib.magic_numbers import FIVE_MINUTES
 from ol_infrastructure.lib.aws.route53_helper import (
     fastly_certificate_validation_records,
     is_root_domain,
@@ -66,46 +68,26 @@ ol_redirect_service = fastly.ServiceVcl(
     opts=pulumi.ResourceOptions(protect=True).merge(fastly_provider),
 )
 
-ol_redirect_service_redirects_dictionary = fastly.ServiceDictionaryItems(
-    "ol-redirect-service-redirects-dictionary",
-    dictionary_id=ol_redirect_service.dictionaries.apply(
-        lambda dicts: next((d["id"] for d in dicts if d["name"] == "redirects"), None)
-    ),
-    items={
-        "/": "301|keep|https://docs.google.com/forms/d/e/1FAIpQLSdvkI2cPG1iMM4gN_KyKem4fNLh4irWzrmjX-JhcFXa51su5g/viewform?fbzx=2658557852628862500",
-        "/courses/6/6.045/2015spring/L01/MIT-6.045-lec-mit-0000-2015apr07-0929-L01/": "301|keep|https://video.odl.mit.edu/videos/65da1d1388ce400983c9447f75883760/?start=278",
-        "/courses/6/6.045/2015spring/L01/MIT-6.045-lec-mit-0000-2015apr09-0929-L01/": "301|keep|https://video.odl.mit.edu/videos/33abf21f4e174e6e8f21a5071c30a66f/?start=250",
-        "/courses/6/6.045/2015spring/L01/MIT-6.045-lec-mit-0000-2015apr14-0929-L01/": "301|keep|https://video.odl.mit.edu/videos/b5cc48724aa5405c9fd7b1211a2b0b3c/",
-        "/courses/6/6.045/2015spring/L01/MIT-6.045-lec-mit-0000-2015apr16-0929-L01/": "301|keep|https://video.odl.mit.edu/videos/201c33a2dab349359f5af23a293d9c5a/?start=333",
-        "/courses/6/6.045/2015spring/L01/MIT-6.045-lec-mit-0000-2015apr23-0929-L01/": "301|keep|https://video.odl.mit.edu/videos/9ea370cd18b14656a712e7cc723cf7a9/?start=393",
-        "/courses/6/6.045/2015spring/L01/MIT-6.045-lec-mit-0000-2015apr28-0929-L01/": "301|keep|https://video.odl.mit.edu/videos/59219f99dd30490e9586ad960ad2691f/?start=463",
-        "/courses/6/6.045/2015spring/L01/MIT-6.045-lec-mit-0000-2015apr30-0929-L01/": "301|keep|https://video.odl.mit.edu/videos/4656112ede89496fb100fe10b02fa304/?start=396",
-        "/courses/6/6.045/2015spring/L01/MIT-6.045-lec-mit-0000-2015may05-0929-L01/": "301|keep|https://video.odl.mit.edu/videos/b5cc48724aa5405c9fd7b1211a2b0b3c/",
-        "/courses/6/6.045/2015spring/L01/MIT-6.045-lec-mit-0000-2015may07-0929-L01/": "301|keep|https://video.odl.mit.edu/videos/c5691b38739d4a43a232bf3ef07f1646/?start=504",
-        "/courses/6/6.045/2015spring/L01/MIT-6.045-lec-mit-0000-2015may14-0929-L01/": "301|keep|https://video.odl.mit.edu/videos/ef3aa2b5dbb14adc9c15452b0d27c436/?start=398",
-        "/courses/6/6.045/2015spring/MIT-6.045-lec-mit-0000-2015feb03-0929-L01/": "301|keep|https://video.odl.mit.edu/videos/7a11f55dd99d49349f377d2d87f0aef2/?start=341",
-        "/courses/6/6.045/2015spring/MIT-6.045-lec-mit-0000-2015feb05-0929-L01/": "301|keep|https://video.odl.mit.edu/videos/2f043945501941e9a0fe35e5a4bbf782/",
-        "/courses/6/6.045/2015spring/MIT-6.045-lec-mit-0000-2015feb12-0929-L01/": "301|keep|https://video.odl.mit.edu/videos/5d64d5e504c140a19efb1b69b5385482/",
-        "/courses/6/6.045/2015spring/MIT-6.045-lec-mit-0000-2015feb19-0929-L01/": "301|keep|https://video.odl.mit.edu/videos/9178561d53b5460db360c72bd9dec2b5/?start=430",
-        "/courses/6/6.045/2015spring/MIT-6.045-lec-mit-0000-2015feb24-0929-L01/": "301|keep|https://video.odl.mit.edu/videos/c973ebdcbca34ecf868cc12a0344d8f5/?start=268",
-        "/courses/6/6.045/2015spring/MIT-6.045-lec-mit-0000-2015feb26-0929-L01/": "301|keep|https://video.odl.mit.edu/videos/7dcf75a64fa54356bc2e6f0a8e38fc79/?start=272",
-        "/courses/6/6.045/2015spring/MIT-6.045-lec-mit-0000-2015mar03-0929-L01/": "301|keep|https://video.odl.mit.edu/videos/bc78f4af98e9492cbef7278d92156b92/?start=170",
-        "/courses/6/6.045/2015spring/MIT-6.045-lec-mit-0000-2015mar05-0929-L01/": "301|keep|https://video.odl.mit.edu/videos/bc78f4af98e9492cbef7278d92156b92/?start=170",
-        "/courses/6/6.045/2015spring/MIT-6.045-lec-mit-0000-2015mar10-0929-L01/": "301|keep|https://video.odl.mit.edu/videos/d7248606616240dca81337d163a28055/?start=172",
-        "/courses/6/6.045/2015spring/MIT-6.045-lec-mit-0000-2015mar12-0929-L01/": "301|keep|https://video.odl.mit.edu/videos/08eaaeb932d54d119e529deac416e804/?start=331",
-        "/courses/6/6.045/2015spring/MIT-6.045-lec-mit-0000-2015mar17-0929-L01/": "301|keep|https://video.odl.mit.edu/videos/3d86fd1c074840988b4480356e2d4746/?start=290",
-        "/courses/6/6.045/2015spring/MIT-6.045-lec-mit-0000-2015mar19-0929-L01/": "301|keep|https://video.odl.mit.edu/videos/4c89dc64071f4c5ea5f948b80a2201bc/?start=128",
-        "/courses/6/6.045/2015spring/MIT-6.045-lec-mit-0000-2015mar31-0929-L01/": "301|keep|https://video.odl.mit.edu/videos/5605b2609dcc4661b7aeb0c18645d5c9/?start=301",
-    },
-    service_id=ol_redirect_service.id,
-    opts=pulumi.ResourceOptions(protect=True).merge(fastly_provider),
-)
+if stack_info.env_suffix == "production":
+    ol_redirect_service_redirects_dictionary = fastly.ServiceDictionaryItems(
+        "ol-redirect-service-redirects-dictionary",
+        dictionary_id=ol_redirect_service.dictionaries.apply(
+            lambda dicts: next(
+                (d.dictionary_id for d in dicts if d.name == "redirects"), None
+            )
+        ),
+        items=json.loads(Path("ovs_redirect_dict.json").read_text()),
+        service_id=ol_redirect_service.id,
+        opts=pulumi.ResourceOptions(protect=True).merge(fastly_provider),
+    )
 
 ol_redirect_service_tls = fastly.TlsSubscription(
     "ol-redirect-service-tls-subscription",
     # valid values are certainly, lets-encrypt, or globalsign
     certificate_authority="certainly",
-    domains=list(redirect_domains),
+    domains=[
+        domain for domain in redirect_domains if lookup_zone_id_from_domain(domain)
+    ],
     opts=fastly_provider,
 )
 
@@ -115,7 +97,8 @@ ol_redirect_service_tls.managed_dns_challenges.apply(
 
 validated_tls_subscription = fastly.TlsSubscriptionValidation(
     "ol-redirect-service-tls-subscription-validation",
-    subscription_id=ol_redirect_service.id,
+    subscription_id=ol_redirect_service_tls.id,
+    opts=fastly_provider,
 )
 
 for domain in redirect_domains:
@@ -128,5 +111,6 @@ for domain in redirect_domains:
             type=record_type,
             records=[str(record) for record in record_map[record_type]],
             allow_overwrite=True,
+            ttl=FIVE_MINUTES,
             zone_id=zone_id,
         )
