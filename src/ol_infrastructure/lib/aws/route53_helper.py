b408aadeecda9eb70112388c4553f7d6fa93bf43
Tobias Macey <tmacey@mit.edu>
2025-08-21 13:20:01 -0400

    feat: Put Fastly redirect service under code management

--- src/ol_infrastructure/lib/aws/route53_helper.py
+++ src/ol_infrastructure/lib/aws/route53_helper.py
@@ -1,5 +1,8 @@
+from functools import lru_cache
+
 import boto3
 import pulumi
+import pulumi_fastly as fastly
 from pulumi_aws import route53
 from pulumi_aws.acm.outputs import CertificateDomainValidationOption
 
@@ -9,13 +12,33 @@ FIVE_MINUTES = 60 * 5
 route53_client = boto3.client("route53")
 
 
-def lookup_zone_id_from_domain(domain: str) -> str:
-    zone = route53_client.list_hosted_zones_by_name(
-        DNSName=".".join(domain.split(".")[-3:])
-    )["HostedZones"][0]
-    return zone["Id"].split("/")[
-        -1
-    ]  # 'Id' attribute is of the form /hostedzone/<ZONE_ID>
+@lru_cache
+def zone_id_map() -> dict[str, str]:
+    zones_by_domain = {}
+    zone_kwargs: dict[str, str] = {"MaxItems": "100"}
+    while True:
+        zone_list = route53_client.list_hosted_zones_by_name(**zone_kwargs)
+        if not zone_list["HostedZones"]:
+            break
+        for zone in zone_list["HostedZones"]:
+            zones_by_domain[zone["Name"].strip(".")] = zone["Id"]
+        if zone_list["IsTruncated"]:
+            zone_kwargs = {
+                "DNSName": zone_list["NextDNSName"],
+                "HostedZoneId": zone_list["NextHostedZoneID"],
+                "MaxItems": "100",
+            }
+        else:
+            break
+    return zones_by_domain
+
+
+def lookup_zone_id_from_domain(domain: str) -> str | None:
+    zones_by_domain = zone_id_map()
+    for zone in zones_by_domain:
+        if domain.endswith(zone):
+            return zones_by_domain[zone].split("/")[-1]
+    return None
 
 
 def zone_opts(domain: str) -> pulumi.ResourceOptions:
@@ -76,3 +99,33 @@ def acm_certificate_validation_records(
             )
         )
     return records_array
+
+
+def fastly_certificate_validation_records(
+    validation_options: list[fastly.TlsSubscriptionManagedDnsChallengeArgs],
+    opts: pulumi.ResourceOptions | None = None,
+) -> list[route53.Record]:
+    records_array = []
+    for index, challenge in enumerate(validation_options):
+        records_array.append(
+            route53.Record(
+                f"fastly-cert-validation-route53-record-{index}",
+                name=challenge.record_name,
+                zone_id=lookup_zone_id_from_domain(challenge.record_name),
+                type=challenge.record_type,
+                records=[challenge.record_value],
+                ttl=FIVE_MINUTES,
+                allow_overwrite=True,
+                opts=opts,
+            )
+        )
+    return records_array
+
+
+def is_root_domain(domain: str) -> bool:
+    zones = zone_id_map()
+    try:
+        zones[domain]
+    except KeyError:
+        return False
+    return True
