f2329f6b608a3124bac5aa6a9f092753ef96e539
dansubak <dansubak@mit.edu>
2025-08-28 09:01:18 -0400

    Remove profile list

--- src/ol_infrastructure/applications/jupyterhub/dynamicImageConfig.py
+++ src/ol_infrastructure/applications/jupyterhub/dynamicImageConfig.py
@@ -1,35 +1,3 @@
-# c.KubeSpawner.profile_list = [
-#     {
-#         "display_name": "Clustering and Descriptive AI",
-#         "description": "Clustering and Descriptive AI",
-#         "kubespawner_override": {
-#             "image": "610119931565.dkr.ecr.us-east-1.amazonaws.com/ol-course-notebooks:clustering_and_descriptive_ai",
-#         },
-#     },
-#     {
-#         "display_name": "Deep Learning Foundations and Applications",
-#         "description": "Deep Learning Foundations and Applications",
-#         "kubespawner_override": {
-#             "image": "610119931565.dkr.ecr.us-east-1.amazonaws.com/ol-course-notebooks:deep_learning_foundations_and_applications",
-#         },
-#     },
-#     {
-#         "display_name": "Supervised Learning Fundamentals",
-#         "description": "Supervised Learning Fundamentals",
-#         "kubespawner_override": {
-#             "image": "610119931565.dkr.ecr.us-east-1.amazonaws.com/ol-course-notebooks:supervised_learning_fundamentals",
-#         },
-#     },
-#     {
-#         "display_name": "Introduction to Data Analytics and Machine Learning",
-#         "description": "Introduction to Data Analytics and Machine Learning",
-#         "kubespawner_override": {
-#             "image": "610119931565.dkr.ecr.us-east-1.amazonaws.com/ol-course-notebooks:introduction_to_data_analytics_and_machine_learning",
-#         },
-#     },
-# ]
-#
-
 import uuid
 
 from jupyterhub.auth import Authenticator
