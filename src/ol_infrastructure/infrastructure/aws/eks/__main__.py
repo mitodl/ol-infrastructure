e1103954ac5348dc37c66e8ed96d39d88641ee4d
Mike Davidson <41908054+Ardiea@users.noreply.github.com>
2025-09-05 12:51:14 -0400

    Md/refresh stability (#3547)
    * Updated min/max/desired config for applications core node configurations.
    
    * fix: coredns resiliency updates.

--- src/ol_infrastructure/infrastructure/aws/eks/__main__.py
+++ src/ol_infrastructure/infrastructure/aws/eks/__main__.py
@@ -33,6 +33,7 @@ from bridge.lib.versions import (
     VAULT_SECRETS_OPERATOR_CHART_VERSION,
 )
 from ol_infrastructure.components.aws.eks import OLEKSTrustRole, OLEKSTrustRoleConfig
+from ol_infrastructure.infrastructure.aws.eks.core_dns import create_core_dns_resources
 from ol_infrastructure.lib.aws.eks_helper import (
     ECR_DOCKERHUB_REGISTRY,
     get_cluster_version,
@@ -2028,4 +2029,11 @@ metrics_server_release = kubernetes.helm.v3.Release(
         depends_on=[node_groups[0]],
         delete_before_replace=True,
     ),
+)
+
+create_core_dns_resources(
+    cluster_name=cluster_name,
+    k8s_global_labels=k8s_global_labels,
+    k8s_provider=k8s_provider,
+    cluster=cluster,
 )
