1d8a0ab2d4cb8cc7696b150996b4c3691c61761a
Asad Ali <asad.ali@arbisoft.com>
2025-09-10 17:51:31 +0500

    fix: use getconfig

--- src/bridge/settings/openedx/mfe/slot_config/authoring-mfe-config.env.jsx
+++ src/bridge/settings/openedx/mfe/slot_config/authoring-mfe-config.env.jsx
@@ -1,10 +1,12 @@
+import { getConfig } from '@edx/frontend-platform';
+
 import config from './common-mfe-config.env.jsx';
 
 // We use postMessage to communicate between the iframe and the parent window.
 // Listen for messages from xBlock Asides to trigger a course refresh when Aside state changes.
 // Uses localStorage and the StorageEvent to notify the authoring MFE to refresh the course data.
 window.addEventListener("message", function (event) {
-  if (event.origin !== configData.STUDIO_BASE_URL) return;
+  if (event.origin !== getConfig().STUDIO_BASE_URL) return;
 
   if (event.data?.type === "COURSE_REFRESH_TRIGGER") {
     // Authoring MFE listens for changes to this key to trigger a refresh of the course data.
