8ab74f86fc739fc0746fc61ac81228d77304086e
Tobias Macey <tmacey@mit.edu>
2025-09-21 21:29:26 -0400

    config: Fix lookup of AWS EKS charts with Renovate

--- src/bridge/lib/versions.py
+++ src/bridge/lib/versions.py
@@ -25,9 +25,9 @@ AIRBYTE_CHART_VERSION = "2.0.12"
 SUPERSET_CHART_VERSION = "0.15.0"
 # renovate: datasource=docker depName=apisix packageName=registry-1.docker.io/bitnamicharts/apisix
 APISIX_CHART_VERSION = "5.1.6"
-# renovate: datasource=helm depName=aws-load-balancer-controller packageName=eks/aws-load-balancer-controller registryUrl=https://aws.github.io/eks-charts
+# renovate: datasource=helm depName=aws-load-balancer-controller packageName=aws-load-balancer-controller registryUrl=https://aws.github.io/eks-charts
 AWS_LOAD_BALANCER_CONTROLLER_CHART_VERSION = "1.13.4"
-# renovate: datasource=helm depName=aws-node-termination-handler packageName=eks/aws-node-termination-handler registryUrl=https://aws.github.io/eks-charts
+# renovate: datasource=helm depName=aws-node-termination-handler packageName=aws-node-termination-handler registryUrl=https://aws.github.io/eks-charts
 AWS_NODE_TERMINATION_HANDLER_CHART_VERSION = "0.27.2"
 # renovate: datasource=helm depName=botkube packageName=botkube
 BOTKUBE_CHART_VERSION = "v1.14.0"
