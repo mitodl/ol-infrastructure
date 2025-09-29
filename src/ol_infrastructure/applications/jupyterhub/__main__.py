60b690ac5b2cacf972b10bf49f64ad144e4a3007
Dan Subak <dsubak@users.noreply.github.com>
2025-09-29 14:09:55 -0400

    Add new course image to build pipeline, recognized courses (#3630)

--- src/ol_infrastructure/applications/jupyterhub/__main__.py
+++ src/ol_infrastructure/applications/jupyterhub/__main__.py
@@ -297,7 +297,10 @@ COURSE_NAMES = [
     "introduction_to_data_analytics_and_machine_learning",
 ]
 COURSE_NAMES.extend(
-    [f"uai_source-uai.{i}" for i in [0, "0a", 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13]]
+    [
+        f"uai_source-uai.{i}"
+        for i in ["intro", 0, "0a", 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13]
+    ]
 )
 EXTRA_IMAGES = {
     course_name.replace(".", "-").replace("_", "-"): {
