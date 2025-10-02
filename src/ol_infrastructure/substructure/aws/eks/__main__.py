c3c308de4626864a0fb460fb9891002ec75f0221
dansubak <dansubak@mit.edu>
2025-10-02 11:10:02 -0400

    small tweaks

--- src/ol_infrastructure/substructure/aws/eks/__main__.py
+++ src/ol_infrastructure/substructure/aws/eks/__main__.py
@@ -789,8 +789,12 @@ ksm_release = kubernetes.helm.v3.Release(
                 "kube_pod_container_info",
                 "kube_pod_container_status_restarts_total",
             ],
-            "namespaces": ["jupyter"],
+            "namespaces": "jupyter",
             "customLabels": {"source": "kube-state-monitor"},
+            "image": {
+                "repository": "bitnamilegacy/kube-state-metrics",
+                "tag": "2.16.0-debian-12-r5",
+            },
         },
     ),
     opts=ResourceOptions(
