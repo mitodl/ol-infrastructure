ba1a50126bfe1bb8c4f54fc2f0acd65be590cb3d
dansubak <dansubak@mit.edu>
2025-10-02 12:23:29 -0400

    use extraArgs escape hatch

--- src/ol_infrastructure/substructure/aws/eks/__main__.py
+++ src/ol_infrastructure/substructure/aws/eks/__main__.py
@@ -453,37 +453,37 @@ alloy_configmap = kubernetes.core.v1.ConfigMap(
                 action       = "replace"
               }}
               rule {{
-                source_labels = ["source"]
-                regex         = "kube-state-monitor"
+                source_labels = ["job"]
+                regex         = "kube-state-metrics"
                 action        = "replace"
                 target_label  = "pod"
                 replacement   = "DROP_ME"
               }}
               // Collapse some labels that include UUIDs and are not useful
               rule {{
-                source_labels = ["source"]
-                regex         = "kube-state-monitor"
+                source_labels = ["job"]
+                regex         = "kube-state-metrics"
                 action        = "replace"
                 target_label  = "uid"
                 replacement   = "DROP_ME"
               }}
               rule {{
-                source_labels = ["source"]
-                regex         = "kube-state-monitor"
+                source_labels = ["job"]
+                regex         = "kube-state-metrics"
                 action        = "replace"
                 target_label  = "image_spec"
                 replacement   = "DROP_ME"
               }}
               rule {{
-                source_labels = ["source"]
-                regex         = "kube-state-monitor"
+                source_labels = ["job"]
+                regex         = "kube-state-metrics"
                 action        = "replace"
                 target_label  = "image_id"
                 replacement   = "DROP_ME"
               }}
               rule {{
-                source_labels = ["source"]
-                regex         = "kube-state-monitor"
+                source_labels = ["job"]
+                regex         = "kube-state-metrics"
                 action        = "replace"
                 target_label  = "container_id"
                 replacement   = "DROP_ME"
