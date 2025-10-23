d36cfad6a072ff978bb903f5d6513ca87e0429fb
Tobias Macey <tmacey@mit.edu>
2025-10-23 09:31:42 -0400

    fix: Add GRPC env vars for Dagster Daemon
    The settings were added to the user code deployments and webserver but missed getting
    added othe daemn. This has prevented the lakehouse schedules from executing.

--- src/ol_infrastructure/applications/dagster/__main__.py
+++ src/ol_infrastructure/applications/dagster/__main__.py
@@ -779,11 +779,15 @@ dagster_helm_values = {
                 "value": "300",
             },
             {
-                "name": "GRPC_MAX_SEND_MESSAGE_LENGTH",
+                "name": "DAGSTER_GRPC_TIMEOUT_SECONDS",
+                "value": "300",
+            },
+            {
+                "name": "DAGSTER_GRPC_MAX_SEND_BYTES",
                 "value": "268435456",
             },
             {
-                "name": "GRPC_MAX_RECEIVE_MESSAGE_LENGTH",
+                "name": "DAGSTER_GRPC_MAX_RX_BYTES",
                 "value": "268435456",
             },
             {"name": "AWS_DEFAULT_REGION", "value": "us-east-1"},
