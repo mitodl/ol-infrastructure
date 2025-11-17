7cf7e6a65d64d84868798cc00efd7c693e85f274
Tobias Macey <tmacey@mit.edu>
2025-11-17 15:35:33 -0500

    fix: Stringify bools for K8s env vars

--- src/ol_infrastructure/applications/mit_learn_nextjs/__main__.py
+++ src/ol_infrastructure/applications/mit_learn_nextjs/__main__.py
@@ -142,10 +142,10 @@ raw_env_vars = {
     "NEXT_PUBLIC_SENTRY_TRACES_SAMPLE_RATE": "0.25",
     "NEXT_PUBLIC_SITE_NAME": "MIT Learn",
     "NEXT_PUBLIC_VERSION": MIT_LEARN_NEXTJS_DOCKER_TAG,
-    "NEXT_PUBLIC_FEATURE_product_page_courses": False,
-    "NEXT_PUBLIC_FEATURE_enrollment_dashboard": False,  # pragma: allowlist secret
-    "NEXT_PUBLIC_FEATURE_lr_drawer_chatbot": True,
-    "NEXT_PUBLIC_FEATURE_home_page_recommendation_bot": True,  # pragma: allowlist secret  # noqa: E501
+    "NEXT_PUBLIC_FEATURE_product_page_courses": "false",
+    "NEXT_PUBLIC_FEATURE_enrollment_dashboard": "false",  # pragma: allowlist secret
+    "NEXT_PUBLIC_FEATURE_lr_drawer_chatbot": "true",
+    "NEXT_PUBLIC_FEATURE_home_page_recommendation_bot": "true",  # pragma: allowlist secret  # noqa: E501
 }
 
 env_vars = []
