c3b9c8a9337d8e2c86095aa93ff6592eb97978e8
Tobias Macey <tmacey@mit.edu>
2026-01-11 15:05:21 -0500

    config: Increase Celery concurrency to 2 for edxapps

--- src/ol_infrastructure/applications/edxapp/k8s_resources.py
+++ src/ol_infrastructure/applications/edxapp/k8s_resources.py
@@ -802,7 +802,7 @@ def create_k8s_resources(  # noqa: C901
                                 "--queues=edx.lms.core.default,edx.lms.core.high,edx.lms.core.high_mem",
                                 "--exclude-queues=edx.cms.core.default",
                                 "-Ofair",
-                                "--concurrency=1",  # Don't try to use all cores on node
+                                "--concurrency=2",  # Don't try to use all cores on node
                             ],
                             env=[
                                 kubernetes.core.v1.EnvVarArgs(
@@ -1268,7 +1268,7 @@ def create_k8s_resources(  # noqa: C901
                                 "--queues=edx.cms.core.default",
                                 "--exclude-queues=edx.lms.core.default",
                                 "-Ofair",
-                                "--concurrency=1",  # Don't try to use all cores on node
+                                "--concurrency=2",  # Don't try to use all cores on node
                             ],
                             env=[
                                 kubernetes.core.v1.EnvVarArgs(
