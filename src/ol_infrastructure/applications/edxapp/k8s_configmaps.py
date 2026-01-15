e7f7dac787d5bcb6dfbcb241e405fd8bedf59b19
Asad Ali <asad.ali@arbisoft.com>
2026-01-15 14:37:27 +0500

    Update TRANSLATIONS_REPO_PATH in k8s_configmaps.py

--- src/ol_infrastructure/applications/edxapp/k8s_configmaps.py
+++ src/ol_infrastructure/applications/edxapp/k8s_configmaps.py
@@ -265,7 +265,7 @@ def _build_interpolated_config_dict(
                 )
                 or False,
                 "TRANSLATIONS_GITHUB_TOKEN": "",
-                "TRANSLATIONS_REPO_PATH": "",
+                "TRANSLATIONS_REPO_PATH": "/openedx/data/mitxonline-translations",
                 "TRANSLATIONS_REPO_URL": "https://github.com/mitodl/mitxonline-translations.git",
                 "IDA_LOGOUT_URI_LIST": [
                     f"https://{marketing_domain}/logout",
