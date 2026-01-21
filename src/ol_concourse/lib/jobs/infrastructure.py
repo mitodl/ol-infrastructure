cab7e65b08264acb66efd0435bca0e9901573277
Tobias Macey <tmacey@mit.edu>
2026-01-21 13:07:46 -0500

    hack: Temporarily hard-code pulumi job to use all inputs after V8 update

--- src/ol_concourse/lib/jobs/infrastructure.py
+++ src/ol_concourse/lib/jobs/infrastructure.py
@@ -359,6 +359,7 @@ def pulumi_job(  # noqa: PLR0913
                 ),
             ),
             PutStep(
+                inputs=["all"],
                 put=pulumi_resource.name,
                 get_params={"skip_implicit_get": True},
                 params={
