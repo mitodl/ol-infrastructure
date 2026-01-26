2e49c51909167fcb3349f89ac9434e7a999399d6
Tobias Macey <tmacey@mit.edu>
2026-01-26 16:47:17 -0500

    chore: Remove Consul references from Learn
    These values are no longer loaded from Consul now that edX is running on Kubernetes

--- src/ol_infrastructure/applications/mit_learn/__main__.py
+++ src/ol_infrastructure/applications/mit_learn/__main__.py
@@ -8,7 +8,6 @@ import textwrap
 from pathlib import Path
 from string import Template
 
-import pulumi_consul as consul
 import pulumi_fastly as fastly
 import pulumi_github as github
 import pulumi_kubernetes as kubernetes
@@ -63,7 +62,6 @@ from ol_infrastructure.lib.aws.eks_helper import (
     setup_k8s_provider,
 )
 from ol_infrastructure.lib.aws.iam_helper import IAM_POLICY_VERSION, lint_iam_policy
-from ol_infrastructure.lib.consul import get_consul_provider
 from ol_infrastructure.lib.fastly import (
     build_fastly_log_format_string,
     get_fastly_provider,
@@ -1229,96 +1227,6 @@ cert_manager_certificate = OLCertManagerCert(
         dns_names=[mitlearn_config.require("api_domain")],
     ),
 )
-
-xpro_consul_opts = get_consul_provider(
-    stack_info=stack_info,
-    consul_address=f"https://consul-xpro-{stack_info.env_suffix}.odl.mit.edu",
-    provider_name=f"consul-provider-xpro-{stack_info.env_suffix}",
-)
-consul.Keys(
-    f"learn-api-domain-consul-key-for-xpro-openedx-{stack_info.env_suffix}",
-    keys=[
-        consul.KeysKeyArgs(
-            path="edxapp/learn-api-domain",
-            delete=False,
-            value=mitlearn_api_domain,
-        ),
-        consul.KeysKeyArgs(
-            path="edxapp/learn-frontend-domain",
-            delete=False,
-            value=learn_frontend_domain,
-        ),
-    ],
-    opts=xpro_consul_opts,
-)
-
-
-mitxonline_consul_opts = get_consul_provider(
-    stack_info,
-    consul_address=f"https://consul-mitxonline-{stack_info.env_suffix}.odl.mit.edu",
-    provider_name=f"consul-provider-mitxonline-{stack_info.env_suffix}",
-)
-consul.Keys(
-    "learn-api-domain-consul-key-for-mitxonline-openedx",
-    keys=[
-        consul.KeysKeyArgs(
-            path="edxapp/learn-api-domain",
-            delete=False,
-            value=mitlearn_api_domain,
-        ),
-        consul.KeysKeyArgs(
-            path="edxapp/learn-frontend-domain",
-            delete=False,
-            value=learn_frontend_domain,
-        ),
-    ],
-    opts=mitxonline_consul_opts,
-)
-
-mitx_consul_opts = get_consul_provider(
-    stack_info=stack_info,
-    consul_address=f"https://consul-mitx-{stack_info.env_suffix}.odl.mit.edu",
-    provider_name=f"consul-provider-mitx-{stack_info.env_suffix}",
-)
-consul.Keys(
-    f"learn-api-domain-consul-key-for-mitx-openedx-{stack_info.env_suffix}",
-    keys=[
-        consul.KeysKeyArgs(
-            path="edxapp/learn-api-domain",
-            delete=False,
-            value=mitlearn_api_domain,
-        ),
-        consul.KeysKeyArgs(
-            path="edxapp/learn-frontend-domain",
-            delete=False,
-            value=learn_frontend_domain,
-        ),
-    ],
-    opts=mitx_consul_opts,
-)
-
-mitx_staging_consul_opts = get_consul_provider(
-    stack_info=stack_info,
-    consul_address=f"https://consul-mitx-staging-{stack_info.env_suffix}.odl.mit.edu",
-    provider_name=f"consul-provider-mitx-staging-{stack_info.env_suffix}",
-)
-consul.Keys(
-    f"learn-api-domain-consul-key-for-mitx-staging-openedx-{stack_info.env_suffix}",
-    keys=[
-        consul.KeysKeyArgs(
-            path="edxapp/learn-api-domain",
-            delete=False,
-            value=mitlearn_api_domain,
-        ),
-        consul.KeysKeyArgs(
-            path="edxapp/learn-frontend-domain",
-            delete=False,
-            value=learn_frontend_domain,
-        ),
-    ],
-    opts=mitx_staging_consul_opts,
-)
-
 
 # Redis / Elasticache
 # only for applications deployed in k8s
