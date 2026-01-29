d0d4424386aa043b882664b6b629d3d86fb76e2a
Arslan Ashraf <arslan.ashraf@arbisoft.com>
2026-01-29 15:28:27 +0500

    config: disable COURSES_INVITE_ONLY for residential MITx

--- src/ol_infrastructure/applications/edxapp/config_builder.py
+++ src/ol_infrastructure/applications/edxapp/config_builder.py
@@ -423,6 +423,7 @@ def get_deployment_overrides(env_prefix: str) -> ConfigDict:
         "EMAIL_HOST": "outgoing.mit.edu",
         "EMAIL_PORT": 587,
         "EMAIL_USE_TLS": True,
+        "COURSES_INVITE_ONLY": False,
         "COURSE_MODE_DEFAULTS": {
             "name": "Honor",
             "android_sku": None,
