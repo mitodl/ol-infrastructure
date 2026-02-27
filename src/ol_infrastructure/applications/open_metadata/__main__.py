5f1ad9b48cb75381165129372a5be2f62f92af3f
Tobias Macey <tmacey@mit.edu>
2026-02-27 11:18:33 -0500

    config: Pin K8s ingestion image to release version

--- src/ol_infrastructure/applications/open_metadata/__main__.py
+++ src/ol_infrastructure/applications/open_metadata/__main__.py
@@ -329,6 +329,7 @@ open_metadata_application = kubernetes.helm.v3.Release(
                             # The schema doesn't yet support this value (TMM 2026-02-26)
                             # "namespace": open_metadata_namespace,  # noqa: ERA001
                             "enableFailureDiagnostics": True,
+                            "ingestionImage": f"docker.getcollate.io/openmetadata/ingestion-base:{OPEN_METADATA_VERSION}",  # noqa: E501
                         },
                     },
                     "elasticsearch": {
