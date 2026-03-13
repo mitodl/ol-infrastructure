a45d3fd94e7ab4485b3bf80b1767f331900c75c7
Christopher Patti <feoh@feoh.org>
2026-03-13 15:12:08 -0400

    feat: Add a meta pipeline for both ol-django and openedx-plugins
    This pipeline will:
      - Publish ol-django when there are changes to publish
      - Publish openedx-plugins when ol-django is updated

--- src/ol_concourse/pipelines/libraries/ol_django.py
+++ src/ol_concourse/pipelines/libraries/ol_django.py
@@ -1,4 +1,4 @@
-"""Generate a publish pipeline for all packages in the open-edx-plugins repo."""
+"""Generate a publish pipeline for all packages in the ol-django monorepo."""
 
 from pathlib import Path
 
@@ -8,39 +8,40 @@ from ol_concourse.pipelines.libraries.pypi_monorepo import (
     monorepo_publish_pipeline,
 )
 
-SOURCE_REPO_URI = "https://github.com/mitodl/open-edx-plugins"
+SOURCE_REPO_URI = "https://github.com/mitodl/ol-django"
 EXPECTED_ARG_COUNT = 2
 
 
 def pipeline_from_source(source_repo_path: str | Path) -> tuple[list[str], Pipeline]:
     """Generate the pipeline from a checked-out source repository."""
 
-    plugins = discover_python_packages(source_repo_path)
+    package_dirs = discover_python_packages(source_repo_path)
     pipeline = monorepo_publish_pipeline(
         source_repo_uri=SOURCE_REPO_URI,
-        package_dirs=plugins,
-        build_command_factory=lambda plugin, repo_name: (
+        package_dirs=package_dirs,
+        build_command_factory=lambda package_dir, repo_name: (
             f"""
             cd {repo_name};
-            uv build --package {plugin};
+            PYTHONPATH=build-support/bin uv build src/{package_dir};
             uvx twine check dist/*
             uvx twine upload --skip-existing --non-interactive dist/*
             """
         ),
     )
-    return plugins, pipeline
+    return package_dirs, pipeline
+
 
 if __name__ == "__main__":
     import sys
 
     if len(sys.argv) != EXPECTED_ARG_COUNT:
-        msg = "Usage: build_publish_plugins.py <path-to-open-edx-plugins-checkout>"
+        msg = "Usage: ol_django.py <path-to-ol-django-checkout>"
         raise SystemExit(msg)
 
-    plugins, pipeline = pipeline_from_source(sys.argv[1])
+    package_dirs, pipeline = pipeline_from_source(sys.argv[1])
     with open("definition.json", "w") as definition:  # noqa: PTH123
         definition.write(pipeline.model_dump_json(indent=2))
     sys.stdout.write(pipeline.model_dump_json(indent=2))
     print()  # noqa: T201
-    print(f"Discovered packages: {', '.join(plugins)}")  # noqa: T201
-    print("fly -t pr-main sp -p publish-open-edx-plugins-pypi -c definition.json")  # noqa: T201
+    print(f"Discovered packages: {', '.join(package_dirs)}")  # noqa: T201
+    print("fly -t pr-main sp -p publish-ol-django-pypi -c definition.json")  # noqa: T201
---
+++ src/ol_concourse/pipelines/libraries/python_packages_meta.py
@@ -0,0 +1,177 @@
+"""Generate the meta pipeline that manages Python monorepo publish pipelines."""
+
+import sys
+
+from ol_concourse.lib.models.pipeline import (
+    AnonymousResource,
+    Command,
+    GetStep,
+    Identifier,
+    Input,
+    Job,
+    Output,
+    Pipeline,
+    Platform,
+    SetPipelineStep,
+    TaskConfig,
+    TaskStep,
+)
+from ol_concourse.lib.resources import git_repo
+
+OL_INFRASTRUCTURE_REPO = git_repo(
+    name=Identifier("python-package-pipeline-definitions"),
+    uri="https://github.com/mitodl/ol-infrastructure",
+    branch="main",
+    paths=[
+        "src/ol_concourse/lib/",
+        "src/ol_concourse/pipelines/libraries/",
+        "src/ol_concourse/pipelines/open_edx/open_edx_plugins/",
+    ],
+)
+
+OPEN_EDX_PLUGINS_REPO = git_repo(
+    name=Identifier("open-edx-plugins-source"),
+    uri="https://github.com/mitodl/open-edx-plugins",
+    branch="main",
+    paths=["src/", "pyproject.toml", "uv.lock"],
+)
+
+OL_DJANGO_REPO = git_repo(
+    name=Identifier("ol-django-source"),
+    uri="https://github.com/mitodl/ol-django",
+    branch="main",
+    paths=["src/", "pyproject.toml", "uv.lock"],
+)
+
+PIPELINE_IMAGE = AnonymousResource(
+    type="registry-image",
+    source={
+        "repository": "mitodl/ol-infrastructure",
+        "tag": "latest",
+    },
+)
+
+
+def set_child_pipeline_job(
+    *,
+    job_name: str,
+    pipeline_name: str,
+    generator_script: str,
+    source_resource_name: str,
+) -> Job:
+    """Build a job that generates and sets a child pipeline."""
+
+    return Job(
+        name=Identifier(job_name),
+        plan=[
+            GetStep(get=OL_INFRASTRUCTURE_REPO.name, trigger=True),
+            GetStep(get=Identifier(source_resource_name), trigger=True),
+            TaskStep(
+                task=Identifier(f"generate-{pipeline_name}-definition"),
+                config=TaskConfig(
+                    platform=Platform.linux,
+                    image_resource=PIPELINE_IMAGE,
+                    inputs=[
+                        Input(name=OL_INFRASTRUCTURE_REPO.name),
+                        Input(name=Identifier(source_resource_name)),
+                    ],
+                    outputs=[Output(name=Identifier("pipeline"))],
+                    run=Command(
+                        path="sh",
+                        dir="pipeline",
+                        user="root",
+                        args=[
+                            "-exc",
+                            (
+                                "python "
+                                f"../{OL_INFRASTRUCTURE_REPO.name}/{generator_script} "
+                                f"../{source_resource_name}"
+                            ),
+                        ],
+                    ),
+                ),
+            ),
+            SetPipelineStep(
+                set_pipeline=Identifier(pipeline_name),
+                file="pipeline/definition.json",
+            ),
+        ],
+    )
+
+
+def self_update_job() -> Job:
+    """Build the job that regenerates this meta pipeline."""
+
+    return Job(
+        name=Identifier("set-python-packages-meta-pipeline"),
+        plan=[
+            GetStep(get=OL_INFRASTRUCTURE_REPO.name, trigger=True),
+            TaskStep(
+                task=Identifier("generate-python-packages-meta-definition"),
+                config=TaskConfig(
+                    platform=Platform.linux,
+                    image_resource=PIPELINE_IMAGE,
+                    inputs=[Input(name=OL_INFRASTRUCTURE_REPO.name)],
+                    outputs=[Output(name=Identifier("pipeline"))],
+                    run=Command(
+                        path="python",
+                        dir="pipeline",
+                        user="root",
+                        args=[
+                            (
+                                "../python-package-pipeline-definitions/src/"
+                                "ol_concourse/pipelines/libraries/python_packages_meta.py"
+                            )
+                        ],
+                    ),
+                ),
+            ),
+            SetPipelineStep(
+                set_pipeline="self",
+                file="pipeline/definition.json",
+            ),
+        ],
+    )
+
+
+def meta_pipeline() -> Pipeline:
+    """Generate the meta pipeline definition."""
+
+    return Pipeline(
+        resources=[
+            OL_INFRASTRUCTURE_REPO,
+            OPEN_EDX_PLUGINS_REPO,
+            OL_DJANGO_REPO,
+        ],
+        jobs=[
+            self_update_job(),
+            set_child_pipeline_job(
+                job_name="set-open-edx-plugins-pipeline",
+                pipeline_name="publish-open-edx-plugins-pypi",
+                generator_script=(
+                    "src/ol_concourse/pipelines/open_edx/open_edx_plugins/"
+                    "build_publish_plugins.py"
+                ),
+                source_resource_name=str(OPEN_EDX_PLUGINS_REPO.name),
+            ),
+            set_child_pipeline_job(
+                job_name="set-ol-django-pipeline",
+                pipeline_name="publish-ol-django-pypi",
+                generator_script=(
+                    "src/ol_concourse/pipelines/libraries/ol_django.py"
+                ),
+                source_resource_name=str(OL_DJANGO_REPO.name),
+            ),
+        ],
+    )
+
+
+if __name__ == "__main__":
+    pipeline = meta_pipeline()
+    with open("definition.json", "w") as definition:  # noqa: PTH123
+        definition.write(pipeline.model_dump_json(indent=2))
+    sys.stdout.write(pipeline.model_dump_json(indent=2))
+    sys.stdout.write("\n")
+    sys.stdout.write(
+        "fly -t pr-main sp -p publish-python-packages-meta -c definition.json\n"
+    )
