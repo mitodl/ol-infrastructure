721aa9cc87d7eb9d8b6a5823b769b9931f3d31c8
Chris Chudzicki <christopher.chudzicki@gmail.com>
2026-04-14 11:37:09 -0400

    use Application enum

--- src/ol_infrastructure/applications/mit_learn/__main__.py
+++ src/ol_infrastructure/applications/mit_learn/__main__.py
@@ -1029,7 +1029,7 @@ mitlearn_fastly_service = fastly.ServiceVcl(
             content_type="application/json",
             format=build_fastly_log_format_string(
                 additional_static_fields={
-                    "application": stack_info.env_prefix,
+                    "application": Application.mit_learn,
                     "environment": stack_info.env_suffix,
                 }
             ),
