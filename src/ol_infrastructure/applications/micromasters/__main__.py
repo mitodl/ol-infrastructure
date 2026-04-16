8243340b367a267cc518fa9c80e9f9f6c0990174
Ardiea <mas48@mit.edu>
2026-04-16 14:15:22 -0400

    chorse: cleanup heroku references in micromasters pulumi code.

--- src/ol_infrastructure/applications/micromasters/__main__.py
+++ src/ol_infrastructure/applications/micromasters/__main__.py
@@ -15,12 +15,10 @@ import pulumi
 import pulumi_fastly as fastly
 import pulumi_kubernetes as kubernetes
 import pulumi_vault as vault
-import pulumiverse_heroku as heroku
 from pulumi import (
     ROOT_STACK_RESOURCE,
     Alias,
     Config,
-    InvokeOptions,
     Output,
     ResourceOptions,
     StackReference,
@@ -77,7 +75,6 @@ from ol_infrastructure.lib.fastly import (
     build_fastly_log_format_string,
     get_fastly_provider,
 )
-from ol_infrastructure.lib.heroku import setup_heroku_provider
 from ol_infrastructure.lib.ol_types import (
     AWSBase,
     BusinessUnit,
@@ -93,11 +90,8 @@ from ol_infrastructure.lib.stack_defaults import defaults
 from ol_infrastructure.lib.vault import setup_vault_provider
 
 setup_vault_provider(skip_child_token=True)
-setup_heroku_provider()
 
 micromasters_config = Config("micromasters")
-heroku_config = Config("heroku")
-heroku_app_config = Config("heroku_app")
 
 stack_info = parse_stack()
 network_stack = StackReference(f"infrastructure.aws.network.{stack_info.name}")
@@ -279,186 +273,6 @@ micromasters_vault_backend_config = OLVaultPostgresDatabaseConfig(
     db_host=micromasters_db.db_instance.address,
 )
 micromasters_vault_backend = OLVaultDatabaseBackend(micromasters_vault_backend_config)
-
-heroku_vars = {
-    "BATCH_UPDATE_RATE_LIMIT": "2/m",
-    "CLIENT_ELASTICSEARCH_URL": "/api/v0/search/",
-    "CRONTAB_DISCUSSIONS_SYNC": "0 9 * * *",
-    "ENABLE_STUNNEL_AMAZON_RDS_FIX": "True",
-    "FEATURE_ENABLE_PROGRAM_LETTER": "True",
-    "FEATURE_FINAL_GRADE_ALGORITHM": "v1",
-    "FEATURE_MITXONLINE_LOGIN": "True",
-    "FEATURE_OPEN_DISCUSSIONS_CREATE_CHANNEL_UI": "True",
-    "FEATURE_OPEN_DISCUSSIONS_POST_UI": "True",
-    "FEATURE_OPEN_DISCUSSIONS_USER_SYNC": "True",
-    "FEATURE_OPEN_DISCUSSIONS_USER_UPDATE": "False",
-    "FEATURE_PROGRAM_RECORD_LINK": "True",
-    "MICROMASTERS_ADMIN_EMAIL": "cuddle-bunnies@mit.edu",
-    "MICROMASTERS_DB_CONN_MAX_AGE": 0,
-    "MICROMASTERS_DB_DISABLE_SSL": "True",
-    "MICROMASTERS_ECOMMERCE_EMAIL": "cuddle-bunnies@mit.edu",
-    "MICROMASTERS_EMAIL_PORT": 587,
-    "MICROMASTERS_EMAIL_TLS": "True",
-    "MICROMASTERS_FROM_EMAIL": "MITx MicroMasters <micromasters-support@mit.edu>",
-    "MICROMASTERS_SUPPORT_EMAIL": "micromasters-support@mit.edu",
-    "MICROMASTERS_USE_S3": "True",
-    "NODE_MODULES_CACHE": "False",
-    "OPENSEARCH_INDEX": "micromasters",
-    "OPEN_DISCUSSIONS_REDIRECT_COMPLETE_URL": "/complete/micromasters",
-    "OPEN_DISCUSSIONS_SITE_KEY": "micromasters",
-    "OPEN_EXCHANGE_RATES_URL": "https://openexchangerates.org/api/",
-    "SENTRY_ORG_NAME": "mit-office-of-digital-learning",
-    "SENTRY_PROJECT_NAME": "micromasters",
-    "UWSGI_PROCESSES": 2,
-    "UWSGI_SOCKET_TIMEOUT": 1,
-    "UWSGI_THREADS": 50,
-}
-
-# Combine var source above with values explicitly defined in pulumi configuration file
-heroku_vars.update(**heroku_app_config.get_object("vars"))
-
-auth_aws_mitx_micromasters = vault.generic.get_secret_output(
-    path="aws-mitx/creds/micromasters",
-    with_lease_start_time=False,
-    opts=InvokeOptions(parent=micromasters_vault_backend_role),
-)
-
-postgres_micromasters_creds_app = vault.generic.get_secret_output(
-    path="postgres-micromasters/creds/app",
-    with_lease_start_time=False,
-    opts=InvokeOptions(parent=micromasters_vault_backend_role),
-)
-secret_micromasters_edx = vault.generic.get_secret_output(
-    path="secret-micromasters/edx",
-    opts=InvokeOptions(parent=micromasters_vault_backend_role),
-)
-secret_micromasters_google = vault.generic.get_secret_output(
-    path="secret-micromasters/google",
-    opts=InvokeOptions(parent=micromasters_vault_backend_role),
-)
-secret_micromasters_mitxonline = vault.generic.get_secret_output(
-    path="secret-micromasters/mitxonline",
-    opts=InvokeOptions(parent=micromasters_vault_backend_role),
-)
-secret_micromasters_opensearch = vault.generic.get_secret_output(
-    path="secret-micromasters/opensearch",
-    opts=InvokeOptions(parent=micromasters_vault_backend_role),
-)
-secret_micromasters_open_discussions = vault.generic.get_secret_output(
-    path="secret-micromasters/open-discussions",
-    opts=InvokeOptions(parent=micromasters_vault_backend_role),
-)
-secret_micromasters_open_exchange_rates = vault.generic.get_secret_output(
-    path="secret-micromasters/open-exchange-rates",
-    opts=InvokeOptions(parent=micromasters_vault_backend_role),
-)
-secret_micromasters_django = vault.generic.get_secret_output(
-    path="secret-micromasters/django",
-    opts=InvokeOptions(parent=micromasters_vault_backend_role),
-)
-secret_micromasters_sentry = vault.generic.get_secret_output(
-    path="secret-micromasters/sentry",
-    opts=InvokeOptions(parent=micromasters_vault_backend_role),
-)
-secret_micromasters_cybersource = vault.generic.get_secret_output(
-    path="secret-micromasters/cybersource",
-    opts=InvokeOptions(parent=micromasters_vault_backend_role),
-)
-secret_mailgun = vault.generic.get_secret_output(
-    path="secret-operations/mailgun",
-    opts=InvokeOptions(parent=micromasters_vault_backend_role),
-)
-secret_operations_mit_smtp = vault.generic.get_secret_output(
-    path="secret-operations/mit-smtp",
-    opts=InvokeOptions(parent=micromasters_vault_backend_role),
-)
-
-sensitive_heroku_vars = {
-    "AWS_ACCESS_KEY_ID": auth_aws_mitx_micromasters.data.apply(
-        lambda data: "{}".format(data["access_key"])
-    ),
-    "AWS_SECRET_ACCESS_KEY": auth_aws_mitx_micromasters.data.apply(
-        lambda data: "{}".format(data["secret_key"])
-    ),
-    "CYBERSOURCE_ACCESS_KEY": secret_micromasters_cybersource.data.apply(
-        lambda data: "{}".format(data["access_key"])
-    ),
-    "CYBERSOURCE_PROFILE_ID": secret_micromasters_cybersource.data.apply(
-        lambda data: "{}".format(data["profile_id"])
-    ),
-    "CYBERSOURCE_SECURITY_KEY": secret_micromasters_cybersource.data.apply(
-        lambda data: "{}".format(data["security_key"])
-    ),
-    "DATABASE_URL": postgres_micromasters_creds_app.data.apply(
-        lambda data: (
-            "postgres://{}:{}@micromasters-{}-app-db.cbnm7ajau6mi.us-east-1.rds.amazonaws.com:5432/micromasters".format(
-                data["username"], data["password"], stack_info.env_suffix.lower()
-            )
-        )
-    ),
-    "MAILGUN_KEY": secret_mailgun.data.apply(lambda data: "{}".format(data["api_key"])),
-    "MICROMASTERS_EMAIL_HOST": secret_operations_mit_smtp.data.apply(
-        lambda data: "{}".format(data["relay_host"])
-    ),
-    "MICROMASTERS_EMAIL_PASSWORD": secret_operations_mit_smtp.data.apply(
-        lambda data: "{}".format(data["relay_password"])
-    ),
-    "MICROMASTERS_EMAIL_USER": secret_operations_mit_smtp.data.apply(
-        lambda data: "{}".format(data["relay_username"])
-    ),
-    "EDXORG_CLIENT_ID": secret_micromasters_edx.data.apply(
-        lambda data: "{}".format(data["client_id"])
-    ),
-    "EDXORG_CLIENT_SECRET": secret_micromasters_edx.data.apply(
-        lambda data: "{}".format(data["client_secret"])
-    ),
-    "GOOGLE_API_KEY": secret_micromasters_google.data.apply(
-        lambda data: "{}".format(data["api_key"])
-    ),
-    "MITXONLINE_CLIENT_ID": secret_micromasters_mitxonline.data.apply(
-        lambda data: "{}".format(data["oauth_client_id"])
-    ),
-    "MITXONLINE_CLIENT_SECRET": secret_micromasters_mitxonline.data.apply(
-        lambda data: "{}".format(data["oauth_client_secret"])
-    ),
-    "MITXONLINE_STAFF_ACCESS_TOKEN": secret_micromasters_mitxonline.data.apply(
-        lambda data: "{}".format(data["staff_access_token"])
-    ),
-    "OPENSEARCH_HTTP_AUTH": secret_micromasters_opensearch.data.apply(
-        lambda data: "{}".format(data["http_auth"])
-    ),
-    "OPENSEARCH_URL": secret_micromasters_opensearch.data.apply(
-        lambda data: "{}".format(data["url"])
-    ),
-    "OPEN_DISCUSSIONS_JWT_SECRET": secret_micromasters_open_discussions.data.apply(
-        lambda data: "{}".format(data["jwt_secret"])
-    ),
-    "OPEN_EXCHANGE_RATES_APP_ID": secret_micromasters_open_exchange_rates.data.apply(
-        lambda data: "{}".format(data["app_id"])
-    ),
-    "SENTRY_AUTH_TOKEN": secret_micromasters_sentry.data.apply(
-        lambda data: "{}".format(data["auth_token"])
-    ),
-    "SENTRY_DSN": secret_micromasters_sentry.data.apply(
-        lambda data: "{}".format(data["dsn"])
-    ),
-}
-if stack_info.env_suffix == "production":
-    sensitive_heroku_vars["SECRET_KEY"] = secret_micromasters_django.data.apply(
-        lambda data: "{}".format(data["secret_key"])
-    )
-    sensitive_heroku_vars["STATUS_TOKEN"] = secret_micromasters_django.data.apply(
-        lambda data: "{}".format(data["status_token"])
-    )
-
-heroku_app_id = heroku_config.require("app_id")
-micromasters_heroku_configassociation = heroku.app.ConfigAssociation(
-    f"micromasters-{stack_info.env_suffix}-heroku-configassociation",
-    app_id=heroku_app_id,
-    sensitive_vars=sensitive_heroku_vars,
-    vars=heroku_vars,
-)
-
 
 if micromasters_config.get_bool("deploy_k8s"):
     vault_config = Config("vault")
