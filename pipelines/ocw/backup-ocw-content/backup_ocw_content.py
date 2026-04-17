6af745820f0416dae20eaff625ee60a3a08f0ba4
Christopher Patti <feoh@feoh.org>
2026-04-17 17:00:29 -0400

    feat: Create new OCW backup with s3 sync Concourse resource.

---
+++ pipelines/ocw/backup-ocw-content/backup_ocw_content.py
@@ -0,0 +1,76 @@
+"""Concourse pipeline to back up OCW content S3 buckets.
+
+Syncs draft and live OCW content buckets to their backup counterparts every 6
+hours using the mitodl/concourse-s3-sync-resource.
+
+Deploy with (substituting the appropriate environment vars file):
+
+    fly -t <target> set-pipeline \\
+        -p backup-ocw-content-<env> \\
+        -c definition.json \\
+        -l vars-backup-<env>.yml
+"""
+
+import sys
+
+from ol_concourse.lib.models.pipeline import (
+    GetStep,
+    Identifier,
+    InParallelStep,
+    Job,
+    Pipeline,
+    PutStep,
+    Resource,
+)
+from ol_concourse.lib.resource_types import s3_sync
+from ol_concourse.lib.resources import schedule
+
+build_schedule = schedule(
+    name=Identifier("build-schedule"),
+    interval="6h",
+)
+
+draft_backup = Resource(
+    name=Identifier("backup-draft-content"),
+    type="s3-sync",
+    check_every="never",
+    source={
+        "bucket": "((draft_destination_bucket_name))",
+        "source_bucket": "((draft_source_bucket_name))",
+    },
+)
+
+live_backup = Resource(
+    name=Identifier("backup-live-content"),
+    type="s3-sync",
+    check_every="never",
+    source={
+        "bucket": "((live_destination_bucket_name))",
+        "source_bucket": "((live_source_bucket_name))",
+    },
+)
+
+backup_pipeline = Pipeline(
+    resource_types=[s3_sync()],
+    resources=[build_schedule, draft_backup, live_backup],
+    jobs=[
+        Job(
+            name=Identifier("backup-ocw-content"),
+            plan=[
+                GetStep(get=build_schedule.name, trigger=True),
+                InParallelStep(
+                    in_parallel=[
+                        PutStep(put=draft_backup.name),
+                        PutStep(put=live_backup.name),
+                    ]
+                ),
+            ],
+        )
+    ],
+)
+
+if __name__ == "__main__":
+    with open("definition.json", "w") as definition:  # noqa: PTH123
+        definition.write(backup_pipeline.model_dump_json(indent=2))
+    sys.stdout.write(backup_pipeline.model_dump_json(indent=2))
+    print()  # noqa: T201
