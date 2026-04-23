23ce63a576d07071757b6a7aad83d15ee26a298e
Carey P Gumaer <gumaerc@mit.edu>
2026-04-23 11:12:54 -0400

    hide npm token from console output

--- src/ol_concourse/pipelines/libraries/scripts/api-clients-publish-node.sh
+++ src/ol_concourse/pipelines/libraries/scripts/api-clients-publish-node.sh
@@ -2,7 +2,9 @@
 # publish the npm package to npmjs.org
 
 export YARN_NPM_PUBLISH_REGISTRY=https://registry.npmjs.org/
-export YARN_NPM_AUTH_TOKEN=$NPM_TOKEN
+set +x
+export YARN_NPM_AUTH_TOKEN="${NPM_TOKEN}"
+set -x
 corepack enable
 COREPACK_ENABLE_DOWNLOAD_PROMPT=0 yarn install --immutable
 yarn build
