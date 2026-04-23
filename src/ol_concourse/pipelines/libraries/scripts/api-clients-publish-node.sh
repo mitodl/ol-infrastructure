7d782d8159b8afadb820c4016a703205329aead5
Carey P Gumaer <gumaerc@mit.edu>
2026-04-23 11:18:21 -0400

    export npm config to env instead of overwriting yarn config (#4535)
    * export npm config to env instead of overwriting yarn config
    
    * hide npm token from console output

--- src/ol_concourse/pipelines/libraries/scripts/api-clients-publish-node.sh
+++ src/ol_concourse/pipelines/libraries/scripts/api-clients-publish-node.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -x
 # publish the npm package to npmjs.org
 
-#echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
-echo "npmPublishRegistry: https://registry.npmjs.org/" > .yarnrc.yml
-echo "npmAuthToken: $NPM_TOKEN" >> .yarnrc.yml
+export YARN_NPM_PUBLISH_REGISTRY=https://registry.npmjs.org/
+set +x
+export YARN_NPM_AUTH_TOKEN="${NPM_TOKEN}"
+set -x
 corepack enable
 COREPACK_ENABLE_DOWNLOAD_PROMPT=0 yarn install --immutable
 yarn build
