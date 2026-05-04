7177e94bb64218042b3d7dfc0e1e54f15186ee61
Tobias Macey <tmacey@mit.edu>
2026-05-04 13:00:23 -0400

    config: Re-enable stack refresh on Dagster

--- src/ol_concourse/pipelines/infrastructure/dagster/pipeline.py
+++ src/ol_concourse/pipelines/infrastructure/dagster/pipeline.py
@@ -218,7 +218,7 @@ def build_dagster_docker_pipeline() -> Pipeline:
 
     pulumi_fragment = pulumi_jobs_chain(
         pulumi_code,
-        refresh_stack=False,
+        refresh_stack=True,
         stack_names=["QA", "Production"],
         project_name="ol-infrastructure-dagster-server",
         project_source_path=PULUMI_CODE_PATH.joinpath("applications/dagster/"),
