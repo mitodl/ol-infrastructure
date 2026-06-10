bd3928f6574fa3de60bf443adec534bf460df0c7
dansubak <dansubak@mit.edu>
2026-06-10 10:40:19 -0400

    This repo uses master instead of main

--- src/ol_concourse/pipelines/container_images/google_ads_optimization.py
+++ src/ol_concourse/pipelines/container_images/google_ads_optimization.py
@@ -15,7 +15,7 @@ from ol_concourse.lib.resources import git_repo
 ad_opt_repository = git_repo(
     name=Identifier("ad-opt-resource"),
     uri="https://github.com/josephine-situ/ad_opt_v2",
-    branch="main",
+    branch="master",
     check_every="24h",
 )
 
