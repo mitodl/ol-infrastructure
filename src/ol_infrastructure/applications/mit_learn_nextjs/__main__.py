aeeba281136729a4396502c16ba822c3a207146c
Chris Patti <feoh@feoh.org>
2026-08-07 17:21:54 -0400

    Migrate mit_learn_nextjs to stack-output Sentry DSN (#5227)
    * Migrate mit_learn_nextjs to stack-output Sentry DSN
    
    Confirmed with the mit-learn team: mit_learn_nextjs shares the
    'open-next' Sentry project with the mit_learn Django backend.
    
    Replace nextjs_config.require('sentry_dsn') with
    sentry_stack.require_output('open_next_sentry_dsn') and remove the
    now-unused encrypted nextjs:sentry_dsn Pulumi config value from
    Pulumi.{CI,QA,Production}.yaml.
    
    Part of mitodl/ol-infrastructure#5004.
    
    * fix: reference the mit_learn_sentry_dsn stack output
    
    The open-next Sentry key is exported from the sentry stack as
    mit_learn_sentry_dsn, not open_next_sentry_dsn, so require_output would
    have failed at preview time.

--- src/ol_infrastructure/applications/mit_learn_nextjs/__main__.py
+++ src/ol_infrastructure/applications/mit_learn_nextjs/__main__.py
@@ -35,6 +35,7 @@ from ol_infrastructure.lib.pulumi_helper import (
 stack_info = parse_stack()
 
 cluster_stack = make_stack_reference(projects.EKS, f"applications.{stack_info.name}")
+sentry_stack = make_stack_reference(projects.SENTRY, "Production")
 MIT_LEARN_NEXTJS_DOCKER_TAG = get_docker_image_tag("MIT_LEARN_NEXTJS")
 
 app_image = ecr_image_uri(
@@ -150,7 +151,7 @@ raw_env_vars = {
     "NEXT_PUBLIC_POSTHOG_API_KEY": nextjs_config.require("posthog_api_key"),
     "NEXT_PUBLIC_POSTHOG_PROJECT_ID": nextjs_config.require("posthog_project_id"),
     "NEXT_PUBLIC_POSTHOG_UI_HOST": "https://us.posthog.com",
-    "NEXT_PUBLIC_SENTRY_DSN": nextjs_config.require("sentry_dsn"),
+    "NEXT_PUBLIC_SENTRY_DSN": sentry_stack.require_output("mit_learn_sentry_dsn"),
     "NEXT_PUBLIC_SENTRY_ENV": nextjs_config.require("sentry_env"),
     "NEXT_PUBLIC_SENTRY_PROFILES_SAMPLE_RATE": "0.25",
     "NEXT_PUBLIC_SENTRY_TRACES_SAMPLE_RATE": "0.001",
