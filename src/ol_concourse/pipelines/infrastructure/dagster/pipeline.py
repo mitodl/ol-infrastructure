0ee1109729ff903be2be064277a62fe142d3f417
Tobias Macey <tmacey@mit.edu>
2026-09-01 13:10:24 -0400

    config: Don't gate Dagster QA deployments

--- src/ol_concourse/pipelines/infrastructure/dagster/pipeline.py
+++ src/ol_concourse/pipelines/infrastructure/dagster/pipeline.py
@@ -245,6 +245,7 @@ def build_dagster_docker_pipeline() -> Pipeline:
         topology="preview-gated",
         record_deployments=False,
         stack_names=["QA", "Production"],
+        auto_deploy_stages=["QA"],
         project_name="ol-infrastructure-dagster-server",
         project_source_path=PULUMI_CODE_PATH.joinpath("applications/dagster/"),
         dependencies=pulumi_dependencies,
